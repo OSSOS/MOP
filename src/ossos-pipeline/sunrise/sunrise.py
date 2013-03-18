e8cdb9638a81e8f23fc13574dde02fff239b64db
mtbannister <michele@mso.anu.edu.au>
2013-03-19 10:12:36 +1100

    update in sunrise

--- src/ossos-pipeline/sunrise/sunrise.py
+++ src/ossos-pipeline/sunrise/sunrise.py
@@ -1,16 +1,28 @@
 # sunrise.py
 # Retrieve last night's observations, check they're fine, start processing if possible.
 #
-# MTB 2013-03-14
+# MTB 2013-03
 
 
 	# retrieve image info from CADC via TAP: incorporate src/jjk/preproc/ObsStatus.py
 	ask_cadc_for_observations
 	# also getExposures.pl
-	# also need vos, VOspace tuned for CADC
-	# examine for contiguous image sets, images missing from ends of each field's observing
+
+	# need vos, VOspace tuned for CADC
+	# have a shared data space at CADC that all the ossos processing results
+	# are pushed out to: ossuary
+
+	# examine for contiguous image sets, 
+	# images missing from ends of each field's observing
+
 	# populate OSSOS metadata db, ossuary, with info
-	# process info to see if any fields in that batch of observation had three images in the night
+	populate_ossuary
+
+	# process info to see if any fields in that batch of observation had three 
+	# images in the night
 	# report via email on result of night's observing
-	# \copy postgres_db to stdout with csv header  (this'll be a daily dump of the db to disk)
+	
+	# \copy postgres_db to stdout with csv header  
+	# (this'll be a daily dump of the db to disk)
+	
 	# if fields > 0, initiate next step of processing, preproc.
