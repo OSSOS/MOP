3455ccecf56483a212cc0f31e91c7015fd0c387b
mtbannister <michele@mso.anu.edu.au>
2013-04-30 11:41:43 -0700

    db design in instantiate_db for images table (header metadata)

---
+++ src/ossos-pipeline/public_survey_overview.py
@@ -0,0 +1,29 @@
+# structuring the interface for ossos data
+#
+# so need a TAP query for ?
+
+# are there images: TAP query the telescope (where it puts stuff anyhow)
+# okay yes there are
+# 
+# are there triplets?
+# multiple images on a night: verify image quality to confirm if a 
+# triplet exist of sufficient quality
+
+
+
+
+
+# public summary page:
+#		proportion of survey complete: X %
+#			surveyed to completion: N sq. deg.
+#			where data have been fully processed: N sq. deg.
+#			(ie. area awaiting processing: difference between those two)
+#
+#		Objects discovered: X
+#		Objects reported to MPC: Y
+#	
+#		Current survey efficiency: Z %
+#
+#		Most recent observations that completed a block: [date]	
+#		Next scheduled OSSOS observations: 	[date] 
+#											expected Mauna Kea weather: [yank from website]
