d0c9b85ffca304a8c42749a27a068c8f3dda3617
drusk <drusk@uvic.ca>
2013-05-03 16:40:01 -0700

    Created parser module with brief doc but no implementation.

---
+++ src/mopgui/mopgui/parsing/parser.py
@@ -0,0 +1,12 @@
+"""
+Parsers for reading input specification files.
+"""
+
+class AstromParser(object):
+    """
+    Parses a .astrom file (our own format) which specifies exposure numbers,
+    identified point sources, their x, y location, and much more.
+    """
+
+    def parse(self, filename):
+        pass
