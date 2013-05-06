602767b47023533c0abef11710d99dd384e65149
drusk <drusk@uvic.ca>
2013-05-06 12:03:12 -0700

    Added test case for retrieving basic observation details like exposure number, ccd number

--- src/mopgui/test/test_mopgui/test_parsing/test_parsing.py
+++ src/mopgui/test/test_mopgui/test_parsing/test_parsing.py
@@ -23,6 +23,23 @@ class ParserTest(base_tests.FileReadingTestCase):
         assert_that(self.astrom_data.observations[2].rawname,
                     equal_to("1584453p15"))
 
+    def test_parse_observation_basic_details(self):
+        obs0 = self.astrom_data.observations[0]
+        obs1 = self.astrom_data.observations[1]
+        obs2 = self.astrom_data.observations[2]
+
+        assert_that(obs0.expnum, equal_to("1584431"))
+        assert_that(obs0.ftype, equal_to("p"))
+        assert_that(obs0.ccdnum, equal_to("15"))
+
+        assert_that(obs1.expnum, equal_to("1584449"))
+        assert_that(obs1.ftype, equal_to("p"))
+        assert_that(obs1.ccdnum, equal_to("15"))
+
+        assert_that(obs2.expnum, equal_to("1584453"))
+        assert_that(obs2.ftype, equal_to("p"))
+        assert_that(obs2.ccdnum, equal_to("15"))
+
 
 if __name__ == '__main__':
     unittest.main()
