d7a1ec4c525e8513654fc4b3cdc3988c88e47c72
drusk <drusk@uvic.ca>
2013-05-06 11:58:50 -0700

    Test case refactoring; parse file in setUp since there is only one test file at the moment.

--- src/mopgui/test/test_mopgui/test_parsing/test_parsing.py
+++ src/mopgui/test/test_mopgui/test_parsing/test_parsing.py
@@ -10,14 +10,18 @@ class ParserTest(base_tests.FileReadingTestCase):
     def setUp(self):
         self.parser = AstromParser()
 
-    def test_parse_observation_rawnames(self):
+        # The main test file
         filename = self.get_abs_path("data/1584431p15.measure3.cands.astrom")
-        astrom_data = self.parser.parse(filename)
+        self.astrom_data = self.parser.parse(filename)
 
-        assert_that(astrom_data.observations, has_length(3))
-        assert_that(astrom_data.observations[0].rawname, equal_to("1584431p15"))
-        assert_that(astrom_data.observations[1].rawname, equal_to("1584449p15"))
-        assert_that(astrom_data.observations[2].rawname, equal_to("1584453p15"))
+    def test_parse_observation_rawnames(self):
+        assert_that(self.astrom_data.observations, has_length(3))
+        assert_that(self.astrom_data.observations[0].rawname,
+                    equal_to("1584431p15"))
+        assert_that(self.astrom_data.observations[1].rawname,
+                    equal_to("1584449p15"))
+        assert_that(self.astrom_data.observations[2].rawname,
+                    equal_to("1584453p15"))
 
 
 if __name__ == '__main__':
