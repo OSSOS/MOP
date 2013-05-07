e4457bffd4fa23f8cb63d3286509a1067a32c694
drusk <drusk@uvic.ca>
2013-05-07 11:41:54 -0700

    Added basic module for resolving observations to VOSpace URIs simply based on file location conventions.

---
+++ src/mopgui/mopgui/data_retrieval/resolver.py
@@ -0,0 +1,15 @@
+"""
+Resolves observation descriptions to their URIs.
+"""
+
+
+class VOSpaceResolver(object):
+    def __init__(self):
+        # TODO extract root location to an application-wide config file
+        self.dataset_root = "vos://cadc.nrc.ca~vospace/OSSOS/dbimages"
+
+    def resolve_uri(self, observation):
+        # XXX can there be other file extensions?  For example, fits.fz?
+        # Do we need to search the vospace directory and choose based on that?
+        return "%s/%s/%s.fits" % (self.dataset_root, observation.expnum,
+                                  observation.rawname)
