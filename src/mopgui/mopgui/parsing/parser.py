fe1148ce824fcf59185d8583aa8b96caddc1f4fd
drusk <drusk@uvic.ca>
2013-05-07 09:11:08 -0700

    Can now parse observation headers using regex.

--- src/mopgui/mopgui/parsing/parser.py
+++ src/mopgui/mopgui/parsing/parser.py
@@ -14,6 +14,8 @@ class AstromParser(object):
     def __init__(self):
         self.obs_regex = re.compile("#\s+(?P<rawname>(?P<expnum>\d{7})"
                                     "(?P<ftype>[ops])(?P<ccdnum>\d+))")
+        self.obs_header_regex = re.compile(
+            """##\s+MOPversion\s+#\s+(?P<MOPversion>\d+\.[\d\w]+)\s+##\s+MJD-OBS-CENTER\s+EXPTIME\s+THRES\s+FWHM\s+MAXCOUNT\s+CRVAL1\s+CRVAL2\s+EXPNUM\s+#\s+(?P<MJD_OBS_CENTER>\d{4} \d{2} \d+\.\d+)\s+(?P<EXPTIME>\d+\.\d+)\s+(?P<THRES>\d+\.\d+)\s+(?P<FWHM>\d+\.\d+)\s+(?P<MAXCOUNT>\d+\.\d+)\s+(?P<CRVAL1>\d+\.\d+)\s+(?P<CRVAL2>\d+\.\d+)\s+(?P<EXPNUM>\d+)\s+##\s+SCALE\s+CHIP\s+CRPIX1\s+CRPIX2\s+NAX1\s+NAX2\s+DETECTOR\s+PHADU\s+RDNOIS\s+#\s+(?P<SCALE>\d+\.\d+)\s+(?P<CHIP>\d+)\s+(?P<CRPIX1>-?\d+\.\d+)\s+(?P<CRPIX2>-?\d+\.\d+)\s+(?P<NAX1>\d+)\s+(?P<NAX2>\d+)\s+(?P<DETECTOR>\w+)\s+(?P<PHADU>\d+\.\d+)\s+(?P<RDNOIS>\d+\.\d+)""")
 
     def parse(self, filename):
         with open(filename, "rb") as filehandle:
@@ -21,9 +23,21 @@ class AstromParser(object):
 
         assert filestr is not None
 
+        # Parse observation list
         matches = self.obs_regex.findall(filestr) # returns list of tuples
-
         observations = [Observation(*match) for match in matches]
+
+        # Parse observation headers
+        obsnum = 0
+        for match in self.obs_header_regex.finditer(filestr):
+            obs = observations[obsnum]
+            for header_key, header_val in match.groupdict().iteritems():
+                obs.header[header_key] = header_val
+            obsnum += 1
+
+        assert obsnum == len(observations), ("Number of observations headers "
+                                             "parsed doesn't match length of "
+                                             "observation list")
 
         return AstromData(observations)
 
@@ -50,3 +64,4 @@ class Observation(object):
         self.ftype = ftype
         self.ccdnum = ccdnum
 
+        self.header = {}
--- src/mopgui/test/test_mopgui/test_parsing/test_parsing.py
+++ src/mopgui/test/test_mopgui/test_parsing/test_parsing.py
@@ -1,6 +1,6 @@
 import unittest
 
-from hamcrest import assert_that, equal_to, has_length
+from hamcrest import assert_that, equal_to, has_length, has_entries
 
 from test import base_tests
 from mopgui.parsing.parser import AstromParser
@@ -39,6 +39,36 @@ class ParserTest(base_tests.FileReadingTestCase):
         assert_that(obs2.expnum, equal_to("1584453"))
         assert_that(obs2.ftype, equal_to("p"))
         assert_that(obs2.ccdnum, equal_to("15"))
+
+    def test_parse_observation_headers(self):
+        obs0 = self.astrom_data.observations[0]
+        obs1 = self.astrom_data.observations[1]
+        obs2 = self.astrom_data.observations[2]
+
+        assert_that(obs0.header, has_length(18))
+        assert_that(obs0.header, has_entries(
+            {"MOPversion": "1.20",
+             "MJD_OBS_CENTER": "2012 10 21.40516",
+             "EXPTIME": "320.14",
+             "THRES": "2.70",
+             "FWHM": "2.90",
+             "MAXCOUNT": "30000.0",
+             "CRVAL1": "26.92871",
+             "CRVAL2": "29.01125",
+             "EXPNUM": "1584431",
+             "SCALE": "0.185",
+             "CHIP": "16",
+             "CRPIX1": "-3227.00",
+             "CRPIX2": "-75.96",
+             "NAX1": "2112",
+             "NAX2": "4644",
+             "DETECTOR": "MegaPrime",
+             "PHADU": "1.60",
+             "RDNOIS": "3.00"}
+        ))
+
+        assert_that(obs1.header, has_length(18))
+        assert_that(obs2.header, has_length(18))
 
 
 if __name__ == '__main__':
