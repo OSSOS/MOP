0aa8a86da89ae521cdeeca87687293ce8519c050
drusk <drusk@uvic.ca>
2013-05-07 12:58:48 -0700

    Added skeleton of main application script with basic argument parsing.

---
+++ src/mopgui/app.py
@@ -0,0 +1,25 @@
+"""
+Main module for running the MOP graphical user interface.
+"""
+
+import argparse
+
+
+def run(astrom_file, debug_mode):
+    print "astrom_file: %s" % astrom_file
+    print "debug_mode: %s" % debug_mode
+
+    # TODO: actually run the application
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("astrom_file",
+                        help="the .astrom file specifying the images to be analyzed")
+    parser.add_argument("--debug", action="store_true", default=False,
+                        help="start the system in debug mode")
+
+    args = parser.parse_args()
+
+    run(args.astrom_file, args.debug)
