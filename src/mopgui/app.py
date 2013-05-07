867e1d1a5b95e6cf449c9f25a666e0274b8c9a02
drusk <drusk@uvic.ca>
2013-05-07 14:34:52 -0700

    Added view package with very ApplicationView that gets launched from app.py.  The view is just an empty wxpython frame.

--- src/mopgui/app.py
+++ src/mopgui/app.py
@@ -4,12 +4,15 @@ Main module for running the MOP graphical user interface.
 
 import argparse
 
+from mopgui.view import appview
+
 
 def run(astrom_file, debug_mode):
     print "astrom_file: %s" % astrom_file
     print "debug_mode: %s" % debug_mode
 
     # TODO: actually run the application
+    appview.ApplicationView().launch()
 
 
 if __name__ == "__main__":
