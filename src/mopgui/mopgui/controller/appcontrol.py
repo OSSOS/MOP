495465ab2413f579db0a47f9470212b7cb652d22
drusk <drusk@uvic.ca>
2013-05-08 15:17:50 -0700

    ApplicationView is now passed a reference to the image viewer, so is able to start up ds9 with the image for the first observation of the first source.

--- src/mopgui/mopgui/controller/appcontrol.py
+++ src/mopgui/mopgui/controller/appcontrol.py
@@ -6,19 +6,25 @@ from mopgui.parsing.parser import AstromParser
 from mopgui.data_retrieval.resolver import VOSpaceResolver
 from mopgui.data_retrieval.image_retriever import ImageSliceRetriever
 from mopgui.view.appview import ApplicationView
+from mopgui.view.imageview import DS9ImageViewer
 
 
 class ApplicationController(object):
+    """
+    Controls and coordinates the various components of the application.
+    """
+
     def __init__(self):
         self.parser = AstromParser()
         self.resolver = VOSpaceResolver()
         self.image_retriever = ImageSliceRetriever()
+        self.image_viewer = DS9ImageViewer()
 
     def run(self, astrom_file, debug_mode):
-        astrom_data = self.parser.parse(astrom_file)
+        self.astrom_data = self.parser.parse(astrom_file)
 
-        # TODO load all image slices up front for now
-        for source in astrom_data.sources:
+        # Load all image slices up front for now
+        for source in self.astrom_data.sources:
             for reading in source:
                 image_uri = self.resolver.resolve_uri(reading.obs)
                 image = self.image_retriever.retrieve_image(image_uri, reading)
@@ -27,5 +33,7 @@ class ApplicationController(object):
                     "No image retrieved for source reading %s" % reading
 
                 reading.image = image
+                print "Read image"
 
-        self.view = ApplicationView(astrom_data).launch(debug_mode)
+        self.view = ApplicationView(self.astrom_data,
+                                    self.image_viewer).launch(debug_mode)
