27a9ba2ddf1383d80942995dc03a6575876ac612
drusk <drusk@uvic.ca>
2013-05-10 14:59:10 -0700

    Made AstroDataModel.next_source events observable using wxPython's pubsub module.

--- src/mopgui/mopgui/model/astrodata.py
+++ src/mopgui/mopgui/model/astrodata.py
@@ -3,6 +3,12 @@ Provides interfaces to the application data which can be manipulated by the
 user interface.
 """
 
+from wx.lib.pubsub import Publisher as pub
+
+# Pub/Sub ids
+ASTRODATA_MSG_ROOT = ("astrodataroot", )
+ASTRODATA_MSG_NEXT_SRC = ASTRODATA_MSG_ROOT + ("nextsrc",)
+
 
 class AstroDataModel(object):
     """
@@ -23,6 +29,7 @@ class AstroDataModel(object):
 
     def next_source(self):
         self._current_source_number = (self._current_source_number + 1) % self.get_source_count()
+        pub.sendMessage(ASTRODATA_MSG_NEXT_SRC, data=self._current_source_number)
 
     def previous_source(self):
         self._current_source_number = (self._current_source_number - 1) % self.get_source_count()
