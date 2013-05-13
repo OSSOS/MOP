5e68aa87905dc6b20f43d0b8211722cd3779189d
drusk <drusk@uvic.ca>
2013-05-13 13:49:20 -0700

    Extracted created of mocked model with default values from test_appview and pulled it up into WxWidgetTestCase.

--- src/mopgui/test/base_tests.py
+++ src/mopgui/test/base_tests.py
@@ -6,6 +6,7 @@ import os
 import unittest
 
 import wx
+from mock import Mock
 
 
 class FileReadingTestCase(unittest.TestCase):
@@ -45,3 +46,12 @@ class WxWidgetTestCase(unittest.TestCase):
         event = wx.CommandEvent(wx.wxEVT_COMMAND_BUTTON_CLICKED,
                                 widget.GetId())
         widget.GetEventHandler().ProcessEvent(event)
+
+    def mock_model(self):
+        self.model = Mock()
+        self.model.get_reading_data.return_value = (("TEST1", 1), ("TEST2", 2))
+        self.model.get_header_data_list.return_value = [("Key1", "Val1"), ("Key2", "Val2")]
+        self.model.get_current_image_source_point.return_value = (0, 0)
+        self.model.get_current_image_FWHM.return_value = 3
+        self.model.get_current_source_number.return_value = 0
+        self.model.get_source_count.return_value = 2
--- src/mopgui/test/test_mopgui/test_view/test_appview.py
+++ src/mopgui/test/test_mopgui/test_view/test_appview.py
@@ -10,13 +10,7 @@ from mopgui.controller.appcontrol import ApplicationController
 
 class ApplicationViewTest(WxWidgetTestCase):
     def setUp(self):
-        self.model = Mock()
-        self.model.get_reading_data.return_value = (("TEST1", 1), ("TEST2", 2))
-        self.model.get_header_data_list.return_value = [("Key1", "Val1"), ("Key2", "Val2")]
-        self.model.get_current_image_source_point.return_value = (0, 0)
-        self.model.get_current_image_FWHM.return_value = 3
-        self.model.get_current_source_number.return_value = 0
-        self.model.get_source_count.return_value = 2
+        self.mock_model()
 
         self.image_viewer = Mock()
         self.appcontroller = ApplicationController(self.model, self.image_viewer,
