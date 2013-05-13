1c06d93273dba14111e1de27b653d567abd58fc7
drusk <drusk@uvic.ca>
2013-05-13 13:44:20 -0700

    Added new WxWidgetTestCase base test case class and refactored test_navview to use it.

--- src/mopgui/test/base_tests.py
+++ src/mopgui/test/base_tests.py
@@ -5,6 +5,8 @@ Base classes for test cases.
 import os
 import unittest
 
+import wx
+
 
 class FileReadingTestCase(unittest.TestCase):
     def get_abs_path(self, relative_path):
@@ -24,3 +26,22 @@ class FileReadingTestCase(unittest.TestCase):
             relative_path = relative_path[1:]
         relative_path.replace("/", os.sep)
         return os.path.join(os.path.dirname(__file__), relative_path)
+
+
+class WxWidgetTestCase(unittest.TestCase):
+    def _get_by_label(self, label, iterable):
+        for child in iterable:
+            if child.GetLabel().lower() == label.lower():
+                return child
+        return None
+
+    def get_child_by_label(self, parent, label):
+        return self._get_by_label(label, parent.GetChildren())
+
+    def get_menuitem_by_label(self, menu, label):
+        return self._get_by_label(label, menu.GetMenuItems())
+
+    def fire_button_click_event(self, widget):
+        event = wx.CommandEvent(wx.wxEVT_COMMAND_BUTTON_CLICKED,
+                                widget.GetId())
+        widget.GetEventHandler().ProcessEvent(event)
