14ed50a275ce5a14856cc24dba6fbcfcfd0b13ac
drusk <drusk@uvic.ca>
2013-05-14 12:35:56 -0700

    Made ImageLoader class work asynchronously from the rest of the application while still loading the individual images serially.  Currently configured in the ApplicationLauncher to work in a synchronous manner still though.

--- src/mopgui/test/test_mopgui/test_data_retrieval/test_loader.py
+++ src/mopgui/test/test_mopgui/test_data_retrieval/test_loader.py
@@ -1,32 +1,48 @@
 import unittest
 
 from mock import Mock
-from hamcrest import assert_that, equal_to
 
-from mopgui.data_retrieval.loader import ProgressiveImageLoader
+from mopgui.data_retrieval.loader import AsynchronousImageLoader
 
 
-class LoaderTest(unittest.TestCase):
+class AsynchronousImageLoaderTest(unittest.TestCase):
     def setUp(self):
-        # Set up mocked data with 2 sources, each with 3 observations
-        self.astrom_data = Mock()
-        source = Mock()
-        reading = Mock()
-        reading.obs = Mock()
-        source = [reading] * 3
-        self.astrom_data.sources = [source] * 2
-
         self.resolver = Mock()
         self.image_retriever = Mock()
         self.image_retriever.retrieve_image.return_value = (Mock(), Mock())
 
-        self.loader = ProgressiveImageLoader(self.resolver, self.image_retriever)
+        self.loader = AsynchronousImageLoader(self.resolver, self.image_retriever)
 
-    def test_loading_callback(self):
-        callback = Mock()
-        self.loader.start_loading(self.astrom_data, callback)
+    def mock_astrom_data(self, sources, observations):
+        astrom_data = Mock()
 
-        assert_that(callback.call_count, equal_to(6))
+        reading = Mock()
+        reading.obs = Mock()
+        source = [reading] * observations
+        astrom_data.sources = [source] * sources
+
+        return astrom_data
+
+    def test_do_load(self):
+        sources = 3
+        observations = 2
+        astrom_data = self.mock_astrom_data(sources, observations)
+
+        resolved_uris = ["URI%d" % i for i in range(sources * observations)]
+        self.resolver.resolve_uri.side_effect = resolved_uris
+
+        self.loader.do_loading = Mock()
+
+        self.loader.start_loading(astrom_data)
+
+        self.loader.do_loading.assert_called_with([
+            ("URI0", astrom_data.sources[0][0]),
+            ("URI1", astrom_data.sources[0][1]),
+            ("URI2", astrom_data.sources[1][0]),
+            ("URI3", astrom_data.sources[1][1]),
+            ("URI4", astrom_data.sources[2][0]),
+            ("URI5", astrom_data.sources[2][1]),
+        ])
 
 
 if __name__ == '__main__':
