e8b25ba7f2910145ff818a0de7ac0c42cbee5002
drusk <drusk@uvic.ca>
2013-05-14 09:01:34 -0700

    Refactoring: extracted AbstractKeyValueListPanel to remove duplication in the current data views.

--- src/mopgui/mopgui/view/dataview.py
+++ src/mopgui/mopgui/view/dataview.py
@@ -4,43 +4,43 @@ from mopgui.model import astrodata
 from mopgui.view.list_ctrl import ListCtrlPanel
 
 
-class ReadingDataView(ListCtrlPanel):
-    """
-    XXX duplication with ObservationHeaderView
-    """
+class AbstractKeyValueListPanel(ListCtrlPanel):
+    def __init__(self, parent):
+        super(AbstractKeyValueListPanel, self).__init__(parent, ("Key", "Value"))
 
-    def __init__(self, parent, model):
-        super(ReadingDataView, self).__init__(parent, ("Key", "Value"))
-
-        self.model = model
-
-        pub.subscribe(self.on_change_reading, astrodata.MSG_NAV)
+        pub.subscribe(self.on_change_data, astrodata.MSG_NAV)
 
         self.display_data()
+
+    def get_data(self):
+        """
+        Returns a collection of (key, value) tuples to be displayed in a
+        list view.
+        """
+        raise NotImplementedError("This method must be overriden by a subclass")
 
     def display_data(self):
-        self.populate_list(self.model.get_reading_data())
+        self.populate_list(self.get_data())
 
-    def on_change_reading(self, event):
+    def on_change_data(self, event):
         self.display_data()
 
 
-class ObservationHeaderView(ListCtrlPanel):
-    """
-    XXX duplication with ReadingDataView
-    """
-
+class ReadingDataView(AbstractKeyValueListPanel):
     def __init__(self, parent, model):
-        super(ObservationHeaderView, self).__init__(parent, ("Key", "Value"))
-
         self.model = model
 
-        pub.subscribe(self.on_change_obs, astrodata.MSG_NAV)
+        super(ReadingDataView, self).__init__(parent)
 
-        self.display_data()
+    def get_data(self):
+        return self.model.get_reading_data()
 
-    def display_data(self):
-        self.populate_list(self.model.get_header_data_list())
 
-    def on_change_obs(self, event):
-        self.display_data()
+class ObservationHeaderView(AbstractKeyValueListPanel):
+    def __init__(self, parent, model):
+        self.model = model
+
+        super(ObservationHeaderView, self).__init__(parent)
+
+    def get_data(self):
+        return self.model.get_header_data_list()
