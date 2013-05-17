cf6fc22a0331cabef2644a73595e8b9c584c6071
drusk <drusk@uvic.ca>
2013-05-17 11:10:03 -0700

    Completed a ProvisionalNameGenerator based on taking the base26 encoding of the exposure number concatenated with a counter.

--- src/mopgui/mopgui/io/output.py
+++ src/mopgui/mopgui/io/output.py
@@ -1,5 +1,8 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
+import collections
+import re
+
 
 def to_base26(number):
     if number < 0:
@@ -27,6 +30,10 @@ class ProvisionalNameGenerator(object):
     http://www.minorplanetcenter.net/iau/info/PackedDes.html
     """
 
+    def __init__(self):
+        self.valid_expnum_reg = re.compile("^\d{7}$")
+        self._processed_expnums = collections.defaultdict(int)
+
     def generate_name(self, exposure_number):
         """
         Generates a name from an exposure number.  In order to compress it
@@ -36,10 +43,33 @@ class ProvisionalNameGenerator(object):
         """
         exp_str = str(exposure_number)
 
-        if len(exp_str) != 7:
-            raise ValueError("Expected 7 digit exposure number but was: %d" % exposure_number)
+        if not self.valid_expnum_reg.match(exp_str):
+            raise ValueError(
+                "Exposure number (%s) does not meet required format (%s)" % (
+                    exp_str, self.valid_expnum_reg.pattern))
 
-        # TODO
+        exposure_number = int(exposure_number)
+
+        encoded_expnum = to_base26(exposure_number)
+
+        return encoded_expnum + self._get_obsnum_str(exposure_number)
+
+    def _get_obsnum_str(self, expnum):
+        """
+        Get the suffix part of the name that gets incremented if we need
+        to generate a name for the same exposure number multiple times.
+        """
+        obsnum = self._processed_expnums[expnum]
+        obsnum_str = "%02d" % obsnum
+
+        if len(obsnum_str) > 2:
+            raise ValueError("Observation number for specified exposure "
+                             "number (%d) has length greater than 2: %d" % (
+                                 expnum, obsnum))
+
+        self._processed_expnums[expnum] += 1
+
+        return obsnum_str
 
 
 class AcceptRejectResultsWriter(object):
