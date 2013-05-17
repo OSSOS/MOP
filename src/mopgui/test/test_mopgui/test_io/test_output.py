479678f6ae5b9f1720d7224434954c2d2807c3ee
drusk <drusk@uvic.ca>
2013-05-17 11:10:03 -0700

    Completed a ProvisionalNameGenerator based on taking the base26 encoding of the exposure number concatenated with a counter.

--- src/mopgui/test/test_mopgui/test_io/test_output.py
+++ src/mopgui/test/test_mopgui/test_io/test_output.py
@@ -4,23 +4,59 @@ import unittest
 
 from hamcrest import assert_that, has_length, less_than_or_equal_to, equal_to
 
-from mopgui.io.output import ProvisionalNameGenerator
+from mopgui.io.output import ProvisionalNameGenerator, to_base26
 
 
 class ProvisionalNameGeneratorTest(unittest.TestCase):
+    def setUp(self):
+        self.undertest = ProvisionalNameGenerator()
 
     def assert_valid_length(self, name):
         assert_that(name, has_length(less_than_or_equal_to(7)))
 
-    def test_generate_name(self):
-        undertest = ProvisionalNameGenerator()
+    def test_exposure_number_not_7_digits(self):
+        self.assertRaises(ValueError, self.undertest.generate_name, 123456)
+        self.assertRaises(ValueError, self.undertest.generate_name, 12345678)
+        self.assertRaises(ValueError, self.undertest.generate_name, 0)
+        self.assertRaises(ValueError, self.undertest.generate_name, None)
+        self.assertRaises(ValueError, self.undertest.generate_name, "abcdefg")
 
-        name = undertest.generate_name("1616703")
-
+    def test_generate_names_from_str_no_repeat_exp1(self):
+        name = self.undertest.generate_name("1616703")
         self.assert_valid_length(name)
+        assert_that(name, equal_to("DNZOX00"))
 
-        # TODO
-        assert_that(name, equal_to(""))
+    def test_generate_names_from_str_no_repeat_exp2(self):
+        name = self.undertest.generate_name("1616704")
+        self.assert_valid_length(name)
+        assert_that(name, equal_to("DNZOY00"))
+
+    def test_generate_names_from_int_no_repeat_exp1(self):
+        name = self.undertest.generate_name(1616703)
+        self.assert_valid_length(name)
+        assert_that(name, equal_to("DNZOX00"))
+
+    def test_generate_names_from_int_no_repeat_exp2(self):
+        name = self.undertest.generate_name(1616704)
+        self.assert_valid_length(name)
+        assert_that(name, equal_to("DNZOY00"))
+
+    def test_generate_names_repeat_exp(self):
+        name = self.undertest.generate_name(1616703)
+        self.assert_valid_length(name)
+        assert_that(name, equal_to("DNZOX00"))
+
+        name2 = self.undertest.generate_name(1616703)
+        self.assert_valid_length(name2)
+        assert_that(name2, equal_to("DNZOX01"))
+
+        name3 = self.undertest.generate_name(1616703)
+        self.assert_valid_length(name3)
+        assert_that(name3, equal_to("DNZOX02"))
+
+        name4 = self.undertest.generate_name(1616704)
+        self.assert_valid_length(name4)
+        assert_that(name4, equal_to("DNZOY00"))
 
 
 class Base26ConverterTest(unittest.TestCase):
