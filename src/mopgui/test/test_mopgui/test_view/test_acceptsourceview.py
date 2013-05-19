883dfc4610db3332a6eb38811bd7bb4449a23492
drusk <drusk@uvic.ca>
2013-05-19 16:23:41 -0700

    Added note1 and note2 combo-boxes to accept source dialog.

--- src/mopgui/test/test_mopgui/test_view/test_acceptsourceview.py
+++ src/mopgui/test/test_mopgui/test_view/test_acceptsourceview.py
@@ -24,16 +24,37 @@ class AcceptSourceDialogTest(WxWidgetTestCase):
         undertest = AcceptSourceDialog(self.rootframe, TEST_PROVISIONAL_NAME)
 
         component_labels = [AcceptSourceDialog.MINOR_PLANET_NUMBER, AcceptSourceDialog.PROVISIONAL_NAME,
-                            AcceptSourceDialog.DISCOVERY_ASTERISK]
+                            AcceptSourceDialog.DISCOVERY_ASTERISK, AcceptSourceDialog.NOTE1,
+                            AcceptSourceDialog.NOTE2]
 
         for label in component_labels:
             self.assert_has_child_with_label(undertest, label)
 
-    def test_preset_values(self):
+    def test_required_preset_values(self):
         undertest = AcceptSourceDialog(self.rootframe, TEST_PROVISIONAL_NAME)
 
         assert_that(undertest.GetTitle(), equal_to(AcceptSourceDialog.TITLE))
         self.assert_has_child_with_label(undertest, TEST_PROVISIONAL_NAME)
+
+        note1_combobox = self.get_child_by_label(undertest, AcceptSourceDialog.NOTE1)
+        assert_that(note1_combobox)
+
+    def test_note_comboboxes_populated(self):
+        note1_choices = ["n1a", "n1b"]
+        note2_choices = ["n2a", "n2b", "n2c"]
+        undertest = AcceptSourceDialog(self.rootframe, TEST_PROVISIONAL_NAME,
+                                       note1_choices=note1_choices,
+                                       note2_choices=note2_choices)
+
+        note1_combobox = self.get_child_by_name(undertest, AcceptSourceDialog.NOTE1)
+        assert_that(note1_combobox.GetValue(), equal_to(""))
+        assert_that(not note1_combobox.IsEditable())
+        assert_that(note1_combobox.GetCount(), equal_to(len(note1_choices)))
+
+        note2_combobox = self.get_child_by_name(undertest, AcceptSourceDialog.NOTE2)
+        assert_that(note2_combobox.GetValue(), equal_to(""))
+        assert_that(not note2_combobox.IsEditable())
+        assert_that(note2_combobox.GetCount(), equal_to(len(note2_choices)))
 
 
 if __name__ == '__main__':
