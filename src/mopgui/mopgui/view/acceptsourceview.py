8742ac1862e4afc48b0125584024692305516048
drusk <drusk@uvic.ca>
2013-05-20 15:49:57 -0700

    The accept source dialog's ok and cancel buttons are now bound and delegate to a validation controller after pulling out data from the form.

--- src/mopgui/mopgui/view/acceptsourceview.py
+++ src/mopgui/mopgui/view/acceptsourceview.py
@@ -19,10 +19,11 @@ class AcceptSourceDialog(wx.Dialog):
     OK_BTN = "Ok"
     CANCEL_BTN = "Cancel"
 
-    def __init__(self, parent, provisional_name, date_of_obs, ra, dec,
+    def __init__(self, parent, controller, provisional_name, date_of_obs, ra, dec,
                  note1_choices=None, note2_choices=None):
         super(AcceptSourceDialog, self).__init__(parent, title=self.TITLE)
 
+        self.controller = controller
         self.provisional_name = provisional_name
         self.date_of_obs = date_of_obs
         self.ra_str = str(ra)
@@ -32,16 +33,17 @@ class AcceptSourceDialog(wx.Dialog):
         self.note2_choices = note2_choices if note2_choices is not None else []
 
         self._init_ui()
+        self._bind_events()
 
     def _init_ui(self):
         self.minor_planet_num_label = wx.StaticText(self, label=self.MINOR_PLANET_NUMBER)
-        self.minor_planet_num_text = wx.TextCtrl(self)
+        self.minor_planet_num_text = wx.TextCtrl(self, name=self.MINOR_PLANET_NUMBER)
 
         self.provisional_name_label = wx.StaticText(self, label=self.PROVISIONAL_NAME)
-        self.provision_name_text = wx.StaticText(self, label=self.provisional_name)
+        self.provision_name_text = wx.StaticText(self, label=self.provisional_name, name=self.PROVISIONAL_NAME)
 
         self.discovery_asterisk_cb = wx.CheckBox(self, label=self.DISCOVERY_ASTERISK,
-                                                 style=wx.ALIGN_RIGHT)
+                                                 style=wx.ALIGN_RIGHT, name=self.DISCOVERY_ASTERISK)
 
         self.note1_label = wx.StaticText(self, label=self.NOTE1)
         self.note1_combobox = wx.ComboBox(self, choices=self.note1_choices, style=wx.CB_READONLY,
@@ -52,22 +54,22 @@ class AcceptSourceDialog(wx.Dialog):
                                           name=self.NOTE2)
 
         self.date_of_obs_label = wx.StaticText(self, label=self.DATE_OF_OBS)
-        self.date_of_obs_text = wx.StaticText(self, label=self.date_of_obs)
+        self.date_of_obs_text = wx.StaticText(self, label=self.date_of_obs, name=self.DATE_OF_OBS)
 
         self.ra_label = wx.StaticText(self, label=self.RA)
-        self.ra_text = wx.StaticText(self, label=self.ra_str)
+        self.ra_text = wx.StaticText(self, label=self.ra_str, name=self.RA)
 
         self.dec_label = wx.StaticText(self, label=self.DEC)
-        self.dec_text = wx.StaticText(self, label=self.dec_str)
+        self.dec_text = wx.StaticText(self, label=self.dec_str, name=self.DEC)
 
         self.obs_mag_label = wx.StaticText(self, label=self.OBS_MAG)
-        self.obs_mag_text = wx.TextCtrl(self)
+        self.obs_mag_text = wx.TextCtrl(self, name=self.OBS_MAG)
 
         self.band_label = wx.StaticText(self, label=self.BAND)
-        self.band_text = wx.TextCtrl(self)
+        self.band_text = wx.TextCtrl(self, name=self.BAND)
 
         self.observatory_code_label = wx.StaticText(self, label=self.OBSERVATORY_CODE)
-        self.observatory_code_text = wx.TextCtrl(self)
+        self.observatory_code_text = wx.TextCtrl(self, name=self.OBSERVATORY_CODE)
 
         self.ok_button = wx.Button(self, label=self.OK_BTN, name=self.OK_BTN)
         self.cancel_button = wx.Button(self, label=self.CANCEL_BTN, name=self.CANCEL_BTN)
@@ -111,3 +113,34 @@ class AcceptSourceDialog(wx.Dialog):
         hsizer.Add(widget1, flag=flag, border=border)
         hsizer.Add(widget2, flag=flag, border=border)
         return hsizer
+
+    def _bind_events(self):
+        self.ok_button.Bind(wx.EVT_BUTTON, self._on_ok)
+        self.cancel_button.Bind(wx.EVT_BUTTON, self._on_cancel)
+
+    def _on_ok(self, event):
+        # Grab data out of the form
+        # TODO validation
+        minor_planet_number = self.minor_planet_num_text.GetValue()
+        discover_asterisk = "*" if self.discovery_asterisk_cb.IsChecked() else " "
+        note1 = self.note1_combobox.GetStringSelection()
+        note2 = self.note2_combobox.GetStringSelection()
+        obs_mag = self.obs_mag_text.GetValue()
+        band = self.band_text.GetValue()
+        observatory_code = self.observatory_code_text.GetValue()
+
+        self.controller.on_do_accept(minor_planet_number,
+                                     self.provisional_name,
+                                     discover_asterisk,
+                                     note1,
+                                     note2,
+                                     self.date_of_obs,
+                                     self.ra_str,
+                                     self.dec_str,
+                                     obs_mag,
+                                     band,
+                                     observatory_code
+        )
+
+    def _on_cancel(self, event):
+        self.controller.on_cancel_accept()
