fc749bdcc8031a892751e86915fe5df910cb4fcf
drusk <drusk@uvic.ca>
2013-05-20 21:33:46 -0700

    Quick hookup of accept source dialog and its controller to the rest of the application, including using MPCWriter.  Using static dummy data in some places and test cases/refactoring still TODO.

--- src/mopgui/mopgui/controller/validationcontrol.py
+++ src/mopgui/mopgui/controller/validationcontrol.py
@@ -1,5 +1,13 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
+from wx.lib.pubsub import Publisher as pub
+
+# Pub/Sub ids
+MSG_ROOT = ("valctrlroot", )
+MSG_INITIATE_ACCEPT = MSG_ROOT + ("initaccept", )
+MSG_DO_ACCEPT = MSG_ROOT + ("doaccept", )
+MSG_CANCEL_ACCEPT = MSG_ROOT + ("cancelaccept", )
+
 
 class SourceValidationController(object):
     def __init__(self, model, output_writer, name_generator):
@@ -13,12 +21,18 @@ class SourceValidationController(object):
 
     def on_initiate_accept(self, event):
         """Initiates acceptance procedure, gathering required data."""
-        self.output_writer.write_result(self._get_provisional_name(),
-                                        "accepted")
+        preset_vals = (
+            self._get_provisional_name(),
+            "",  # TODO pull from model
+            1.2,  # TODO pull from model
+            3.4,  # TODO pull from model
+            ["A", "B"], # TODO read from config file
+            ["C", "D", "E"]  # TODO read from config file
+        )
+        pub.sendMessage(MSG_INITIATE_ACCEPT, data=preset_vals)
 
     def on_reject(self, event):
-        self.output_writer.write_result(self._get_provisional_name(),
-                                        "rejected")
+        self.model.next_source()
 
     def on_do_accept(self,
                      minor_plant_number,
@@ -46,6 +60,8 @@ class SourceValidationController(object):
             band,
             observatory_code)
 
-    def on_cancel_accept(self):
-        pass
+        pub.sendMessage(MSG_DO_ACCEPT)
+        self.model.next_source()
 
+    def on_cancel_accept(self):
+        pub.sendMessage(MSG_CANCEL_ACCEPT)
--- src/mopgui/mopgui/view/appview.py
+++ src/mopgui/mopgui/view/appview.py
@@ -2,9 +2,12 @@ __author__ = "David Rusk <drusk@uvic.ca>"
 
 import wx
 import wx.lib.inspection
+from wx.lib.pubsub import Publisher as pub
 
 from mopgui.view import wxutil
 from mopgui.view.mainview import MainFrame
+from mopgui.view.acceptsourceview import AcceptSourceDialog
+from mopgui.controller import validationcontrol
 
 
 class ApplicationView(object):
@@ -22,6 +25,12 @@ class ApplicationView(object):
         self.wx_app = wx.App(False)
         self.mainframe = MainFrame(model, appcontroller, validationcontroller,
                                    navcontroller)
+        self.accept_source_dialog = None
+
+        # TODO refactor
+        pub.subscribe(self.show_accept_source_dialog, validationcontrol.MSG_INITIATE_ACCEPT)
+        pub.subscribe(self.close_accept_source_dialog, validationcontrol.MSG_DO_ACCEPT)
+        pub.subscribe(self.close_accept_source_dialog, validationcontrol.MSG_CANCEL_ACCEPT)
 
     def launch(self, debug_mode=False, unittest=False):
         wx.CallAfter(self.mainframe.show_image_loading_dialog)
@@ -48,6 +57,15 @@ class ApplicationView(object):
     @wxutil.guithread
     def set_source_status(self, current_source, total_sources):
         self.mainframe.set_source_status(current_source, total_sources)
+
+    def show_accept_source_dialog(self, event):
+        self.accept_source_dialog = AcceptSourceDialog(
+            self.mainframe, self.validationcontroller, *event.data)
+        self.accept_source_dialog.ShowModal()
+
+    def close_accept_source_dialog(self, event):
+        if self.accept_source_dialog is not None:
+            self.accept_source_dialog.Destroy()
 
     def as_widget(self):
         return self.mainframe
