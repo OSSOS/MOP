8119c32802fdc70bfb0f9707e6c0f0e6ea57c2ba
drusk <drusk@uvic.ca>
2013-05-23 11:30:51 -0700

    xy2sky implemented and tested against Fortran output.  Values are the same up to a delta of 0.000005

--- src/mopgui/mopgui/astrometry/wcs.py
+++ src/mopgui/mopgui/astrometry/wcs.py
@@ -1,5 +1,9 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
+import math
+
+PI180 = 57.2957795130823208767981548141052
+
 
 def get_order(pv):
     """
@@ -32,7 +36,7 @@ def get_order(pv):
                          "%d coefficients per row" % row_len)
 
 
-def xy2sky(x, y, crpix1, crpix2, cd, pv, crval1, crval2):
+def xy2sky(x, y, crpix1, crpix2, crval1, crval2, cd, pv, nord):
     """
     Transforms from pixel coordinates to celestial coordinates taking
     non-linear distortion into account with the World Coordinate System
@@ -43,11 +47,20 @@ def xy2sky(x, y, crpix1, crpix2, cd, pv, crval1, crval2):
     Args:
       x, y: int
         Input pixel coordinate
-      crpix1, crpix2: int
-        Linear offset of WCS
+      crpix1: float
+        Tangent point x, pixels
+      crpix2: float
+        Tangent point y, pixels
+      crval1: float
+        Tangent point RA, degrees
+      crval2:
+        Tangent point Dec, degrees
       cd: 2d array
         Expresses the scale, the rotation and any possible skew of the image
         with respect to the sky.
+      pv: 2d array
+      nord: int
+        order of the fit
 
     Returns:
       ra: float
@@ -58,7 +71,63 @@ def xy2sky(x, y, crpix1, crpix2, cd, pv, crval1, crval2):
     xp = x - crpix1
     yp = y - crpix2
 
-    x_deg = cd[1][1] * xp + cd[1][2] * yp
-    y_deg = cd[2][1] * xp + cd[2][2] * yp
+    print "xp=%.16e, yp=%.16e" % (xp, yp)
 
-    order = get_order(pv)
+    # IMPORTANT NOTE: 0-based indexing in Python means indexing for values
+    # in cd and pv will be shifted from in the paper.
+    x_deg = cd[0][0] * xp + cd[0][1] * yp
+    y_deg = cd[1][0] * xp + cd[1][1] * yp
+
+    if nord < 0:
+        xi = x
+        eta = y
+
+    if nord >= 0:
+        xi = pv[0][0]
+        eta = pv[1][0]
+
+    print ">=0: xi=%.16e, eta=%.16e" % (xi, eta)
+
+    if nord >= 1:
+        r = math.sqrt(x_deg ** 2 + y_deg ** 2)
+        xi += pv[0][1] * x_deg + pv[0][2] * y_deg + pv[0][3] * r
+        eta += pv[1][1] * y_deg + pv[1][2] * x_deg + pv[1][3] * r
+
+    if nord >= 2:
+        x2 = x_deg ** 2
+        xy = x_deg * y_deg
+        y2 = y_deg ** 2
+
+        xi += pv[0][4] * x2 + pv[0][5] * xy + pv[0][6] * y2
+        eta += pv[1][4] * y2 + pv[1][5] * xy + pv[1][6] * x2
+
+    if nord >= 3:
+        x3 = x_deg ** 3
+        x2y = x2 * y_deg
+        xy2 = x_deg * y2
+        y3 = y_deg ** 3
+
+        xi += pv[0][7] * x3 + pv[0][8] * x2y + pv[0][9] * xy2 + pv[0][10] * y3
+        eta += pv[1][7] * y3 + pv[1][8] * xy2 + pv[1][9] * x2y + pv[1][10] * x3
+
+    xir = float(xi) / PI180
+    etar = float(eta) / PI180
+
+    ra0 = crval1 / PI180
+    dec0 = crval2 / PI180
+
+    ctan = math.tan(dec0)
+    ccos = math.cos(dec0)
+    raoff = math.atan2(xir / ccos, 1 - etar * ctan)
+    ra = raoff + ra0
+    dec = math.atan(math.cos(raoff) / ((1 - (etar * ctan)) / (etar + ctan)))
+
+    ra *= PI180
+    if ra < 0:
+        ra += 360
+    if ra >= 360:
+        ra -= 360
+
+    dec *= PI180
+
+    return ra, dec
--- src/mopgui/test/test_mopgui/test_astrometry/test_wcs.py
+++ src/mopgui/test/test_mopgui/test_astrometry/test_wcs.py
@@ -2,9 +2,11 @@ __author__ = "David Rusk <drusk@uvic.ca>"
 
 import unittest
 
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, close_to
 
 from mopgui.astrometry import wcs
+
+DELTA = 0.000005
 
 
 class WCSTest(unittest.TestCase):
@@ -29,17 +31,29 @@ class WCSTest(unittest.TestCase):
         pv = [range(11), range(11)]
         assert_that(wcs.get_order(pv), equal_to(3))
 
-    def test_xy2sky_linear(self):
+    def test_xy2sky_nord3(self):
         x = 15000
         y = 20000
         crpix1 = -7535.57493517
         crpix2 = 9808.40914361
-        cd = [[5.115244026718E-05, 7.064503033578E-07],
-              [-1.280229655229E-07, -5.123112374523E-05]]
-        pv = [[-7.030338745606E-03, 1.01755337222, 8.262429361142E-03, 0.00000000000],
-              [-6.146513090656E-03, 1.01552885426, 8.259666421752E-03, 0.00000000000]]
         crval1 = 176.486157083
         crval2 = 8.03697351091
+        cd = [[5.115244026718E-05, 7.064503033578E-07],
+              [-1.280229655229E-07, -5.123112374523E-05]]
+        pv = [[-7.030338745606E-03, 1.01755337222, 8.262429361142E-03,
+               0.00000000000, -5.910145454849E-04, -7.494178330178E-04,
+               -3.470178516657E-04, -2.331150605755E-02, -8.187062772669E-06,
+               -2.325429510806E-02, 1.135299506292E-04],
+              [-6.146513090656E-03, 1.01552885426, 8.259666421752E-03,
+               0.00000000000, -4.567030382243E-04, -6.978676921999E-04,
+               -3.732572951216E-04, -2.332572754467E-02, -2.354317291723E-05,
+               -2.329623852891E-02, 1.196394469003E-04]]
+        nord = 3
+
+        ra, dec = wcs.xy2sky(x, y, crpix1, crpix2, crval1, crval2, cd, pv, nord)
+
+        assert_that(ra, close_to(177.62042274595882, DELTA))
+        assert_that(dec, close_to(7.5256071336988679, DELTA))
 
 
 if __name__ == '__main__':
