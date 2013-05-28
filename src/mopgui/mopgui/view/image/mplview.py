6089c6ddb5988c85820350eff852d9431030fd08
drusk <drusk@uvic.ca>
2013-05-28 09:41:31 -0700

    Got a basic example of rendering an image in a wxframe working.

--- src/mopgui/mopgui/view/image/mplview.py
+++ src/mopgui/mopgui/view/image/mplview.py
@@ -1,8 +1,7 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
 import wx
-import numpy as np
-from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_wxagg import \
     FigureCanvasWxAgg as FigureCanvas
 
@@ -13,21 +12,17 @@ class MPLImageViewer(object):
     """
 
     def __init__(self, parent):
-        # Create the actual mpl figure we will manipulate
-        self.figure = Figure()
-
-        self.axes = self.figure.add_subplot(111)
-        x = np.arange(0, 6, .01)
-        y = np.sin(x ** 2) * np.exp(-x)
-        self.axes.plot(x, y)
+        # Create the actual mpl figure we will draw on
+        self.figure = plt.figure()
 
         # Create the canvas on which the figure is rendered
         self.canvas = FigureCanvas(parent, wx.ID_ANY, self.figure)
 
     def view_image(self, hdulist):
-        pass
+        plt.imshow(hdulist[0].data)
 
     def draw_circle(self, x, y, radius):
+        # TODO
         pass
 
     def close(self):
