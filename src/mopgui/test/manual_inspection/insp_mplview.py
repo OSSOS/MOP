4f8f3845974e55d07f03efdd083cc855dd60f269
drusk <drusk@uvic.ca>
2013-05-28 09:41:31 -0700

    Got a basic example of rendering an image in a wxframe working.

--- src/mopgui/test/manual_inspection/insp_mplview.py
+++ src/mopgui/test/manual_inspection/insp_mplview.py
@@ -1,5 +1,10 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
+import collections
+
+from mock import Mock
+
+import numpy as np
 import wx
 
 from mopgui.view.image.mplview import MPLImageViewer
@@ -9,7 +14,11 @@ def main():
     app = wx.App()
     rootframe = wx.Frame(None)
 
-    MPLImageViewer(rootframe)
+    viewer = MPLImageViewer(rootframe)
+    hdu = Mock()
+    hdu.data = np.random.uniform(size=(512, 512))
+    hdulist = [hdu]
+    viewer.view_image(hdulist)
 
     rootframe.Show()
     app.MainLoop()
