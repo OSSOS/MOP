e5fa8dab04a66cc1f21c0dc235920ba2c1ebc160
drusk <drusk@uvic.ca>
2013-05-28 10:58:13 -0700

    Removed old result writer used for early experimentation which is no longer used.

--- src/mopgui/mopgui/io/writer.py
+++ src/mopgui/mopgui/io/writer.py
@@ -90,22 +90,3 @@ def format_ra_dec(ra_deg, dec_deg):
 
     return formatted_ra, formatted_dec
 
-
-class AcceptRejectResultsWriter(object):
-    """
-    A simplified output that just writes the source name and whether it
-    was accepted or rejected.  It just does formatting, it must be provided
-    with all required data.
-    """
-
-    def __init__(self, filehandle, name_generator):
-        # XXX name generator not used
-        self.name_generator = name_generator
-        self.filehandle = filehandle
-
-    def write_result(self, sourcename, status):
-        self.filehandle.write("%s: %s\n" % (sourcename, status))
-
-    def close(self):
-        self.filehandle.close()
-
