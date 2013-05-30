e3d1b577d9d26d385b67bec075505c33d73ada36
drusk <drusk@uvic.ca>
2013-05-30 11:04:07 -0700

    VOSpace resolver can now resolve apcor URIs in addition to image URIs for an observation.

--- src/pymop/test/test_pymop/test_io/test_resolver.py
+++ src/pymop/test/test_pymop/test_io/test_resolver.py
@@ -12,10 +12,23 @@ class ResolverTest(unittest.TestCase):
     def setUp(self):
         self.resolver = VOSpaceResolver()
 
-    def test_resolve_uri(self):
+    def test_resolve_image_uri(self):
         observation = Observation("1584431", "p", "15")
         expected_uri = "vos://cadc.nrc.ca~vospace/OSSOS/dbimages/1584431/1584431p.fits"
-        assert_that(self.resolver.resolve_uri(observation),
+        assert_that(self.resolver.resolve_image_uri(observation),
+                    equal_to(expected_uri))
+
+    def test_resolve_apcor_uri(self):
+        observation = Observation("1616681", "p", "22")
+        expected_uri = "vos://cadc.nrc.ca~vospace/OSSOS/dbimages/1616681/ccd22/1616681p22.apcor"
+        assert_that(self.resolver.resolve_apcor_uri(observation),
+                    equal_to(expected_uri))
+
+    def test_resolve_apcor_uri_single_digit_ccd(self):
+        """Just double checking we don't run into trouble with leading zeros"""
+        observation = Observation("1616681", "p", "05")
+        expected_uri = "vos://cadc.nrc.ca~vospace/OSSOS/dbimages/1616681/ccd05/1616681p05.apcor"
+        assert_that(self.resolver.resolve_apcor_uri(observation),
                     equal_to(expected_uri))
 
 
