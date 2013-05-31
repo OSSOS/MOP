296877d1615a3e6ea5918dabcbe8f9e1150cc0ee
drusk <drusk@uvic.ca>
2013-05-31 11:07:39 -0700

    Added zscaling for matplotlib image viewer.  Introduces dependency on stsci which is a meta-package which contains, among other things, numdisplay.

--- src/pymop/pymop/gui/view/image/mplview.py
+++ src/pymop/pymop/gui/view/image/mplview.py
@@ -4,6 +4,7 @@ import wx
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_wxagg import \
     FigureCanvasWxAgg as FigureCanvas
+from stsci import numdisplay
 
 
 class MPLImageViewer(object):
@@ -19,7 +20,7 @@ class MPLImageViewer(object):
         self.canvas = FigureCanvas(parent, wx.ID_ANY, self.figure)
 
     def view_image(self, hdulist):
-        plt.imshow(hdulist[0].data, cmap="gray")
+        plt.imshow(zscale(hdulist[0].data), cmap="gray")
 
     def draw_circle(self, x, y, radius):
         # TODO
@@ -28,3 +29,28 @@ class MPLImageViewer(object):
     def close(self):
         pass
 
+
+def zscale(img):
+    """
+    Performs the zscale operation on an image.
+
+    Args:
+      img: numpy.ndarray
+        The image to be scaled.
+
+    Returns:
+      scaled_img: numpy.ndarray
+        The input image after scaling.
+    """
+    # Using the default values, but listing explicitly
+    z1, z2 = numdisplay.zscale.zscale(img, nsamples=1000, contrast=0.25)
+    return normalize(img, z1, z2)
+
+
+def normalize(arr, lower, upper):
+    """
+    Normalizes a numpy array to values between the given lower and upper
+    bounds.
+    """
+    scaling_factor = float(upper - lower) / (arr.max() - arr.min())
+    return scaling_factor * arr + lower
