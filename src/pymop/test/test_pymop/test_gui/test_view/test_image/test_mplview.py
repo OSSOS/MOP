dc1bda04be3b38f76c59693795b0628d17e66e9b
drusk <drusk@uvic.ca>
2013-05-31 11:55:17 -0700

    MPLImageViewer can now perform basic circle drawing.

--- src/pymop/test/test_pymop/test_gui/test_view/test_image/test_mplview.py
+++ src/pymop/test/test_pymop/test_gui/test_view/test_image/test_mplview.py
@@ -1,14 +1,38 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
 import unittest
+import wx
 
 import numpy as np
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, has_length
 
 from pymop.gui.view.image import mplview
 
 
 class MPLViewTest(unittest.TestCase):
+    def setUp(self):
+        self.app = wx.App()
+        self.rootframe = wx.Frame(None)
+
+        self.viewer = mplview.MPLImageViewer(self.rootframe)
+
+    def test_draw_one_circle(self):
+        axes = self.viewer.axes
+
+        assert_that(axes.patches, has_length(0))
+        cx = 1
+        cy = 2
+        cr = 3
+        self.viewer.draw_circle(cx, cy, cr)
+
+        assert_that(axes.patches, has_length(1))
+        circle = axes.patches[0]
+
+        assert_that(circle.center, equal_to((cx, cy)))
+        assert_that(circle.radius, equal_to(cr))
+
+
+class UtilityTest(unittest.TestCase):
     def test_normalize_array(self):
         arr = np.array([[100, 20], [0, 60]])
         norm = mplview.normalize(arr, 5, 10)
