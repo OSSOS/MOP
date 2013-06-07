8349981500caa11b2449cde86bee2a0122a234a8
drusk <drusk@uvic.ca>
2013-06-07 16:31:23 -0700

    Fixed zscaling to use clipping instead of normalization.

--- src/pymop/test/test_pymop/test_gui/test_mplview.py
+++ src/pymop/test/test_pymop/test_gui/test_mplview.py
@@ -3,7 +3,6 @@ __author__ = "David Rusk <drusk@uvic.ca>"
 import unittest
 import wx
 
-import numpy as np
 from matplotlib.backend_bases import MouseEvent as MPLMouseEvent
 from mock import Mock
 from hamcrest import assert_that, equal_to, has_length, instance_of, none
@@ -207,26 +206,6 @@ class InteractionTest(unittest.TestCase):
 
 
 class UtilityTest(unittest.TestCase):
-    def test_normalize_array(self):
-        arr = np.array([[100, 20], [0, 60]])
-        norm = imgviewer.normalize(arr, 5, 10)
-
-        assert_that(norm[0, 0], equal_to(10))
-        assert_that(norm[0, 1], equal_to(6))
-        assert_that(norm[1, 0], equal_to(5))
-        assert_that(norm[1, 1], equal_to(8))
-
-    @unittest.skip("TODO: handle negative values")
-    def test_normalize_array_negatives(self):
-        arr = np.array([[100, -60], [-100, 60]])
-
-        norm = imgviewer.normalize(arr, 5, 10)
-
-        assert_that(norm[0, 0], equal_to(10))
-        assert_that(norm[0, 1], equal_to(6))
-        assert_that(norm[1, 0], equal_to(5))
-        assert_that(norm[1, 1], equal_to(8))
-
     def test_clip_in_range(self):
         assert_that(imgviewer.clip(0.5, 0, 1), equal_to(0.5))
 
