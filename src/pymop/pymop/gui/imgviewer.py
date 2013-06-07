Fixed zscaling to use clipping instead of normalization.

src/pymop/pymop/gui/imgviewer.py
@@ -1,5 +1,6 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
+import numpy as np
 import wx
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
@@ -439,16 +440,7 @@ def zscale(img):
     """
     # Using the default values, but listing explicitly
     z1, z2 = numdisplay.zscale.zscale(img, nsamples=1000, contrast=0.25)
-    return normalize(img, z1, z2)
-
-
-def normalize(arr, lower, upper):
-    """
-    Normalizes a numpy array to values between the given lower and upper
-    bounds.
-    """
-    scaling_factor = float(upper - lower) / (arr.max() - arr.min())
-    return scaling_factor * arr + lower
+    return np.clip(img, z1, z2)
 
 
 def clip(value, min_val, max_val):