c731b9c5a7307b07077181419c18fabf2151e170
drusk <drusk@uvic.ca>
2013-06-14 16:27:11 -0700

    The task to be performed can now be selected during startup in addition to the working directory.

--- src/pymop/test/manual_inspection/insp_taskselect.py
+++ src/pymop/test/manual_inspection/insp_taskselect.py
@@ -3,14 +3,14 @@ __author__ = "David Rusk <drusk@uvic.ca>"
 import wx
 
 from context import testutil
-from pymop.gui.taskselect import WorkingDirectorySelector
+from pymop.gui.taskselect import TaskSetupManager
 
 
 def main():
     wxapp = wx.App()
 
     app = testutil.Dummy("PymopApplication")
-    selector = WorkingDirectorySelector(app)
+    selector = TaskSetupManager(app)
     selector.run()
 
     wxapp.MainLoop()
