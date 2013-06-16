ca2d94db97cf35d218d492ed1b5f9a0c59e50995
drusk <drusk@uvic.ca>
2013-06-16 15:50:39 -0700

    Added except ImportError statement for wx pubsub to handle changes in wxpython 2.9.

--- src/pymop/pymop/gui/views.py
+++ src/pymop/pymop/gui/views.py
@@ -3,7 +3,14 @@ __author__ = "David Rusk <drusk@uvic.ca>"
 import os
 
 import wx
-from wx.lib.pubsub import Publisher as pub
+
+try:
+    # wxPython 2.8
+    from wx.lib.pubsub import Publisher as pub
+except ImportError:
+    # wxPython 2.9
+    from wx.lib.pubsub import pub
+
 from wx.lib.mixins import listctrl as listmix
 
 from pymop import config
