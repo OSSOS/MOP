167a0cd3334b2cfe101812b5058e41bcabfb4ce9
mtbannister <michele@mso.anu.edu.au>
2013-06-18 11:24:42 -0700

    Discoveries page with full sqlalchemy implementation and partitioning of db-side work from views html-side work.

--- src/ossos-pipeline/ossos_webdev/ossos.discoveries/ossos/discoveries/views.py
+++ src/ossos-pipeline/ossos_webdev/ossos.discoveries/ossos/discoveries/views.py
@@ -1,31 +1,28 @@
 from pyramid.response import Response
 from pyramid.view import view_config
-import queries
-import ossos.overview.queries as oq
+from queries import DiscoveriesQuery
+
 
 class Discovery(object):
 
 	def __init__(self, request):
 		self.request = request
-		self.discoveries = oq.ossuaryTable('discoveries')
+		self.discoveriesQuery = DiscoveriesQuery()
 
 
 	@property
 	def num_discoveries(self):
-		retval = queries.num_discoveries(self.discoveries)
+		retval = self.discoveriesQuery.num_discoveries()
 		return retval
 
 	@property
 	def ossos_discoveries(self):
-		retval = []   # AWAITING NEED TO BE IMPLEMENTED
+		retval = self.discoveriesQuery.ossos_discoveries()
 		return retval
 
 
 	@view_config(route_name='discoveries', renderer='discoveries.pt')
-	def inspect_observations(self):
-		# uid = self.request.matchdict['uid']
-		# return dict(title='Triplet', uid=uid)
-		
+	def inspect_observations(self):	
 		retval = {'num_discoveries': self.num_discoveries,
 		'discoveries': self.ossos_discoveries
 		}
