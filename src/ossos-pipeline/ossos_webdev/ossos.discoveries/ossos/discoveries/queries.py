678ec14e96d9f01f3e8ee714c387b24b8e11baca
mtbannister <michele@mso.anu.edu.au>
2013-06-18 11:24:42 -0700

    Discoveries page with full sqlalchemy implementation and partitioning of db-side work from views html-side work.

--- src/ossos-pipeline/ossos_webdev/ossos.discoveries/ossos/discoveries/queries.py
+++ src/ossos-pipeline/ossos_webdev/ossos.discoveries/ossos/discoveries/queries.py
@@ -1,22 +1,41 @@
-from sqlalchemy import *
+import sqlalchemy as sa
 import ossos.overview.queries as oq
-import ossos.field_obs.queries as foq
 
 
-def num_discoveries(discoveries):
-	ss = text(
-		"""select count(discovery_id) from discoveries;"""
-		)
-	retval = [n[0] for n in discoveries.conn.execute(ss)][0]
-	
-	return retval 
+class DiscoveriesQuery(object):
+
+	def __init__(self):
+		ot = oq.ossuaryTable('discoveries')
+		self.discoveries = ot.table
+		self.conn = ot.conn
 
 
-def mpc_informed(discoveries):
-	ss = text(
-		"""select count(mpc_told) from discoveries;"""
-		)
-	retval = [n[0] for n in discoveries.conn.execute(ss)][0]
+	def num_discoveries(self):
+		it = self.discoveries
+		ss = sa.select([sa.func.count(it.c.discovery_id)])
+		retval = [n[0] for n in self.conn.execute(ss)][0]
+		
+		return retval 
 
-	return retval 
 
+	def mpc_informed(self):
+		it = self.discoveries
+		ss = sa.select([it.c.mpc_told])
+		# CONFIRM THIS WORKS AFTER ADDING OBJECTS
+		ss.append_whereclause(it.c.mpc_told == True)
+		retval = [n[0] for n in self.conn.execute(ss)][0]
+
+		return retval 
+
+
+	def ossos_discoveries(self):
+		it = self.discoveries
+		cols = [it.c.discovery_id, it.c.a, it.c.e, it.c.i, it.c.q, it.c.h, 
+				it.c.classification, it.c.mpc_told, it.c.discovery_im1,
+				it.c.discovery_im2, it.c.discovery_im3]
+		ss = sa.select(cols)
+
+		# Confirm this formats correctly!
+		retval = [n for n in self.conn.execute(ss)]  
+
+		return retval
