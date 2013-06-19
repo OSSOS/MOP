016db495213047f81f76fc8ebc9f5065507d68b8
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2013-06-19 15:22:51 -0700

    added authentication

---
+++ src/ossos-pipeline/ossos_webdev/ossos.auth/ossos/auth/model.py
@@ -0,0 +1,74 @@
+import urllib
+
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.config import Configurator
+from pyramid.httpexceptions import HTTPForbidden
+from pyramid.httpexceptions import HTTPFound
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.security import authenticated_userid
+from pyramid.security import forget
+from pyramid.security import remember
+from pyramid.view import forbidden_view_config
+from pyramid.view import view_config
+
+import gms
+
+USERS = {}
+
+### DEFINE MODEL
+class User(object):
+
+    def __init__(self, login, password, groups=None):
+        self.login = login
+        self.password = password
+        self.groups = groups or []
+        USERS[login] = self
+
+    def check_password(self, passwd, group):
+        """check that the passwd provided matches the required password."""
+        return gms.isMember(self.login, passwd, group)
+
+@forbidden_view_config()
+def forbidden_view(request):
+    # do not allow a user to login if they are already logged in
+    if authenticated_userid(request):
+        return HTTPForbidden()
+
+    loc = request.route_url('login', _query=(('next', request.path),))
+    return HTTPFound(location=loc)
+
+
+@view_config(
+    route_name='login',
+    renderer='login.mako',
+    )
+def login_view(request):
+    next = request.params.get('next') or request.route_url('home')
+    login = ''
+    did_fail = False
+    if 'submit' in request.POST:
+        login = request.POST.get('login', '')
+        passwd = request.POST.get('passwd', '')
+        user = USERS.get(login,User(login, groups=['OSSOS']))
+        if user.check_password(passwd, 'OSSOS'):
+            headers = remember(request, login)
+            return HTTPFound(location=next, headers=headers)
+        did_fail = True
+
+    return {
+        'login': login,
+        'next': next,
+        'failed_attempt': did_fail,
+        'users': USERS,
+        }
+
+
+@view_config(
+    route_name='logout',
+)
+def logout_view(request):
+    headers = forget(request)
+    loc = request.route_url('home')
+    return HTTPFound(location=loc, headers=headers)
+
