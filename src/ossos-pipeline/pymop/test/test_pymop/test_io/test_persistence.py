395ff7a407b6349bf88086453f0e5e0aadf93915
drusk <drusk@uvic.ca>
2013-06-20 11:12:31 -0700

    Added ability for ProcessRecord object to write progress in addition to reading it.

--- src/ossos-pipeline/pymop/test/test_pymop/test_io/test_persistence.py
+++ src/ossos-pipeline/pymop/test/test_pymop/test_io/test_persistence.py
@@ -3,7 +3,7 @@ __author__ = "David Rusk <drusk@uvic.ca>"
 import os
 import unittest
 
-from hamcrest import assert_that, contains_inanyorder, has_length
+from hamcrest import assert_that, contains_inanyorder, has_length, contains
 
 from test.base_tests import FileReadingTestCase
 from pymop import tasks
@@ -17,23 +17,11 @@ WD_NO_LOG = "data/persistence_no_log"
 class PersistenceTest(FileReadingTestCase):
     def test_load_progress(self):
         progress = persistence.load_progress(self.get_abs_path(WD_HAS_LOG))
-        assert_that(progress.get_processed_cands_files(),
-                    contains_inanyorder("xxx1.cands.astrom", "xxx3.cands.astrom"))
-        assert_that(progress.get_processed_reals_files(),
-                    contains_inanyorder("xxx3.reals.astrom"))
 
         assert_that(progress.get_processed(tasks.CANDS_TASK),
                     contains_inanyorder("xxx1.cands.astrom", "xxx3.cands.astrom"))
         assert_that(progress.get_processed(tasks.REALS_TASK),
                     contains_inanyorder("xxx3.reals.astrom"))
-
-    def test_load_progress_no_logfile(self):
-        working_directory = self.get_abs_path(WD_NO_LOG)
-        progress = persistence.load_progress(working_directory)
-        assert_that(progress.get_processed_cands_files(), has_length(0))
-        assert_that(progress.get_processed_reals_files(), has_length(0))
-
-        os.remove(os.path.join(working_directory, persistence.LOGFILE))
 
     def test_astrom_workload_filtered_reals(self):
         working_directory = self.get_abs_path(WD_HAS_LOG)
@@ -54,6 +42,44 @@ class PersistenceTest(FileReadingTestCase):
         actual_filenames = [filename for filename, astromdata in workload]
 
         assert_that(actual_filenames, contains_inanyorder(*expected_filenames))
+
+
+class PersistenceStartsWithNoLogTest(FileReadingTestCase):
+    def setUp(self):
+        self.working_directory = self.get_abs_path(WD_NO_LOG)
+
+    def tearDown(self):
+        # Get rid of the log file so it doesn't interfere with other tests
+        os.remove(os.path.join(self.working_directory, persistence.LOGFILE))
+
+    def test_load_progress_no_logfile(self):
+        progress = persistence.load_progress(self.working_directory)
+
+        assert_that(progress.get_processed(tasks.CANDS_TASK), has_length(0))
+        assert_that(progress.get_processed(tasks.REALS_TASK), has_length(0))
+
+    def test_write_progress_new_logfile(self):
+        progress = persistence.load_progress(self.working_directory)
+
+        assert_that(progress.get_processed(tasks.CANDS_TASK), has_length(0))
+        assert_that(progress.get_processed(tasks.REALS_TASK), has_length(0))
+
+        processed1 = "xxx2.reals.astrom"
+        progress.record_processed(processed1, tasks.REALS_TASK)
+
+        assert_that(progress.get_processed(tasks.CANDS_TASK), has_length(0))
+        assert_that(progress.get_processed(tasks.REALS_TASK),
+                    contains(processed1))
+
+        # Close the progress object and reload to make sure the changes made
+        # it to disk
+        progress.close()
+
+        reopened_progress = persistence.load_progress(self.working_directory)
+        assert_that(reopened_progress.get_processed(tasks.CANDS_TASK),
+                    has_length(0))
+        assert_that(reopened_progress.get_processed(tasks.REALS_TASK),
+                    contains(processed1))
 
 
 if __name__ == '__main__':
