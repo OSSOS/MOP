2709d2ded9b0de9d9173485a5b9dc38504129c6c
drusk <drusk@uvic.ca>
2013-06-24 14:23:46 -0700

    Added ability to lock and unlock files to persistence module.

--- src/ossos-pipeline/pymop/pymop/io/persistence.py
+++ src/ossos-pipeline/pymop/pymop/io/persistence.py
@@ -1,5 +1,6 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
+import getpass
 import os
 
 from pymop import tasks
@@ -10,6 +11,17 @@ CANDS = "CANDS"
 REALS = "REALS"
 
 DONE_SUFFIX = ".DONE"
+LOCK_SUFFIX = ".LOCK"
+
+
+class FileLockedException(Exception):
+    """Indicates someone already has a lock on the requested file."""
+    def __init__(self, filename, locker):
+        self.filename = filename
+        self.locker = locker
+
+        super(FileLockedException, self).__init__(
+            "%s is locked by %s" % (filename, locker))
 
 
 class ProgressManager(object):
@@ -28,13 +40,44 @@ class ProgressManager(object):
     def record_done(self, filename):
         open(self._get_full_path(filename) + DONE_SUFFIX, "wb").close()
 
+    def record_source(self, index):
+        raise NotImplementedError()
+
+    def lock(self, filename):
+        lockfile = self._get_full_path(filename + LOCK_SUFFIX)
+
+        if os.path.exists(lockfile):
+            # The file is already locked
+            with open(lockfile, "rb") as filehandle:
+                locker = filehandle.read()
+
+            raise FileLockedException(filename, locker)
+
+        else:
+            # The file has not been locked, we can grab it
+            with open(lockfile, "wb") as filehandle:
+                filehandle.write(getpass.getuser())
+
+    def unlock(self, filename):
+        lockfile = self._get_full_path(filename + LOCK_SUFFIX)
+
+        with open(lockfile, "rb") as filehandle:
+            locker = filehandle.read()
+            if locker == getpass.getuser():
+                # It was us who locked it
+                os.remove(lockfile)
+            else:
+                # Can't remove someone else's lock!
+                raise FileLockedException(filename, locker)
+
     def clean(self):
         """
         Remove all persistence-related files from the directory.
         """
-        listing = tasks.listdir_for_suffix(self.directory, DONE_SUFFIX)
-        for filename in listing:
-            os.remove(self._get_full_path(filename))
+        for suffix in [DONE_SUFFIX, LOCK_SUFFIX]:
+            listing = tasks.listdir_for_suffix(self.directory, suffix)
+            for filename in listing:
+                os.remove(self._get_full_path(filename))
 
     def _get_done_suffix(self, task):
         return tasks.suffixes[task] + DONE_SUFFIX
