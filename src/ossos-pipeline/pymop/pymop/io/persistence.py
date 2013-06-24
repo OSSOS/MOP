15f9c86556a1f85e05036ea5a1d0e9ef78d099a6
drusk <drusk@uvic.ca>
2013-06-24 12:44:41 -0700

    Added PersistenceManager class for managing saving through sentinel files.

--- src/ossos-pipeline/pymop/pymop/io/persistence.py
+++ src/ossos-pipeline/pymop/pymop/io/persistence.py
@@ -10,6 +10,39 @@ LOGFILE = ".pymop.log"
 CANDS = "CANDS"
 REALS = "REALS"
 
+DONE_SUFFIX = ".DONE"
+
+
+class PersistenceManager(object):
+    """
+    Manages persistence of progress made processing files in a directory.
+    """
+
+    def __init__(self, directory):
+        self.directory = directory
+
+    def get_processed(self, task):
+        listing = tasks.listdir_for_suffix(self.directory,
+                                           self._get_done_suffix(task))
+        return [done_file[:-len(DONE_SUFFIX)] for done_file in listing]
+
+    def record_processed(self, filename):
+        open(self._get_full_path(filename) + DONE_SUFFIX, "wb").close()
+
+    def clean(self):
+        """
+        Remove all persistence-related files from the directory.
+        """
+        listing = tasks.listdir_for_suffix(self.directory, DONE_SUFFIX)
+        for filename in listing:
+            os.remove(self._get_full_path(filename))
+
+    def _get_done_suffix(self, task):
+        return tasks.suffixes[task] + DONE_SUFFIX
+
+    def _get_full_path(self, filename):
+        return os.path.join(self.directory, filename)
+
 
 class ProgressRecord(object):
     """
--- src/ossos-pipeline/pymop/test/test_pymop/test_io/test_persistence.py
+++ src/ossos-pipeline/pymop/test/test_pymop/test_io/test_persistence.py
@@ -8,9 +8,11 @@ from hamcrest import assert_that, contains_inanyorder, has_length, contains
 from test.base_tests import FileReadingTestCase
 from pymop import tasks
 from pymop.io import persistence
+from pymop.io.persistence import PersistenceManager
 from pymop.io.astrom import AstromWorkload
 
 WD_HAS_LOG = "data/persistence_has_log"
+WD_HAS_PROGRESS = "data/persistence_has_progress"
 WD_NO_LOG = "data/persistence_no_log"
 
 
@@ -37,6 +39,38 @@ class PersistenceTest(FileReadingTestCase):
         working_directory = self.get_abs_path(WD_HAS_LOG)
         progress = persistence.load_progress(working_directory)
         workload = AstromWorkload(working_directory, progress, tasks.CANDS_TASK)
+
+        expected_filenames = ["xxx2.cands.astrom"]
+        actual_filenames = [filename for filename, astromdata in workload]
+
+        assert_that(actual_filenames, contains_inanyorder(*expected_filenames))
+
+
+class PersistenceManagerTest(FileReadingTestCase):
+    def test_load_progress(self):
+        persistence_manager = PersistenceManager(self.get_abs_path(WD_HAS_PROGRESS))
+
+        assert_that(persistence_manager.get_processed(tasks.CANDS_TASK),
+                    contains_inanyorder("xxx1.cands.astrom", "xxx3.cands.astrom"))
+        assert_that(persistence_manager.get_processed(tasks.REALS_TASK),
+                    contains_inanyorder("xxx3.reals.astrom"))
+
+    def test_astrom_workload_filtered_reals(self):
+        working_directory = self.get_abs_path(WD_HAS_PROGRESS)
+        persistence_manager = PersistenceManager(working_directory)
+        workload = AstromWorkload(working_directory, persistence_manager,
+                                  tasks.REALS_TASK)
+
+        expected_filenames = ["xxx1.reals.astrom", "xxx2.reals.astrom"]
+        actual_filenames = [filename for filename, astromdata in workload]
+
+        assert_that(actual_filenames, contains_inanyorder(*expected_filenames))
+
+    def test_astrom_workload_filtered_cands(self):
+        working_directory = self.get_abs_path(WD_HAS_PROGRESS)
+        persistence_manager = PersistenceManager(working_directory)
+        workload = AstromWorkload(working_directory, persistence_manager,
+                                  tasks.CANDS_TASK)
 
         expected_filenames = ["xxx2.cands.astrom"]
         actual_filenames = [filename for filename, astromdata in workload]
@@ -141,6 +175,71 @@ class PersistenceStartsWithNoLogTest(FileReadingTestCase):
         test_progress2 = persistence.load_progress(self.working_directory)
         assert_that(test_progress2.get_processed(tasks.CANDS_TASK), has_length(0))
         assert_that(test_progress2.get_processed(tasks.REALS_TASK),
+                    contains_inanyorder(processed1, processed2))
+
+
+class PersistenceManagerFreshDirectoryTest(FileReadingTestCase):
+    def setUp(self):
+        self.working_directory = self.get_abs_path(WD_NO_LOG)
+        self.persistence_manager = PersistenceManager(self.working_directory)
+
+    def tearDown(self):
+        # Get rid of generated files so we don't interfere with other tests
+        self.persistence_manager.clean()
+
+    def test_load_progress_no_logs(self):
+        assert_that(self.persistence_manager.get_processed(tasks.CANDS_TASK),
+                    has_length(0))
+        assert_that(self.persistence_manager.get_processed(tasks.REALS_TASK),
+                    has_length(0))
+
+    def test_write_progress(self):
+        assert_that(self.persistence_manager.get_processed(tasks.CANDS_TASK),
+                    has_length(0))
+        assert_that(self.persistence_manager.get_processed(tasks.REALS_TASK),
+                    has_length(0))
+
+        processed1 = "xxx2.reals.astrom"
+        self.persistence_manager.record_processed(processed1)
+
+        assert_that(self.persistence_manager.get_processed(tasks.CANDS_TASK),
+                    has_length(0))
+        assert_that(self.persistence_manager.get_processed(tasks.REALS_TASK),
+                    contains(processed1))
+
+        # Create a second persistence manager and make sure it sees the changes
+        manager2 = PersistenceManager(self.working_directory)
+        assert_that(manager2.get_processed(tasks.CANDS_TASK), has_length(0))
+        assert_that(manager2.get_processed(tasks.REALS_TASK), contains(processed1))
+
+    def test_write_progress_two_simultaneous_managers(self):
+        assert_that(self.persistence_manager.get_processed(tasks.CANDS_TASK),
+                    has_length(0))
+        assert_that(self.persistence_manager.get_processed(tasks.REALS_TASK),
+                    has_length(0))
+
+        processed1 = "xxx2.reals.astrom"
+        self.persistence_manager.record_processed(processed1)
+
+        assert_that(self.persistence_manager.get_processed(tasks.CANDS_TASK), has_length(0))
+        assert_that(self.persistence_manager.get_processed(tasks.REALS_TASK),
+                    contains(processed1))
+
+        # Create a second simultaneous manager
+        manager2 = PersistenceManager(self.working_directory)
+        processed2 = "xxx3.reals.astrom"
+        manager2.record_processed(processed2)
+
+        # Make sure second manager sees both entries
+        assert_that(manager2.get_processed(tasks.CANDS_TASK),
+                    has_length(0))
+        assert_that(manager2.get_processed(tasks.REALS_TASK),
+                    contains_inanyorder(processed1, processed2))
+
+        # Make sure original manager sees both entries
+        assert_that(self.persistence_manager.get_processed(tasks.CANDS_TASK),
+                    has_length(0))
+        assert_that(self.persistence_manager.get_processed(tasks.REALS_TASK),
                     contains_inanyorder(processed1, processed2))
 
 
