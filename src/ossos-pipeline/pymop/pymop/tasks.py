b6d15a86d03a783128e7dcd288885327765c51ae
drusk <drusk@uvic.ca>
2013-06-26 11:01:23 -0700

    Modified DirectoryManager API so it is not specific to a particular suffix.

--- src/ossos-pipeline/pymop/pymop/tasks.py
+++ src/ossos-pipeline/pymop/pymop/tasks.py
@@ -13,10 +13,16 @@ suffixes = {
 }
 
 
+# TODO: remove after reworking workload loading
 def listdir_for_task(working_directory, task):
     return listdir_for_suffix(working_directory, suffixes[task])
 
 
+# TODO: remove after reworking workload loading
 def listdir_for_suffix(directory, suffix):
     """Note this returns file names, not full paths."""
     return filter(lambda name: name.endswith(suffix), os.listdir(directory))
+
+
+def get_suffix(task):
+    return suffixes[task]
