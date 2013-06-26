3e8f7028d82a60f3425ac8703dac5d7b97df884a
mtbannister <michele@mso.anu.edu.au>
2013-06-25 19:21:51 -0700

    Loading the page for individual fields generates an update to the discovery triplets file for that block on OSSOS/triplets/ on VOSpace. (Implemented; activation commented out in views.py for the moment).

--- src/ossos-pipeline/ossos_webdev/ossos.field_obs/ossos/field_obs/queries.py
+++ src/ossos-pipeline/ossos_webdev/ossos.field_obs/ossos/field_obs/queries.py
@@ -1,5 +1,10 @@
 import sqlalchemy as sa
 from ossos.overview.ossuary import OssuaryTable
+import os, vos
+
+CERTFILE=os.path.join(os.getenv('HOME'),
+                      '.ssl',
+                      'cadcproxy.pem')
 
 
 class ImagesQuery(object):
@@ -11,11 +16,6 @@ class ImagesQuery(object):
 
 
 	def field_images(self, field):
-		# print field
-		# if not(isinstance(field, str)):
-		# 	msg = '"{0}" is not a string'.format(type(field))
-		# 	raise ValueError(msg)
-
 		it = self.images
 		cols = [it.c.cfht_field, it.c.obs_end, it.c.iq_ossos, it.c.image_id]
 		ss = sa.select(cols, order_by=it.c.obs_end)
@@ -31,7 +31,7 @@ class ImagesQuery(object):
 	def format_imquery_return(self, ims_query):
 		ret_images = []
 		for row in ims_query:
-			ret_images.append([row[1], row[2], (row[3])])
+			ret_images.append([row[1], row[2], (row[3])])  # obs_end, iq_ossos, image_id
 		ims_query.close()
 
 		return ret_images
@@ -143,13 +143,14 @@ class ImagesQuery(object):
 			# for each night, create the best possible triple that meets constraints.
 
 			print date, len(ims)
+			if len(ims) > 1:
 			# HACK FOR TESTING PREVIOUS CODE
-			if len(ims) == 3:
-				# format as ([image_ids], [3 rows of remaining info], worst_iq)
-				good_triples.append(([im[3] for im in ims], 
-									ims,
-									max([im[5] for im in ims])
-									))
+				if len(ims) == 3:
+					# format as ([image_ids], [3 rows of remaining info], worst_iq)
+					good_triples.append(([im[3] for im in ims], 
+										ims,
+										max([im[5] for im in ims])
+										))
 
 		if len(good_triples) > 0:
 			# Return the set of 3 images that have the lowest value of 'worst iq'. 
@@ -160,6 +161,8 @@ class ImagesQuery(object):
 			retval = None
 		
 		return retval
+
+		# this should be a def something like 'best_pair' to obtain double or a triple
 
 			# # is their temporal span sufficiently wide for a triplet?
 			# if (nt[-1] - nt[0]) > datetime.timedelta(minutes=90):
@@ -195,14 +198,15 @@ class ImagesQuery(object):
 
 
 	def num_nailings(self, field):
-		# nailings are single images that occur
-		# at least one night after the night of the discovery triplet?
+		# nailings: single images, at least one night after the night of the discovery triplet
 		triplet = self.discovery_triplet(field)
 		retval = 'no discovery triplet'
 		if triplet:
 			images = self.field_images(field)
 			# NEED TO FIX THIS TO ONLY COUNT SINGLE OBSERVATIONS WITHIN THE NIGHT
-			after = [im for im in images['obs'] if (im[0] > triplet[1][2][4])]
+			after = [im for im in images['obs'] if 
+					((im[0] > triplet[1][2][4]) 
+						and im[0].strftime('%Y-%m-%d') != triplet[1][2][4].strftime('%Y-%m-%d'))]
 			# ie. if there's a spare image left in the night after a triple or double, can it count?
 			retval = len(after)
 
@@ -212,12 +216,11 @@ class ImagesQuery(object):
 	def num_doubles(self, field):
 		# double is a pair of images ~ an hour apart taken on the same night
 		# the night of the pair is at least a night after the discovery triplet night
+		# there are no precovery doubles
 		triplet = self.discovery_triplet(field)
 		retval = 'no discovery triplet'
 		if triplet:
 			images = self.field_images(field)
-			# NEED TO FIX THIS TO BE MORE SUBTLE ABOUT COLLECTING DOUBLES IN THE NIGHT (if multiple obs)
-			# nailings = [im for im in images['obs'] if (im[0] > triplet[1][2][4])]
 			double_nights = self.do_doubles_exist(field, triplet[1][2][4])
 			retval = len(double_nights)*2
 
@@ -226,28 +229,72 @@ class ImagesQuery(object):
 
 	def do_doubles_exist(self, field, last_triplet_image):
 
+		# NEED TO ACCOUNT FOR >2 IMAGES ON THE SAME NIGHT THAT COULD FORM A DOUBLE AS A SUBSET
+
 		tplus = sa.text("""
 			select cfht_field, extract(year from obs_end) as year, 
-			extract(month from obs_end) as month, extract(day from obs_end) as day, 
+			extract(month from obs_end) as month, extract(day from obs_end) as day, obs_end,
 			count(image_id) from images 
 			where (cfht_field = :field
 				and obs_end > :date)
-			group by cfht_field, year, month, day 
+			group by cfht_field, year, month, day, obs_end 
 			having count(image_id) = 2
-			order by cfht_field, year, month, day;""")
-		pp = {'field': field, 'date':last_triplet_image}
+			order by cfht_field, year, month, day, obs_end;""")
+		pp = {'field':field, 'date':last_triplet_image}
 		tplus_res = self.conn.execute(tplus, pp)
 
 		double_nights = []
-		for row in tplus_res:  
-			double_nights.append(row[1:4])  # year, month, day
+		for row in tplus_res:  # Filtering output for now, can't get condition into select yet.
+
+			# NEED THIS TO ACCOUNT FOR IMAGES ALSO BEING SPACED BY AT LEAST HALF AN HOUR
+
+			print row[5].strftime('%Y-%m-%d'),last_triplet_image.strftime('%Y-%m-%d')
+			if row[5].strftime('%Y-%m-%d') != last_triplet_image.strftime('%Y-%m-%d'):
+				double_nights.append(row[1:4])  # year, month, day
 
 		return double_nights
 
 
 
-	def export_discovery_triplet(field):
+	def export_discovery_triplet(self, field):
 		# write discovery_triplet to a file in VOSpace
+		triplet = self.discovery_triplet(field)
+
+		if triplet:
+#			try:
+			stem = 'vos:OSSOS/triplets/'
+			vospace = vos.Client(certFile=CERTFILE)
+			# TESTING TESTING REMOVE BEFORE FLIGHT
+			tmpfile = 'E_13A_discovery_expnums.txt'
+			blockuri = stem + tmpfile
+
+			# does a file for this block already exist in VOSpace? If so, copy it back.
+			if vospace.access(blockuri):  # Does this work this way?
+				# vospace.create(blockuri)
+				vospace.copy(blockuri, './'+tmpfile)
+				print tmpfile, '<-', blockuri
+				# the field is already present
+				with open(tmpfile, 'r+') as scratch:
+					lines = scratch.readlines()
+					for line in lines:				
+						if line.split()[3] == field:  # It's present already. Add updating later
+							return
+					scratch.write('%s %s %s %s\n' % (triplet[0][0], triplet[0][1], triplet[0][2], field))
+			else:
+				with open(tmpfile, 'w') as scratch:
+					# 3-line file: id id id field
+					scratch.write('%s %s %s %s\n' % (triplet[0][0], triplet[0][1], triplet[0][2], field))
+
+			vospace.copy(tmpfile, blockuri)
+			print tmpfile, '->', blockuri
+
+#			except Exception, e:
+#				print e
+
 		return
+
+
+
+
 
 
