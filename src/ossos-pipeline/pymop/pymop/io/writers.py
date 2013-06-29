6f3aa9721bfd25bd7cd9dffa66116dd291a5edab
drusk <drusk@uvic.ca>
2013-06-29 16:43:42 -0700

    StreamingAstromWriter is now smarter and can continue writing sources to a file which has already been created and has headers.

--- src/ossos-pipeline/pymop/pymop/io/writers.py
+++ src/ossos-pipeline/pymop/pymop/io/writers.py
@@ -14,13 +14,13 @@ class WriterFactory(object):
         if full_path.endswith(tasks.get_suffix(tasks.CANDS_TASK)):
             output_filename = full_path.replace(tasks.get_suffix(tasks.CANDS_TASK),
                                                 tasks.get_suffix(tasks.REALS_TASK))
-            output_filehandle = open(output_filename, "wb")
+            output_filehandle = open(output_filename, "a+b")
             return StreamingAstromWriter(output_filehandle, data.sys_header)
 
         elif full_path.endswith(tasks.get_suffix(tasks.REALS_TASK)):
             output_filename = full_path.replace(tasks.get_suffix(tasks.REALS_TASK),
                                                 ".mpc")
-            output_filehandle = open(output_filename, "ab")
+            output_filehandle = open(output_filename, "a+b")
             return MPCWriter(output_filehandle)
 
         else:
