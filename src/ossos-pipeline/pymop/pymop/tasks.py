e08f2474e65d7de19e2b319983341234f12bf06f
drusk <drusk@uvic.ca>
2013-06-29 11:19:08 -0700

    Refactoring: removed unused functions.

--- src/ossos-pipeline/pymop/pymop/tasks.py
+++ src/ossos-pipeline/pymop/pymop/tasks.py
@@ -1,7 +1,5 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
-import os
-
 CANDS_TASK = "Process candidate objects"
 REALS_TASK = "Process real objects"
 
@@ -11,17 +9,6 @@ suffixes = {
     CANDS_TASK: ".cands.astrom",
     REALS_TASK: ".reals.astrom"
 }
-
-
-# TODO: remove after reworking workload loading
-def listdir_for_task(working_directory, task):
-    return listdir_for_suffix(working_directory, suffixes[task])
-
-
-# TODO: remove after reworking workload loading
-def listdir_for_suffix(directory, suffix):
-    """Note this returns file names, not full paths."""
-    return filter(lambda name: name.endswith(suffix), os.listdir(directory))
 
 
 def get_suffix(task):
