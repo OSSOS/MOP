d5c0f37db3e090476fce5ef08a883e41c7a53387
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2013-07-07 16:10:24 -0700

    created a per task work flow, each job submitted individually

--- src/ossos-pipeline/ossos_submit/payload/scripts/prep.sh
+++ src/ossos-pipeline/ossos_submit/payload/scripts/prep.sh
@@ -8,12 +8,10 @@ expnum=$1
 echo "Working on ${expnum}"
 
 # Test that we need to do the job
-vls vos:OSSOS/dbimages/${expnum}/${expnum}p.fits >& /dev/null && echo "Already did ${expnum}" && exit
+vls vos:OSSOS/dbimages/${expnum}/${expnum}p.fits >& /dev/null || preproc.py --overscan --flat 13AQ05_r_flat.fits --short --verbose $expnum 
 
-# prep this image for the ossos pipeline
-preproc.py --overscan --flat 13AQ05_r_flat.fits --short --verbose $expnum 
 update_header.py -v --replace ${expnum}
-vcp -v ${expnum}p.fits vos:OSSOS/dbimages/${expnum}/
-mkpsf.py $expnum -v
-step1.py $expnum -v
+
+#mkpsf.py $expnum -v
+#step1.py $expnum -v
 
--- src/ossos-pipeline/ossos_submit/submit_job.sh
+++ src/ossos-pipeline/ossos_submit/submit_job.sh
@@ -1,2 +1,12 @@
-./build_job.sh
-curl -v -k -E ~/.ssl/cadcproxy.pem -X POST -F 'job_sub=<condor_submit.in' -F 'exec_ls=<selfextract.bsx' "https://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/proc/pub?job=jobFile,param:job_sub&exec=execFile,param:exec_ls"
+task=$1
+./build_job.sh 
+for fileId in `vcat vos:OSSOS/triplets/E_13A_discovery_expnums.txt` ; 
+do 
+  expr  $fileId : 'E.*'  && continue
+  for ccd in {0..35} ; 
+  do 
+    python build_condor_submit.py $task $fileId $ccd > condor_submit.in
+    echo "submitting $task $fileId"
+    curl -k -E ~/.ssl/cadcproxy.pem -X POST -F 'job_sub=<condor_submit.in' -F 'exec_ls=<selfextract.bsx' "https://www.canfar.phys.uvic.ca/proc/pub?job=jobFile,param:job_sub&exec=execFile,param:exec_ls"
+  done
+done
