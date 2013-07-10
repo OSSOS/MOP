580dc2c282e2bfa360442ac59620028f46588c0e
drusk <drusk@uvic.ca>
2013-07-10 10:59:12 -0700

    Created DownloadableItem class to better abstract the download context for each image.  This should make it easier to implement download retries. It also allows more of the logic related to determining the cutout to be decoupled from the downloader.

--- src/ossos-pipeline/ossos/gui/downloads.py
+++ src/ossos-pipeline/ossos/gui/downloads.py
@@ -59,21 +59,23 @@ class AsynchronousImageDownloadManager(object):
     def _download_workunit(self, workunit,
                            image_loaded_callback,
                            all_loaded_callback):
+        needs_apcor = workunit.is_apcor_needed()
+
         items_to_download = []
         for source in workunit.get_unprocessed_sources():
             for reading in source.get_readings():
-                items_to_download.append((reading, source))
+                items_to_download.append(
+                    DownloadableItem(reading, source, needs_apcor,
+                                     image_loaded_callback))
 
         index = 0
         threads = []
 
-        needs_apcor = workunit.is_apcor_needed()
         while index < len(items_to_download) and not self._should_stop:
             threads = filter(lambda thread: thread.is_alive(), threads)
 
             if len(threads) < MAX_THREADS:
-                args = (items_to_download[index], needs_apcor,
-                        image_loaded_callback)
+                args = (items_to_download[index], )
                 new_thread = ErrorHandlingThread(self.error_handler,
                                                  target=self.do_download,
                                                  args=args)
@@ -85,10 +87,9 @@ class AsynchronousImageDownloadManager(object):
         if not self.should_stop() and all_loaded_callback is not None:
             all_loaded_callback()
 
-    def do_download(self, item, needs_apcor, on_finished):
-        reading, source = item
-        on_finished(reading, self.downloader.download(reading, source,
-                                                      needs_apcor=needs_apcor))
+    def do_download(self, downloadable_item):
+        downloaded_item = self.downloader.download(downloadable_item)
+        downloadable_item.finished_download(downloaded_item)
 
     def stop_download(self):
         self._should_stop = True
@@ -98,6 +99,105 @@ class AsynchronousImageDownloadManager(object):
 
     def refresh_vos_client(self):
         self.downloader.refresh_vos_client()
+
+
+class DownloadableItem(object):
+    """
+    Specifies an item (image and potentially related files) to be downloaded.
+    """
+
+    def __init__(self, reading, source, needs_apcor, on_finished_callback,
+                 in_memory=True):
+        """
+        Constructor.
+
+        Args:
+          source_reading: ossos.astrom.SourceReading
+            The reading which will be the focus of the downloaded image.
+          source: ossos.astrom.Source
+            The source for which the reading was taken.
+          needs_apcor: bool
+            If True, the apcor file with data needed for photometry
+            calculations is downloaded in addition to the image.
+          in_memory: bool
+            If True, the image is stored in memory without being written to
+            disk.  If False, the image will be written to a temporary file.
+        """
+        self.reading = reading
+        self.source = source
+        self.needs_apcor = needs_apcor
+        self.on_finished_callback = on_finished_callback
+        self.in_memory = in_memory
+
+    def get_focal_point(self):
+        """
+        Determines what the focal point of the downloaded image should be.
+
+        Returns:
+          focal_point: (x, y)
+            The location of the source in the middle observation, in the
+            coordinate system of the current source reading.
+        """
+        middle_index = int(math.ceil((len(self.source.get_readings()) / 2)))
+        middle_reading = self.source.get_reading(middle_index)
+
+        offset_x, offset_y = self.reading.get_coordinate_offset(middle_reading)
+
+        return middle_reading.x + offset_x, middle_reading.y + offset_y
+
+    def get_full_image_size(self):
+        """
+        Returns:
+          tuple(int width, int height)
+            The full pixel size of the image before any cutouts.
+        """
+        return self.reading.get_original_image_size()
+
+    def get_extension(self):
+        """
+        Returns:
+          extension: str
+            The FITS file extension to be downloaded.
+        """
+        if self._is_observation_fake():
+            # We get the image from the CCD directory and it is not multi-extension.
+            return 0
+
+        # NOTE: ccd number is the extension, BUT Fits file extensions start at 1
+        # Therefore ccd n = extension n + 1
+        return str(self.get_ccd_num() + 1)
+
+    def is_inverted(self):
+        """
+        Returns:
+          inverted: bool
+            True if the stored image is inverted.
+        """
+        # TODO: clean this up.  Shouldn't have to handle this issue in
+        # multiple places in the code.
+        if self._is_observation_fake():
+            # We get the image from the CCD directory and it has already
+            # been corrected for inversion.
+            return False
+
+        return True if self.get_ccd_num() <= MAX_INVERTED_CCD else False
+
+    def get_ccd_num(self):
+        """
+        Returns:
+          ccdnum: int
+            The number of the CCD that the image is on.
+        """
+        return int(self.reading.obs.ccdnum)
+
+    def finished_download(self, downloaded_item):
+        """
+        Triggers callbacks indicating the item has been downloaded.
+        """
+        self.on_finished_callback(self.reading, downloaded_item)
+
+    def _is_observation_fake(self):
+        return self.reading.get_observation().is_fake()
 
 
 class ErrorHandlingThread(threading.Thread):
@@ -174,92 +274,46 @@ class ImageSliceDownloader(object):
 
         self.cutout_calculator = CutoutCalculator(slice_rows, slice_cols)
 
-    def _download_fits_file(self, uri, source_reading, cutout_center):
-        # NOTE: ccd number is the extension, BUT Fits file extensions start at 1
-        # Therefore ccd n = extension n + 1
-        ccdnum = int(source_reading.obs.ccdnum)
-        extension = str(ccdnum + 1)
-
-        imgsize = source_reading.get_original_image_size()
-
-        # TODO: clean this up.  Shouldn't have to handle this issue in
-        # multiple places in the code.
-        if source_reading.get_observation().is_fake():
-            # We get the image from the CCD directory, it is not multi-extension,
-            # and has already been corrected for inversion
-            extension = 0
-            inverted = False
-        else:
-            inverted = True if ccdnum <= MAX_INVERTED_CCD else False
-
+    def _download_fits_file(self, downloadable_item):
         cutout_str, converter = self.cutout_calculator.build_cutout_str(
-            extension, cutout_center, imgsize, inverted=inverted)
+            downloadable_item.get_extension(),
+            downloadable_item.get_focal_point(),
+            downloadable_item.get_full_image_size(),
+            inverted=downloadable_item.is_inverted())
 
-        vofile = self.vosclient.open(uri, view="cutout", cutout=cutout_str)
+        # TODO refactor
+        image_uri = self.resolver.resolve_image_uri(downloadable_item.reading.obs)
+        vofile = self.vosclient.open(image_uri, view="cutout", cutout=cutout_str)
 
         return vofile.read(), converter
 
-    def _download_apcor_file(self, uri):
-        vofile = self.vosclient.open(uri, view="data")
+    def _download_apcor_file(self, downloadable_item):
+        # TODO refactor
+        apcor_uri = self.resolver.resolve_apcor_uri(downloadable_item.reading.obs)
+        vofile = self.vosclient.open(apcor_uri, view="data")
         return vofile.read()
 
-    def calculate_cutout_center(self, current_reading, source):
-        """
-        Determines the point at which the cutout for a reading should be
-        centered.
-
-        Args:
-          current_reading: ossos.astrom.SourceReading
-            The reading for which the cutout is to be performed.
-          source: ossos.astrom.Source
-            The source to which the reading belongs.
-
-        Returns:
-          cutout_center: (x, y)
-            The location of the source in the middle observation, in the
-            coordinate system of the current source reading.
-        """
-        middle_index = int(math.ceil((len(source.get_readings()) / 2)))
-        middle_reading = source.get_reading(middle_index)
-
-        offset_x, offset_y = current_reading.get_coordinate_offset(middle_reading)
-
-        return middle_reading.x + offset_x, middle_reading.y + offset_y
-
-    def download(self, source_reading, source, needs_apcor=True, in_memory=True):
+    def download(self, downloadable_item):
         """
         Retrieves a remote image.
 
         Args:
-          source_reading: ossos.astrom.SourceReading
-            The reading to take a cutout around.
-          source: ossos.astrom.Source
-            The source for which the reading was taken.
-          needs_apcor: bool
-            If True, the apcor file with data needed for photometry
-            calculations is downloaded in addition to the image.
-          in_memory: bool
-            If True, the image is stored in memory without being written to
-            disk.  If False, the image will be written to a temporary file.
+          downloadable_item: DownloadableItem
+            Specification for the item to be downloaded.
 
         Returns:
           fitsimage: ossos.gui.image.DownloadedFitsImage
             The downloaded image, either in-memory or on disk as specified.
         """
-        image_uri = self.resolver.resolve_image_uri(source_reading.obs)
-        apcor_uri = self.resolver.resolve_apcor_uri(source_reading.obs)
+        fits_str, converter = self._download_fits_file(downloadable_item)
 
-        cutout_center = self.calculate_cutout_center(source_reading, source)
-
-        fits_str, converter = self._download_fits_file(
-            image_uri, source_reading, cutout_center)
-
-        if needs_apcor:
-            apcor_str = self._download_apcor_file(apcor_uri)
+        if downloadable_item.needs_apcor:
+            apcor_str = self._download_apcor_file(downloadable_item)
         else:
             apcor_str = None
 
-        return DownloadedFitsImage(fits_str, converter, apcor_str, in_memory=in_memory)
+        return DownloadedFitsImage(fits_str, converter, apcor_str,
+                                   in_memory=downloadable_item.in_memory)
 
     def refresh_vos_client(self):
         """
