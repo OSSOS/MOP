625ebf02eaf6215bc3415e8af02981c40327e2c4
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2013-07-09 18:12:20 -0700

    corrected colling sequence on combine.py

--- src/ossos-pipeline/ossos_submit/payload/mop.sh
+++ src/ossos-pipeline/ossos_submit/payload/mop.sh
@@ -13,7 +13,7 @@ mkpsf.py $1 $2 $3 --ccd $ccd -v  ${force}
 step1.py $1 $2 $3 --ccd $ccd -v  ${force}
 step2.py $1 $2 $3 --ccd $ccd -v  ${force}
 step3.py $1 $2 $3 --ccd $ccd -v  ${force}
-combine.py $3 -v  --ccd $ccd ${force}
+combine.py $1 -v  --ccd $ccd ${force}
 
 ## Now build a scramble set and search
 scramble.py $1 $2 $3 --ccd $ccd -v  ${force}
@@ -21,4 +21,4 @@ plant.py $1 $2 $3 --ccd $ccd -v ${force}
 step1.py $1 $2 $3 --ccd $ccd --fk --type s -v  ${force}
 step2.py $1 $2 $3 --ccd $ccd --fk --type s -v  ${force}
 step3.py $1 $2 $3 --ccd $ccd --fk --type s -v ${force}
-combine.py $3 --ccd $ccd --fk --type s -v  ${force}
+combine.py $1 --ccd $ccd --fk --type s -v  ${force}
