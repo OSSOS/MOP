b4a3b9b4b7a832fe33d2dc3af89206fbf8636c64
mtbannister <michele@mso.anu.edu.au>
2013-07-24 10:14:25 -0700

    add force to checking vtags, otherwise it doesn't get the node properties when on lark

--- src/ossos-pipeline/ossos_webdev/ossos.field_obs/ossos/field_obs/queries.py
+++ src/ossos-pipeline/ossos_webdev/ossos.field_obs/ossos/field_obs/queries.py
@@ -64,7 +64,7 @@ class ImagesQuery(object):
 	def clean_keys(self, row):
 		# first retrieve and clean off the keys
 		uri = os.path.join(storage.DBIMAGES, str(row[2]))
-		node = storage.vospace.getNode(uri).props
+		node = storage.vospace.getNode(uri, force=True).props
 		unwanted = ['creator', 'date', 'groupread', 'groupwrite', 'ispublic', 'length']
 		proc_keys = []
 		for vtag, value in node.items():
