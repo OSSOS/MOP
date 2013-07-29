efd034ffbc0af5ea2fba7ec55e3c734ef0239ad1
drusk <drusk@uvic.ca>
2013-07-29 10:14:11 -0700

    Reword source naming to use base62 of the first reading's exposure number concatenated with the base62 of the first reading's x + y values.

--- src/ossos-pipeline/ossos/naming.py
+++ src/ossos-pipeline/ossos/naming.py
@@ -1,8 +1,5 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
-import collections
-import re
-
 ALPHABET_BASE_26 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ALPHABET_BASE_36 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ALPHABET_BASE_62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -55,55 +52,15 @@ class ProvisionalNameGenerator(object):
     http://www.minorplanetcenter.net/iau/info/PackedDes.html
     """
 
-    def __init__(self):
-        self.valid_expnum_reg = re.compile("^\d{7}$")
-        self._processed_expnums = collections.defaultdict(int)
-        self._processed_sources = {}
-
     def name_source(self, source):
         """
         Generates a name for a source.
         """
-        if source in self._processed_sources:
-            return self._processed_sources[source]
+        reading0 = source.get_reading(0)
+        name = (base62encode(int(reading0.get_exposure_number())) +
+                base62encode(int(reading0.x + reading0.y)))
 
-        exposure_numbers = [int(reading.get_exposure_number())
-                            for reading in source.get_readings()]
-        min_expnum = min(exposure_numbers)
-
-        base = self.compress_exposure_number(min_expnum)
-        extension = self._get_extension(min_expnum)
-        name = base + extension
-
-        self._processed_expnums[min_expnum] += 1
-        self._processed_sources[source] = name
+        if len(name) > 7:
+            raise ValueError("Name is more than 7 characters: %s" % name)
 
         return name
-
-    def compress_exposure_number(self, exposure_number):
-        """
-        Compresses an exposure number for use as part of naming a source.
-        In order to compress it to the required number of characters, some
-        assumptions have to be made about the range that exposure_number is
-        in.  A ValueError will be raised if the exposure number does not
-        meet these expectations.
-        """
-        exp_str = str(exposure_number)
-
-        if not self.valid_expnum_reg.match(exp_str):
-            raise ValueError(
-                "Exposure number (%s) does not meet required format (%s)" % (
-                    exp_str, self.valid_expnum_reg.pattern))
-
-        return base26encode(int(exposure_number))
-
-    def _get_extension(self, min_expnum):
-        ext = self._processed_expnums[min_expnum]
-        ext_str = "%02d" % ext
-
-        if len(ext_str) > 2:
-            raise ValueError("Source with min exposure number %d has had %s "
-                             "names generated, which has length > 2." % (
-                                 min_expnum, ext_str))
-
-        return ext_str
