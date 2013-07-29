b4e4a291be051707cd7cb7221a1862cb940dcbe9
drusk <drusk@uvic.ca>
2013-07-29 10:14:11 -0700

    Reword source naming to use base62 of the first reading's exposure number concatenated with the base62 of the first reading's x + y values.

--- src/ossos-pipeline/tests/test_ossos/test_tools/test_naming.py
+++ src/ossos-pipeline/tests/test_ossos/test_tools/test_naming.py
@@ -4,75 +4,27 @@ import unittest
 
 from hamcrest import assert_that, equal_to
 
+from tests.base_tests import FileReadingTestCase
 from ossos import naming
+from ossos.astrom import AstromParser
 from ossos.naming import ProvisionalNameGenerator
-from ossos.astrom import Observation, Source, SourceReading
 
 
-class ProvisionalNameGeneratorTest(unittest.TestCase):
+class ProvisionalNameGeneratorTest(FileReadingTestCase):
     def setUp(self):
         self.undertest = ProvisionalNameGenerator()
 
-    def create_reading(self, obs):
-        """Create a test reading containing the provided observation"""
-        return SourceReading(0, 0, 0, 0, 0, 0, 0, 0, obs)
+    def parse_data(self, filename="data/1616688s17.measure3.cands.astrom"):
+        return AstromParser().parse(self.get_abs_path(filename))
 
-    def create_source(self, expnums):
-        """
-        Create a test source with default values but the provided exposure numbers.
-        """
-        readings = [self.create_reading(Observation(expnum, "p", "22")) for expnum in expnums]
-        return Source(readings)
+    def test_generate_name(self):
+        astrom_data = self.parse_data()
+        source0 = astrom_data.get_sources()[0]
+        reading0 = source0.get_reading(0)
 
-    def test_exposure_number_not_7_digits(self):
-        self.assertRaises(ValueError, self.undertest.compress_exposure_number, 123456)
-        self.assertRaises(ValueError, self.undertest.compress_exposure_number, 12345678)
-        self.assertRaises(ValueError, self.undertest.compress_exposure_number, 0)
-        self.assertRaises(ValueError, self.undertest.compress_exposure_number, None)
-        self.assertRaises(ValueError, self.undertest.compress_exposure_number, "abcdefg")
-
-    def test_compress_exposure_number_from_str_no_repeat_exp1(self):
-        compressed = self.undertest.compress_exposure_number("1616703")
-        assert_that(compressed, equal_to("DNZOX"))
-
-    def test_compress_exposure_number_from_str_no_repeat_exp2(self):
-        compressed = self.undertest.compress_exposure_number("1616704")
-        assert_that(compressed, equal_to("DNZOY"))
-
-    def test_compress_exposure_number_from_int_no_repeat_exp1(self):
-        compressed = self.undertest.compress_exposure_number(1616703)
-        assert_that(compressed, equal_to("DNZOX"))
-
-    def test_compress_exposure_number_from_int_no_repeat_exp2(self):
-        compressed = self.undertest.compress_exposure_number(1616704)
-        assert_that(compressed, equal_to("DNZOY"))
-
-    def test_generate_names_repeat_exp(self):
-        source1 = self.create_source(["1616703"])
-        source2 = self.create_source(["1616703"])
-        source3 = self.create_source(["1616703"])
-        source4 = self.create_source(["1616704"])
-
-        assert_that(self.undertest.name_source(source1), equal_to("DNZOX00"))
-        assert_that(self.undertest.name_source(source2), equal_to("DNZOX01"))
-        assert_that(self.undertest.name_source(source3), equal_to("DNZOX02"))
-        assert_that(self.undertest.name_source(source4), equal_to("DNZOY00"))
-
-    def test_name_source(self):
-        source = self.create_source(["1616704", "1616703"])
-        assert_that(self.undertest.name_source(source), equal_to("DNZOX00"))
-
-    def test_name_same_source(self):
-        source = self.create_source(["1616704", "1616703"])
-        assert_that(self.undertest.name_source(source), equal_to("DNZOX00"))
-        # Generating the name again should produce the same thing
-        assert_that(self.undertest.name_source(source), equal_to("DNZOX00"))
-
-    def test_name_source_different_for_same_obs_diff_src(self):
-        source1 = self.create_source(["1616703", "1616704"])
-        source2 = self.create_source(["1616703", "1616705"])
-        assert_that(self.undertest.name_source(source1), equal_to("DNZOX00"))
-        assert_that(self.undertest.name_source(source2), equal_to("DNZOX01"))
+        expected = (naming.base62encode(1616688) +
+                    naming.base62encode(int(reading0.x + reading0.y)))
+        assert_that(self.undertest.name_source(source0), equal_to(expected))
 
 
 class EncodingTest(unittest.TestCase):
