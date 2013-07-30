fe6cabd1fd267f9813b13267a946f48dc0a173f4
drusk <drusk@uvic.ca>
2013-07-30 13:17:44 -0700

    Moved encoding and decoding functions into a separate coding module.

--- src/ossos-pipeline/tests/test_ossos/test_tools/test_naming.py
+++ src/ossos-pipeline/tests/test_ossos/test_tools/test_naming.py
@@ -7,7 +7,7 @@ from hamcrest import assert_that, equal_to
 from mock import patch
 
 from tests.base_tests import FileReadingTestCase
-from ossos import naming, storage
+from ossos import storage
 from ossos.astrom import AstromParser
 from ossos.naming import ProvisionalNameGenerator
 
@@ -48,40 +48,6 @@ class ProvisionalNameGeneratorTest(FileReadingTestCase):
 
         assert_that(self.undertest.generate_name(astrom_header, fits_header),
                     equal_to("O13AE01"))
-
-
-class EncodingTest(unittest.TestCase):
-    def test_base26_boundary(self):
-        assert_that(naming.base26encode(0), equal_to("A"))
-        assert_that(naming.base26encode(1), equal_to("B"))
-        assert_that(naming.base26encode(9999999), equal_to("VWYXJ"))
-
-    def test_base26_realistic_exposure_numbers(self):
-        assert_that(naming.base26encode(1616703), equal_to("DNZOX"))
-        assert_that(naming.base26encode(1616704), equal_to("DNZOY"))
-        assert_that(naming.base26encode(1616705), equal_to("DNZOZ"))
-
-    def test_base36_encode_decode_1(self):
-        assert_that(naming.base36encode(1), equal_to("1"))
-        assert_that(naming.base36decode("1"), equal_to(1))
-
-    def test_base36_encode_10(self):
-        assert_that(naming.base36encode(10), equal_to("A"))
-        assert_that(naming.base36decode("A"), equal_to(10))
-
-    def test_base36_encode_100(self):
-        assert_that(naming.base36encode(100), equal_to("2S"))
-        assert_that(naming.base36decode("2S"), equal_to(100))
-
-    def test_base36_encode_10000(self):
-        assert_that(naming.base36encode(10000), equal_to("7PS"))
-        assert_that(naming.base36decode("7PS"), equal_to(10000))
-
-    def test_base36_encode_pad_short(self):
-        assert_that(naming.base36encode(1, pad_length=2), equal_to("01"))
-
-    def test_base36_encode_pad_long(self):
-        assert_that(naming.base36encode(10000, pad_length=2), equal_to("7PS"))
 
 
 if __name__ == '__main__':
