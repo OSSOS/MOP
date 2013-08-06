5605d5b39b6f0b6cc91832c1e47cc1e2da12851e
drusk <drusk@uvic.ca>
2013-08-06 15:16:57 -0700

    Removed method that is no longer used.

--- src/ossos-pipeline/ossos/gui/views/mainframe.py
+++ src/ossos-pipeline/ossos/gui/views/mainframe.py
@@ -69,56 +69,6 @@ class MainFrame(wx.Frame):
 
         self.main_panel.SetSizerAndFit(main_sizer)
 
-    def _create_menus(self):
-        def do_bind(handler, item):
-            self.Bind(wx.EVT_MENU, handler, item)
-
-        # Create menus and their contents
-        file_menu = wx.Menu()
-
-        keymap_item = file_menu.Append(
-            id=wx.ID_ANY,
-            text="Keymap",
-            help="Show mappings for keyboard shortcuts.")
-
-        exit_item = file_menu.Append(
-            id=wx.ID_EXIT,
-            text="Exit",
-            help="Exit the program")
-
-        do_bind(self._on_select_keymap, keymap_item)
-        do_bind(self._on_select_exit, exit_item)
-
-        display_menu = wx.Menu()
-        auto_play_item = display_menu.Append(
-            id=wx.ID_ANY,
-            text="Autoplay",
-            help="Automatically transition through images.",
-            kind=wx.ITEM_CHECK)
-
-        do_bind(self._on_select_autoplay, auto_play_item)
-
-        sync_menu = wx.Menu()
-        auto_sync_item = sync_menu.Append(
-            id=wx.ID_ANY,
-            text="Automatically",
-            help="Automatically synchronize results.",
-            kind=wx.ITEM_CHECK)
-
-        do_bind(self._on_select_automatically_sync, auto_sync_item)
-
-        # Create menu bar
-        menubar = wx.MenuBar()
-        self.file_menu_title = "File"
-        menubar.Append(file_menu, self.file_menu_title)
-        self.display_menu_title = "Display"
-        menubar.Append(display_menu, self.display_menu_title)
-        self.sync_menu_title = "Sync"
-        menubar.Append(sync_menu, self.sync_menu_title)
-        self.SetMenuBar(menubar)
-
-        return menubar
-
     def _create_data_notebook(self):
         notebook = wx.Notebook(self.control_panel)
 
