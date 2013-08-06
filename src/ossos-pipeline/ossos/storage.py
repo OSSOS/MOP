16ac53c566bee87e165923948eecdbd03248077d
drusk <drusk@uvic.ca>
2013-08-06 15:27:08 -0700

    Removed unused exception classes in storage.py.

--- src/ossos-pipeline/ossos/storage.py
+++ src/ossos-pipeline/ossos/storage.py
@@ -30,14 +30,6 @@ vospace = vos.Client(cadc_short_cut=True, certFile=CERTFILE)
 SUCCESS = 'success'
 
 
-class PropertyError(object):
-    """"An error occurred accessing a VOSpace property."""
-
-
-class InvalidURIError(Exception):
-    """An invalid URI was provided."""
-
-
 def populate(dataset_name,
              data_web_service_url = DATA_WEB_SERVICE+"CFHT"):
 
