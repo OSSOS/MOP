90801773a01c1b961e1f6c05d02b31d6d71e2673
drusk <drusk@uvic.ca>
2013-08-14 15:11:37 -0700

    Deleted unused TripletDownloadRequest.

--- src/ossos-pipeline/ossos/downloads/requests.py
+++ src/ossos-pipeline/ossos/downloads/requests.py
@@ -42,30 +42,3 @@ class DownloadRequest(object):
             self.focal_point = reading.source_point
         else:
             self.focal_point = focal_point
-
-
-class TripletDownloadRequest(object):
-    """
-    Specifies a group of DownloadRequests which should be executed together.
-    """
-
-    def __init__(self, source, callback):
-        self.source = source
-        self.callback = callback
-
-        self.downloads_finished = 0
-
-        self._focal_point_calculator = TripletFocalPointCalculator()
-
-    def notify_finished(self):
-        self.downloads_finished += 1
-        if self.downloads_finished == 9:
-            self.callback(self.source)
-
-    def iter_requests(self):
-        for focal_point in self._focal_point_calculator.calculate_focal_points(
-                self.source):
-            yield DownloadRequest(focal_point.reading,
-                                  focal_point=focal_point.point,
-                                  needs_apcor=False,
-                                  callback=self.notify_finished)
