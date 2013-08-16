5c5f6273551627960e622c158dd8673034193540
drusk <drusk@uvic.ca>
2013-08-16 10:49:29 -0700

    The view now creates the controller (using a controller factory), instead of having the controller create the view.

--- src/ossos-pipeline/tests/test_integration/test_controllers.py
+++ src/ossos-pipeline/tests/test_integration/test_controllers.py
@@ -60,7 +60,19 @@ class ProcessRealsControllerTest(WxWidgetTestCase, FileReadingTestCase, Director
 
         self.name_generator = Mock(spec=ProvisionalNameGenerator)
         self.name_generator.generate_name.return_value = TEST_PROVISIONAL_NAME
-        self.controller = ProcessRealsController(self.model, self.name_generator)
+
+        class TestFactory(object):
+            def __init__(self, model, name_generator):
+                self.model = model
+                self.name_generator = name_generator
+
+            def create_controller(self, view):
+                return ProcessRealsController(self.model, view,
+                                              self.name_generator)
+
+        self.view = ApplicationView(
+            TestFactory(self.model, self.name_generator))
+        self.controller = self.view.controller
         self.controller.display_current_image = Mock()
 
     def tearDown(self):
@@ -76,46 +88,44 @@ class ProcessRealsControllerTest(WxWidgetTestCase, FileReadingTestCase, Director
     @patch("ossos.gui.controllers.mpc.Observation")
     def test_reject_disables_validation_controls(self, mock_Observation):
         comment = "test"
-        view = self.controller.view
 
-        assert_that(view.is_source_validation_enabled(), equal_to(True))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(True))
         self.controller.on_do_reject(comment)
 
         # We have moved to the next item, so it should still be enabled
-        assert_that(view.is_source_validation_enabled(), equal_to(True))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(True))
 
         # Loop back to that first item
         self.controller.on_next_obs()
         self.controller.on_next_obs()
-        assert_that(view.is_source_validation_enabled(), equal_to(False))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(False))
 
         self.controller.on_next_obs()
-        assert_that(view.is_source_validation_enabled(), equal_to(True))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(True))
         self.controller.on_next_obs()
-        assert_that(view.is_source_validation_enabled(), equal_to(True))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(True))
 
     @patch("ossos.gui.controllers.mpc.Observation")
     def test_reject_last_item_disables_validation_controls(self, mock_Observation):
         comment = "test"
-        view = self.controller.view
 
         self.controller.on_next_obs()
         self.controller.on_next_obs()
-        assert_that(view.is_source_validation_enabled(), equal_to(True))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(True))
         self.controller.on_do_reject(comment)
 
         # We have moved to the next item (looped back to beginning), so it should still be enabled
-        assert_that(view.is_source_validation_enabled(), equal_to(True))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(True))
 
         # Move forward to that last item again
         self.controller.on_next_obs()
         self.controller.on_next_obs()
-        assert_that(view.is_source_validation_enabled(), equal_to(False))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(False))
 
         self.controller.on_next_obs()
-        assert_that(view.is_source_validation_enabled(), equal_to(True))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(True))
         self.controller.on_next_obs()
-        assert_that(view.is_source_validation_enabled(), equal_to(True))
+        assert_that(self.view.is_source_validation_enabled(), equal_to(True))
 
     def accept_source_reading(self):
         self.controller.on_do_accept(TEST_MINOR_PLANET_NUMBER,
