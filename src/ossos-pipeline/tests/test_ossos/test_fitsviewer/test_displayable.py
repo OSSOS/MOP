839a38041227a767d088cb759b8cdaf4643ca273
drusk <drusk@uvic.ca>
2013-08-19 12:12:49 -0700

    Fixed bug in CutoutGrid where all frames were sharing the same list.

--- src/ossos-pipeline/tests/test_ossos/test_fitsviewer/test_displayable.py
+++ src/ossos-pipeline/tests/test_ossos/test_fitsviewer/test_displayable.py
@@ -2,12 +2,15 @@ __author__ = "David Rusk <drusk@uvic.ca>"
 
 import unittest
 
-from hamcrest import assert_that, equal_to, has_length
 import matplotlib.pyplot as plt
+from hamcrest import assert_that, equal_to, has_length, contains
 from mock import Mock
 
+from ossos import astrom
+from ossos.downloads.cutouts.source import SourceCutout
+from ossos.downloads.cutouts.grid import CutoutGrid
 from ossos.fitsviewer.colormap import clip
-from ossos.fitsviewer.displayable import DisplayableImageSinglet
+from ossos.fitsviewer.displayable import DisplayableImageSinglet, DisplayableImageTriplet
 
 
 class DisplayableImageSingletTest(unittest.TestCase):
@@ -57,6 +60,50 @@ class DisplayableImageSingletTest(unittest.TestCase):
 
         assert_that(circle.center, equal_to((c2x, c2y)))
         assert_that(circle.radius, equal_to(c2r))
+
+
+class DisplayableImageTripletTest(unittest.TestCase):
+    def setUp(self):
+        source = Mock(spec=astrom.Source)
+        source.num_readings.return_value = 3
+
+        grid = CutoutGrid(source)
+        self.hdulist00 = Mock()
+        self.hdulist01 = Mock()
+        self.hdulist02 = Mock()
+        self.hdulist10 = Mock()
+        self.hdulist11 = Mock()
+        self.hdulist12 = Mock()
+        self.hdulist20 = Mock()
+        self.hdulist21 = Mock()
+        self.hdulist22 = Mock()
+
+        def mock_cutout(hdulist):
+            cutout = Mock(spec=SourceCutout)
+            cutout.hdulist = hdulist
+            return cutout
+
+        grid.add_cutout(mock_cutout(self.hdulist00), 0, 0)
+        grid.add_cutout(mock_cutout(self.hdulist01), 0, 1)
+        grid.add_cutout(mock_cutout(self.hdulist02), 0, 2)
+        grid.add_cutout(mock_cutout(self.hdulist10), 1, 0)
+        grid.add_cutout(mock_cutout(self.hdulist11), 1, 1)
+        grid.add_cutout(mock_cutout(self.hdulist12), 1, 2)
+        grid.add_cutout(mock_cutout(self.hdulist20), 2, 0)
+        grid.add_cutout(mock_cutout(self.hdulist21), 2, 1)
+        grid.add_cutout(mock_cutout(self.hdulist22), 2, 2)
+
+        self.grid = grid
+
+    def test_frames_have_correct_hdulists(self):
+        displayable = DisplayableImageTriplet(self.grid)
+
+        assert_that(displayable.frames[0].hdulists,
+                    contains(self.hdulist00, self.hdulist01, self.hdulist02))
+        assert_that(displayable.frames[1].hdulists,
+                    contains(self.hdulist10, self.hdulist11, self.hdulist12))
+        assert_that(displayable.frames[2].hdulists,
+                    contains(self.hdulist20, self.hdulist21, self.hdulist22))
 
 
 class UtilityTest(unittest.TestCase):
