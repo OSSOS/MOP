6ded71671c029cdbc57f9ba2eb3e47d4f4cefc05
drusk <drusk@uvic.ca>
2013-08-23 14:50:17 -0700

    Added test cases.

--- src/ossos-pipeline/tests/test_ossos/test_downloads/test_grid.py
+++ src/ossos-pipeline/tests/test_ossos/test_downloads/test_grid.py
@@ -10,7 +10,7 @@ from ossos.downloads.cutouts.grid import CutoutGrid
 from ossos.downloads.cutouts.source import SourceCutout
 
 
-class CutoutGridTest(unittest.TestCase):
+class CutoutGridPrefilledTest(unittest.TestCase):
     def setUp(self):
         self.source = Mock(spec=astrom.Source)
         self.source.num_readings.return_value = 2
@@ -43,6 +43,36 @@ class CutoutGridTest(unittest.TestCase):
             call(self.cutout01, 0, 1),
             call(self.cutout10, 1, 0),
             call(self.cutout11, 1, 1)))
+
+    def test_reset_source_location(self):
+        self.grid.reset_source_location()
+
+        self.cutout00.reset_source_location.called_once_with()
+        self.cutout01.reset_source_location.called_once_with()
+        self.cutout10.reset_source_location.called_once_with()
+        self.cutout11.reset_source_location.called_once_with()
+
+
+class CutoutGridTest(unittest.TestCase):
+    def test_is_filled(self):
+        source = Mock(spec=astrom.Source)
+        source.num_readings.return_value = 2
+
+        grid = CutoutGrid(source)
+
+        def add_cutout(frame_index, time_index):
+            grid.add_cutout(Mock(spec=SourceCutout), frame_index, time_index)
+
+        assert_that(grid.is_filled(), equal_to(False))
+
+        add_cutout(0, 0)
+        assert_that(grid.is_filled(), equal_to(False))
+        add_cutout(1, 0)
+        assert_that(grid.is_filled(), equal_to(False))
+        add_cutout(1, 1)
+        assert_that(grid.is_filled(), equal_to(False))
+        add_cutout(0, 1)
+        assert_that(grid.is_filled(), equal_to(True))
 
 
 if __name__ == '__main__':
