15451d5e85a4addf4e165b92983a04c70d71653b
drusk <drusk@uvic.ca>
2013-08-30 14:13:10 -0700

    TracksParser now makes a note on the reading which is the discovery reading by setting a new discovery attribute to True.  The TracksWorkUnit then looks for this reading and moves it and the next two to the front of the reading list.  This results in the discovery triplet being at the front of the track work unit.

--- src/ossos-pipeline/ossos/gui/models/workload.py
+++ src/ossos-pipeline/ossos/gui/models/workload.py
@@ -713,12 +713,46 @@ class TracksWorkUnitBuilder(WorkUnitBuilder):
             dry_run=dry_run
         )
 
+    def get_readings(self, data):
+        # Note: Track workunits only have 1 source
+        return data.get_sources()[0].get_readings()
+
+    def set_readings(self, data, readings):
+        # Note: Track workunits only have 1 source
+        data.get_sources()[0].readings = readings
+
+    def get_discovery_index(self, data):
+        # Note: there should only be one reading marked "discovery" amongst
+        # the data.
+        for i, reading in enumerate(self.get_readings(data)):
+            if reading.discovery:
+                return i
+
+        raise ValueError("No discovery index found in track workunit.")
+
+    def move_discovery_to_front(self, data):
+        """
+        Moves the discovery triplet to the front of the reading list.
+        Leaves everything else in the same order.
+        """
+        readings = self.get_readings(data)
+        discovery_index = self.get_discovery_index(data)
+
+        reordered_readings = (readings[discovery_index:discovery_index + 3] +
+                              readings[:discovery_index] +
+                              readings[discovery_index + 3:])
+
+        self.set_readings(data, reordered_readings)
+
     def _do_build_workunit(self,
                            filename,
                            data,
                            progress_manager,
                            output_context,
                            dry_run):
+
+        self.move_discovery_to_front(data)
+
         return TracksWorkUnit(self,
             filename, data, progress_manager, output_context, dry_run=dry_run)
 
