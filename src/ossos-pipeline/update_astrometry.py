aedfae48ed7e8a5ad7d57edbc6f376bc13ec558b
JJ Kavelaars <jjk@uvic.ca>
2013-09-28 11:05:38 -0700

    modified the astrometry script so that is just updates the x/y locations in the MPC line.  This is to correct a bug in validate and is just a temporary usage change.  (also removed zscale code from displayable.py since that is no longer being used.  Finally:  workload no-longer exists when a user selects that they want to 'continue' if 'tracks' are complete.  This allows reviewing of a completed track file. The review process is not yet working.

--- src/ossos-pipeline/update_astrometry.py
+++ src/ossos-pipeline/update_astrometry.py
@@ -1,6 +1,4 @@
 import re
-import math
-import ephem
 
 __author__ = 'jjk'
 """
@@ -23,16 +21,12 @@ def remeasure(mpc_line):
         return
     assert isinstance(mpc_obs.comment, mpc.MPCComment)
     parts = re.search('(?P<expnum>\d{7})(?P<type>\S)(?P<ccd>\d\d)',mpc_obs.comment.frame)
-    print parts.group('expnum'), parts.group('ccd')
     header = storage.get_astheader(parts.group('expnum'),int(parts.group('ccd')))
     this_wcs = wcs.WCS(header)
-    (ra,dec) = this_wcs.xy2sky(float(mpc_obs.comment.X),float(mpc_obs.comment.Y))
-    dr = math.sqrt((ra-mpc_obs.coordinate.ra.degrees)**2+(dec-mpc_obs.coordinate.dec.degrees)**2)*3600.0
-    if dr > 0.5:
-        print "{} ({:08.2f},{:08.2f}) {}".format(mpc_obs.comment.frame,float(mpc_obs.comment.X),
-                                                   float(mpc_obs.comment.Y), dr)
-        print mpc_obs.ra, ephem.hours(math.radians(ra))
-        print mpc_obs.dec, ephem.degrees(math.radians(dec))
+    (x,y) = this_wcs.sky2xy(mpc_obs.coordinate.ra.degrees, mpc_obs.coordinate.dec.degrees)
+    mpc_obs.comment.X = x
+    mpc_obs.comment.Y = y
+    print mpc_obs.to_string()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -40,7 +34,10 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    with open(args.mpc_file) as f:
-        for line in f:
-            remeasure(line)
+    fptr = storage.open_vos_or_local(args.mpc_file)
+    lines = fptr.read().split('\n')
+    for line in lines:
+        if not len(line) > 0:
+            continue
+        remeasure(line)
 
