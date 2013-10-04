c0d9c39e767293df09e51d74961987d167aa7f5d
mtbannister <michele@mso.anu.edu.au>
2013-10-03 17:10:49 -0700

    ability to plot orbit fit residuals. Some refactorising.

--- src/ossos-pipeline/planning/presentation_plots.py
+++ src/ossos-pipeline/planning/presentation_plots.py
@@ -1,5 +1,7 @@
 __author__ = 'Michele Bannister'
 
+import matplotlib.mlab as mlab
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import brewer2mpl
 from matplotlib import rcParams
@@ -8,6 +10,7 @@ import numpy as np
 import math
 import sys
 import ephem
+from scipy.stats import norm
 from track_done import parse, get_names
 from ossos import storage
 
@@ -23,10 +26,9 @@ almost_black = '#262626'
 
 # rcParams['figure.figsize'] = (10, 10)
 # rcParams['figure.dpi'] = 150
-rcParams['axes.color_cycle'] = set2
+# rcParams['axes.color_cycle'] = puor
 rcParams['font.size'] = 12   # good for posters/slides
 rcParams['patch.facecolor'] = set2[0]
-
 
 def remove_border(axes=None, keep=('left', 'bottom'), remove=('right', 'top'), labelcol=almost_black):
     """
@@ -38,7 +40,7 @@ def remove_border(axes=None, keep=('left', 'bottom'), remove=('right', 'top'), l
         ax.spines[spine].set_visible(False)
     for spine in keep:
         ax.spines[spine].set_linewidth(0.5)
-        ax.spines[spine].set_color('white')
+        # ax.spines[spine].set_color('white')
     # match the label colour to that of the axes
     ax.xaxis.label.set_color(labelcol)
     ax.yaxis.label.set_color(labelcol)
@@ -91,7 +93,7 @@ Plots to make:
         - q vs a
 """
 
-def top_down_SolarSystem(date="2013/04/09 08:50:00"):
+def top_down_SolarSystem(discoveries, date="2013/04/09 08:50:00"):
     """
     Plot the OSSOS discoveries on a top-down Solar System showing the position of Neptune and model TNOs.
     Coordinates should be polar to account for RA hours, radial axis is in AU.
@@ -105,30 +107,30 @@ def top_down_SolarSystem(date="2013/04/09 08:50:00"):
 
     # plot exclusion zones due to Galactic plane: RAs indicate where bar starts, rather than its centre angle
     width = math.radians(3*15)
-    plt.bar(math.radians(16.5*15), 65, width=width, color=almost_black, alpha=0.2)
-    plt.bar(math.radians(4.5*15), 65, width=width, color=almost_black, alpha=0.2)
+    plt.bar(math.radians(16.5*15), 65, width=width, color=almost_black, linewidth=0, alpha=0.2)
+    plt.bar(math.radians(4.5*15), 65, width=width, color=almost_black, linewidth=0, alpha=0.2)
 
     # plot OSSOS blocks
     for block in ["14:15:28.89", "15:58:01.35", "00:54:00.00", "01:30:00.00"]:
         # truncate these at 8 AU to show that we don't have sensitivity in close; detect Ijiraq at 9.80 AU
         # again RAs indicate where bar starts, so subtract half the block width from the block centrepoints
-        plt.bar(ephem.hours(block)-math.radians(3.5), 65, width=math.radians(7), bottom=8, color='b', alpha=0.2)
+        plt.bar(ephem.hours(block)-math.radians(3.5), 65, width=math.radians(7), bottom=8, color='b', linewidth=0, alpha=0.2)
 
     ax1.set_rlim(0,65)
     ax1.set_rgrids([20,40,60], labels=['','20 AU','40 AU','60 AU'], angle=308, alpha=0.45)
     ax1.yaxis.set_major_locator(MultipleLocator(20))
-    ax1.xaxis.set_major_locator(MultipleLocator(math.radians(15)))
+    ax1.xaxis.set_major_locator(MultipleLocator(math.radians(30)))  # every 2 hours
     ax1.grid(axis='x', color='k', linestyle='--', alpha=0.2)
-    ax1.set_xticklabels(['0h','','','','4h','','',"","","","","","","","","","","","","","20h","","22h"],
+    ax1.set_xticklabels(['0h','','','','','','',"","","","20h","22h"],
                         color='b', alpha=0.6)  # otherwise they get in the way
 
     plot_planets_plus_Pluto(ax1, date)
-    plot_ossos_discoveries(ax1, blockID='O13AE', date=date)
+    plot_ossos_discoveries(ax1, discoveries, date=date)
      # special detection in 13AE: Ijiraq at 2013-04-09 shows inner limit of sensitivity. Position from Horizons
     ax1.scatter(ephem.hours('14 29 46.57'), 9.805,
                 marker='o', s=5, facecolor='b', edgecolor=almost_black, linewidth=0.15, alpha=0.8)
     ax1.annotate('Ijiraq', (ephem.hours('14 29 46.57') + math.radians(7), 9.8+2), size=5)
-    ra, dist, hlat = synthetic_model_kbos(ax1, date, kbotype='resonant')
+    ra, dist, hlat = synthetic_model_kbos(date, kbotype='resonant')
     ax1.scatter(ra, dist, marker='o', s=2, facecolor=almost_black, edgecolor=almost_black, linewidth=0.1, alpha=0.1)
 
     plt.draw()
@@ -153,13 +155,8 @@ def plot_planets_plus_Pluto(ax, date):
 
     return
 
-def plot_ossos_discoveries(ax, blockID='O13AE', date="2013/04/09 08:50:00"):
-    path = 'vos:OSSOS/measure3/2013A-E/track/submitted/'  # rather than discoveries, for longer arcs
-    discoveries = storage.listdir(path)
-    names = get_names(path, blockID)
-    print 'Discoveries:', len(names)
-    for kbo in names:
-        arclen, orbit = parse(kbo, discoveries, path=path)
+def plot_ossos_discoveries(ax, discoveries, date="2013/04/09 08:50:00"):
+    for orbit in discoveries:
         orbit.predict(date.replace('/', '-'))
         ra = math.radians(orbit.coordinate.ra.degrees)
         fc = 'b'
@@ -169,7 +166,7 @@ def plot_ossos_discoveries(ax, blockID='O13AE', date="2013/04/09 08:50:00"):
         if mean_mag > 24.15:  # we can't track these ones
             fc = 'w'
             alph = 1.
-            print kbo, mean_mag
+            print orbit.observations[0].provisional_name, mean_mag
         # if orbit.distance > 50:   # identifying the one that's far out
         #     print kbo, orbit.distance
         #     sys.exit()
@@ -177,7 +174,7 @@ def plot_ossos_discoveries(ax, blockID='O13AE', date="2013/04/09 08:50:00"):
 
     return
 
-def synthetic_model_kbos(ax, date, maglimit=24.5, kbotype='resonant'):
+def synthetic_model_kbos(date, maglimit=24.5, kbotype='resonant'):
     ra = []
     dist = []
     hlat = []
@@ -230,10 +227,93 @@ def sky_map_global():
     return
 
 
+def orbit_fit_residuals(discoveries, blockID='O13AE'):
+    """Brett: What are relevant and stunning are the actual orbit fit residuals
+    for orbits with d(a)/a<1%. I would be happy with a simple histogram of all those numbers,
+    although the 'tail' (probably blended and not yet completely flagged) should
+    perhaps be excluded.  There will be a pretty clear gaussian I would think, peaking near 0.07".
+    """
+    ra_residuals = []
+    dec_residuals = []
+    ressum = []
+    for i, orbit in enumerate(discoveries):
+        if (orbit.da/orbit.a) < 0.01:
+            print i, len(discoveries), orbit.observations[0].provisional_name, orbit.da/orbit.a
+            res = orbit.residuals.split('\n')
+            for r in res:
+                rr = r.split(' ')
+                if (len(rr) > 1) and not (rr[0].startswith('!')):
+                    ra_residuals.append(float(r.split(' ')[4]))
+                    dec_residuals.append(float(r.split(' ')[5]))
+                    ressum.append(1000*(float(r.split(' ')[4])**2 + float(r.split(' ')[5])**2))
+
+    plt.figure(figsize=(6, 6))
+    bins = [r for r in range(-25,150,25)]
+    n, bins, patches = plt.hist(ressum, histtype='step', color='b', bins=bins, label='$dra^{2}+ddec^{2}$')
+    # midpoints = [r/1000. for r in range(-350,350,50)]
+    # popt, pcov = curve_fit(gaussian, midpoints, n)
+    # print 'a', popt[0], 'mu', popt[1], 'sigma', popt[2]
+    # xm = np.linspace(-.375, .375, 100)  # 100 evenly spaced points
+    # plt.plot(xm, gaussian(xm, popt[0], popt[1], popt[2]), ls='--', color='#E47833', linewidth=2)
+    # sigma = r'$\sigma = %.2f \pm %.2f$' % (popt[2], np.sqrt(pcov[2, 2]))
+    # plt.annotate(sigma, (0.12, 50), color='#E47833')
+    plt.xlim((-25,150))  # sufficient for 13AE data
+    plt.ylabel('observations of orbits with d(a)/a < 1% (i.e 3-5 month arcs)')
+    plt.xlabel('orbit fit residuals (milliarcsec)')
+    plt.legend()
+    # clean_legend()
+    plt.draw()
+    outfile = 'orbit_fit_residuals_13AE_corrected'
+    plt.savefig(outfile+'.pdf', transparent=True)
+
+    return
+
+def gaussian(x, a, b, c):
+    val = a * np.exp(-(x - b)**2 / c**2)
+    return val
+
+
+def delta_a_over_a(discoveries):
+    arclen = []
+    da_over_a = []
+    for orbit in discoveries:
+        if (orbit.da/orbit.a) < 0.01:
+            da_over_a.append((orbit.da/orbit.a)*100)
+            arclen.append(orbit.arc_length)
+
+    plt.figure(figsize=(6, 6))
+    plt.scatter(arclen, da_over_a, marker='o', facecolor='b', edgecolor=almost_black, linewidth=0.15, alpha=0.5)
+    plt.xlabel('arc length of orbit (days)')
+    plt.ylabel('d(a)/a (percent)')
+    remove_border()
+    plt.draw()
+    outfile = 'delta_a_over_a_13AE_corrected'
+    plt.savefig(outfile+'.pdf', transparent=True)
+    print 'objects plotted:', len(arclen)
+
+def get_discoveries(blockID='O13AE'):
+    retval = []
+    path = 'vos:OSSOS/measure3/2013A-E/track/submitted/'  # rather than discoveries, for longer arcs
+#    path = 'vos:OSSOS/measure3/2013A-E/track/radec_corrected/'
+    discoveries = storage.listdir(path)
+    names = get_names(path, blockID)
+    print 'Discoveries:', len(names)
+    for i, kbo in enumerate(names):
+        try:
+            arclen, orbit = parse(kbo, discoveries, path=path)
+            retval.append(orbit)
+        except:
+            continue
+
+    return retval
+
 
 def main():
-    top_down_SolarSystem()
-    # side_elevation_SolarSystem()
+    discoveries = get_discoveries()  # just the Orbit objects
+    top_down_SolarSystem(discoveries)
+#    orbit_fit_residuals(discoveries)
+#    delta_a_over_a(discoveries)
+
 
 if __name__ == "__main__":
     main()
