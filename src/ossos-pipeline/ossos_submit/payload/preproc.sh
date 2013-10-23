ea93c70e04f6f028734aedf576b5673ee03578c7
mtbannister <michele@mso.anu.edu.au>
2013-10-23 12:14:00 -0700

    Notes on initial filename used in flats.

--- src/ossos-pipeline/ossos_submit/payload/preproc.sh
+++ src/ossos-pipeline/ossos_submit/payload/preproc.sh
@@ -6,8 +6,8 @@ source /home/jkavelaars/.moprc
 expnum=$1
 echo "Working on ${expnum}"
 
-# Test that we need to do the job
-preproc.py --overscan --flat 13AQ05_r_flat.fits --short --verbose $expnum  || exit -1
+# Test that we need to do the job. 13A has 13AQ05_r_flat.fits
+preproc.py --overscan --flat 13B_r_flat.fits --short --verbose $expnum  || exit -1
 vcp ${expnum}p.fits vos:OSSOS/dbimages/${expnum}/${expnum}p.fits
 
 
