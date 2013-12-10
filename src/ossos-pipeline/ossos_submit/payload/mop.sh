5c6da1d1e45a0c7c3b0076a540a1f73e2e83387c
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2013-12-10 15:30:13 -0800

    modified script to ensure works... modified

--- src/ossos-pipeline/ossos_submit/payload/mop.sh
+++ src/ossos-pipeline/ossos_submit/payload/mop.sh
@@ -14,15 +14,17 @@ step1.py $1 $2 $3 --ccd $ccd -v  ${force}
 step2.py $1 $2 $3 --ccd $ccd -v  ${force}
 step3.py $1 $2 $3 --ccd $ccd -v  ${force}
 echo "Running combine.py"
-echo combine.py $1 -v  --ccd $ccd ${force}
-combine.py $1 -v  --ccd $ccd ${force}
+combine.py $1 -v  --ccd $ccd ${force} --measure3 vos:OSSOS/measure3/2013A-O --field O-2+0
 
 ## Now build a scramble set and search
+echo "scramble and plant"
 scramble.py $1 $2 $3 --ccd $ccd -v  ${force}
-plant.py $1 $2 $3 --ccd $ccd -v ${force}
+mkpsf.py $1 $2 $3 --ccd $ccd -v  ${force} --type s
+step1.py  $1 $2 $3 --ccd $ccd -v  ${force} --type s
+step2.py   $1 $2 $3 --ccd $ccd -v  ${force} --type s
+plant.py $1 $2 $3 --ccd $ccd -v ${force} --type s
 step1.py $1 $2 $3 --ccd $ccd --fk --type s -v  ${force}
 step2.py $1 $2 $3 --ccd $ccd --fk --type s -v  ${force}
 step3.py $1 $2 $3 --ccd $ccd --fk --type s -v ${force}
 echo "Running combine.py"
-echo combine.py $1 --ccd $ccd --fk --type s -v  ${force}
-combine.py $1 --ccd $ccd --fk --type s -v  ${force}
+combine.py $1 --fk --type s -v --ccd $ccd ${force} --measure3 vos:OSSOS/measure3/2013A-O --field O-2+0
