bb817d9dc3e4a73961106a09e33df12f2fd98c7f
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2014-01-07 20:54:06 -0800

    added use of field and measure3 directory on combine

--- src/ossos-pipeline/ossos_submit/payload/mop.sh
+++ src/ossos-pipeline/ossos_submit/payload/mop.sh
@@ -15,14 +15,15 @@ step2.py $1 $2 $3 --ccd $ccd -v  ${force}
 step3.py $1 $2 $3 --ccd $ccd -v  ${force}
 echo "Running combine.py"
 echo combine.py $1 -v  --ccd $ccd ${force}
-combine.py $1 -v  --ccd $ccd ${force}
+combine.py $1 -v  --ccd $ccd ${force} --measure3 vos:OSSOS/dbimages/measure3/2013A-O --field ${field}
 
 ## Now build a scramble set and search
 scramble.py $1 $2 $3 --ccd $ccd -v  ${force}
+mkpsf.py $1 $2 $3 --ccd $ccd -v  ${force} --type s
 plant.py $1 $2 $3 --ccd $ccd -v ${force}
 step1.py $1 $2 $3 --ccd $ccd --fk --type s -v  ${force}
 step2.py $1 $2 $3 --ccd $ccd --fk --type s -v  ${force}
 step3.py $1 $2 $3 --ccd $ccd --fk --type s -v ${force}
 echo "Running combine.py"
 echo combine.py $1 --ccd $ccd --fk --type s -v  ${force}
-combine.py $1 --ccd $ccd --fk --type s -v  ${force}
+combine.py $1 --ccd $ccd --fk --type s -v --measure3 vos:OSSOS/dbimages/measure3/2013A-O --field ${field}
