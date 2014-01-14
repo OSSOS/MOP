5e571dd5a4f189e1efc93f24fb591a14c083e92b
mtbannister <michele@mso.anu.edu.au>
2014-01-15 00:44:44 +1300

    tag cleanup notes and vtag_cleanup. Need to go find the bug in update_header.py that's generating an extra _p in the tags

--- src/ossos-pipeline/vtag_cleanup.py
+++ src/ossos-pipeline/vtag_cleanup.py
@@ -1,53 +1,45 @@
 __author__ = 'Michele Bannister   git:@mtbannister'
 
-import argparse
+# import argparse
 from ossos import storage
 
 
-def check_status(expnum, old_step, step):
-    """
-    Queries VOSpace node of image for its metadata and acts on metadata accordingly.
-    :param expnum:  int, exposure number
-    :param old_step:
-    :param step:
-    :return: :raise:
-    """
-    message = storage.SUCCESS
-    if not storage.get_status(expnum, ccd, step):
-        raise IOError(35, "mkpsf_p hasn't run?")
-    if storage.get_status(expnum, ccd, prefix+'step1_p') and not :
-        continue
-
-    storage.set_status(expnum,
-                       ccd,
-                       prefix + step,
-                       message)
-    return
 
 if __name__=='__main__':
-    """
-    Query VOSpace node of an image and
+    undone = 'undone.txt'
+    with open('undone.txt', 'r') as infile:
+        for expnum in infile.readlines():
+            message = storage.get_status(expnum, 36, 'update_header_p', version='p', return_message=True)
 
-    """
-    parser=argparse.ArgumentParser(
-        description='Clean up tags on given exposures.')
+            # if message == None:
+            #     with open(undone, 'a') as outfile:
+            #         outfile.write(str(expnum))
+            # if (message != storage.SUCCESS) and (message != None):  # let's see what happened here
+            #     print expnum, message
 
-    parser.add_argument("expnum",
-                        type=int,
-                        nargs='+',
-                        help="Exposure numbers(s) to process")
-
-    args=parser.parse_args()
-    ccdlist = range(0,36)
-    singlesteps = ['preproc', 'preproc_o', 'update_header', 'update_header_p']
-    steps = ['mkpsf', 'mkpsf_p', 'step1', 'step1_p', 'step2', 'step2_p', 'step3', 'step3_p', 'combine', 'combine_p',
-             'scramble', 'scramble_s', 'plant', 'plant_s', 'mkpsf_s', 'step1_s', 'fkstep1', 'fkstep1_s',
-             'fkstep2', 'fkstep2_s', 'fkstep3', 'fkstep3_s', 'fkcombine', 'fkcombine_s']
-
-    for expnum in args.expnum:
-        for step in singlesteps:
+            # for fixing the bug in update_header.py at the moment
+            storage.set_status(expnum, 36, 'update_header', message, version='p')
 
 
-        for ccd in ccdlist:
-
+    # """
+    # Query VOSpace node of an image and
+    #
+    # """
+    # parser=argparse.ArgumentParser(
+    #     description='Clean up tags on given exposures.')
+    #
+    # parser.add_argument("expnum",
+    #                     type=int,
+    #                     nargs='+',
+    #                     help="Exposure numbers(s) to process")
+    #
+    # args=parser.parse_args()
+    # ccdlist = range(0,36)
+    # singlesteps = ['preproc', 'preproc_o', 'update_header', 'update_header_p']
+    # steps = ['mkpsf', 'mkpsf_p', 'step1', 'step1_p', 'step2', 'step2_p', 'step3', 'step3_p', 'combine', 'combine_p',
+    #          'scramble', 'scramble_s', 'plant', 'plant_s', 'mkpsf_s', 'step1_s', 'fkstep1', 'fkstep1_s',
+    #          'fkstep2', 'fkstep2_s', 'fkstep3', 'fkstep3_s', 'fkcombine', 'fkcombine_s']
+    #
+    # for expnum in args.expnum:
+    #     for step in singlesteps:
 
