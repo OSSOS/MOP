36521ae32227e46d4dc0297d9ace6dcb72e436d6
mtbannister <michele@mso.anu.edu.au>
2014-01-24 11:35:47 -0800

    output_discovery_triplets now produces detailed files with the planted and searched areas used for the various blocks, with knowledge of choices made in all of 2013.

--- src/ossos-pipeline/planning/output_discovery_triplets.py
+++ src/ossos-pipeline/planning/output_discovery_triplets.py
@@ -2,6 +2,7 @@ __author__ = 'Michele Bannister   git:@mtbannister'
 
 import sqlalchemy as sa
 # import web.field_obs.queries
+import sys
 
 
 class OssuaryTable(object):
@@ -28,13 +29,18 @@ class ImagesQuery(object):
         self.conn = ot.conn
 
 ims = ImagesQuery()
-outfile = '13AE_triplets_details.txt'
 
-with open('E_13A_discovery_expnums.txt', 'r') as infile:
+field = sys.argv[1]
+
+outfile = 'planning/13A{}_triplets_details.txt'.format(field)
+
+with open('planning/{}_13A_discovery_expnums.txt'.format(field), 'r') as infile:
     it = ims.images
 
     with open(outfile, 'w') as ofile:
-        ofile.write('Expnum RA DEC MJD_middle Exptime_sec\n'.format())
+        ofile.write(
+            'Expnum RA DEC MJD_middle Exptime_sec plant_xmin_px plant_xmax_px plant_ymin_px plant_ymax_px '
+            'search_xmin_px search_xmax_px search_ymin_px search_ymax_px\n'.format())
 
     for triplet in infile.readlines():
         # this should have two versions, a very precise one for use with the updated headers
@@ -54,10 +60,27 @@ with open('E_13A_discovery_expnums.txt', 'r') as infile:
             retval.append((init_retval[3] + init_retval[4])/2.)
             # add the exptime back
             retval.append(init_retval[5].total_seconds())
+            # now the area in which we planted TNOs for characterisation (hardwired in plant.csh's calls to kbo_gen)
+            # chosen to match that in header keyword DATASEC = [33:2080,1:4612] / Imaging area of the entire CCD in
+            # raw frame
+            if field == 'E' or 'O':
+                xmin = 66 # ya, we don't know why it got hardwired this way initially. 1.6% underplanting.
+            else:  # spotted this by the time H and L etc were done. This was fixed for H/L to use DATASEC directly.
+                xmin = 33
+            retval.append(xmin)
+            # xmax, ymin and ymax were all correctly taken from DATASEC
+            retval.append(2080)
+            retval.append(1)
+            retval.append(4612)
+            # the search area for E and O is/was [33:2080;1:4612], correctly, and same for H/L.
+            retval.append(33)
+            retval.append(2080)
+            retval.append(1)
+            retval.append(4612)
 
             with open(outfile, 'a') as ofile:
                 # expnum, ra, dec, obs_end, mjd_middle, exptime (seconds)
-                ofile.write('{} {} {} {} {}\n'.format(*retval))
+                ofile.write('{} {} {} {} {} {} {} {} {} {} {} {} {}\n'.format(*retval))
 
         with open(outfile, 'a') as ofile:  # blank line between triplets
             ofile.write('\n')
