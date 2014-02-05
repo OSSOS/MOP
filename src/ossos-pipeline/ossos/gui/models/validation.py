dacd383e64cbcca7a9bcdd779aa918de7e7b0dcd
JJ Kavelaars <jjk@uvic.ca>
2014-02-04 22:22:13 -0800

    Add a pre-fetching pre-fetch in validation... so that less time is spent waiting..

--- src/ossos-pipeline/ossos/gui/models/validation.py
+++ src/ossos-pipeline/ossos/gui/models/validation.py
@@ -81,11 +81,11 @@ class ValidationModel(object):
                 events.send(events.NO_AVAILABLE_WORK)
                 return
         # pre-load some work units if we are within 4 units of the end
-        #if self.work_units.index >  len(self.work_units) - 5:
-        #    try:
-        #        self._get_new_workunit()
-        #    except:
-        #        pass
+        if self.work_units.index >  len(self.work_units) - 5:
+            try:
+                self._get_new_workunit()
+            except:
+                pass
         self.work_units.next()
 
     def expect_source_transition(self):
