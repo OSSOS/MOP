b93c6b8d867a83bad50c0d8e5f4dce33baa7c2c9
mtbannister <michele@mso.anu.edu.au>
2014-03-04 19:30:08 -0800

    crosscheck on tags to see if corresponding .mpc file exists

--- src/ossos-pipeline/vtag_cleanup.py
+++ src/ossos-pipeline/vtag_cleanup.py
@@ -5,7 +5,7 @@ from glob import glob
 from ossos import storage
 from ossos.gui import context
 from ossos.gui import tasks
-from ossos.gui.progress import LOCK_PROPERTY
+from ossos.gui.progress import DONE_PROPERTY
 
 
 def update_vos_with_local_files(user_id, vos_dir, dir_to_scan):
@@ -34,49 +34,60 @@ def fix_tags_on_cands_missing_reals(user_id, vos_dir, property):
     "At the moment this just checks for a single user's missing reals. Easy to generalise it to all users."
     con = context.get_context(vos_dir)
     user_progress = []
-    listing = con.get_listing(tasks.get_suffix('cands'))
+    listing = con.get_listing(tasks.get_suffix('reals'))
+    mpc_listing = con.get_listing('mpc')
     for filename in listing:
-        user = storage.get_property(con.get_full_path(filename), property)
-        if (user is not None) and (
-            user == user_id):  # modify 'and' to generalise to all users with work in this directory
-            print filename, user
-            user_progress.append(filename)
-            #realsfile = filename.replace('cands', 'reals')
-            #if not storage.exists(con.get_full_path(realsfile)):
-            #    print filename, 'no reals file', realsfile
-            storage.set_property(con.get_full_path(filename), property, None)
+        if not filename.startswith('fk'):
+            user = storage.get_property(con.get_full_path(filename), property)
+            if (user is not None):
+                # and (user == user_id):  # modify 'and' to generalise to all users with work in this directory
+                #realsfile = filename.replace('cands', 'reals')
+                #if not con.exists(realsfile):
+                #    print filename, 'no reals file', realsfile
+
+                # go through the listing of .mpc files and see if any match this reals.astrom
+                is_present = False
+                for mpcfile in [f for f in mpc_listing if not f.startswith('fk')]:
+                    if mpcfile.startswith(filename):
+                        print filename, user, 'exists!', mpcfile
+                        is_present = True
+
+                if not is_present:
+                    user_progress.append(filename)
+                    print filename, user, 'no mpc file'
+                    storage.set_property(con.get_full_path(filename), property, None)
 
     print 'Fixed files:', len(user_progress)
 
     return
 
 
-if __name__=='__main__':
-    user_id = 'jkavelaars'  # set as appropriate
+if __name__ == '__main__':
+    user_id = ''  # set as appropriate
     vos_dir = storage.MEASURE3 + '/2013A-O/'
     dir_to_scan = '/Users/michele/Dropbox/OSSOS/measure3/2013A-O/ptsws/'
 
     #update_vos_with_local_files(user_id, vos_dir, dir_to_scan)
-    fix_tags_on_cands_missing_reals(user_id, vos_dir, LOCK_PROPERTY)  # DONE_PROPERTY or LOCK_PROPERTY
+    fix_tags_on_cands_missing_reals(user_id, vos_dir, DONE_PROPERTY)  # DONE_PROPERTY or LOCK_PROPERTY
 
 
     #undone = 'undone.txt'
-        #with open('undone.txt', 'r') as infile:
-        #    for expnum in infile.readlines():
-        #        message = storage.get_status(expnum, 36, 'update_header_p', version='p', return_message=True)
-        #
-        #        # if message == None:
-        #        #     with open(undone, 'a') as outfile:
-        #        #         outfile.write(str(expnum))
-        #        # if (message != storage.SUCCESS) and (message != None):  # let's see what happened here
-        #        #     print expnum, message
-        #
-        #        # for fixing the bug in update_header.py at the moment
-        #        storage.set_status(expnum, 36, 'update_header', message, version='p')
+    #with open('undone.txt', 'r') as infile:
+    #    for expnum in infile.readlines():
+    #        message = storage.get_status(expnum, 36, 'update_header_p', version='p', return_message=True)
+    #
+    #        # if message == None:
+    #        #     with open(undone, 'a') as outfile:
+    #        #         outfile.write(str(expnum))
+    #        # if (message != storage.SUCCESS) and (message != None):  # let's see what happened here
+    #        #     print expnum, message
+    #
+    #        # for fixing the bug in update_header.py at the moment
+    #        storage.set_status(expnum, 36, 'update_header', message, version='p')
 
 
-        # "This part never got finished (below)""
-        # Query VOSpace node of an image and
+    # "This part never got finished (below)""
+    # Query VOSpace node of an image and
     #
     # """
     # parser=argparse.ArgumentParser(
