6716a47d3ab82105c2fa5a3973afca555012ddad
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2014-04-23 21:46:25 -0700

    some new scripts to help plan observing and distribute data

---
+++ src/ossos-pipeline/2MASS_Vizier.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+from astropy.io import votable
+import requests
+from cStringIO import StringIO
+
+def query(ra ,dec, rad=0.1, query=None):
+    """Query the CADC TAP service to determine the list of images for the
+NewHorizons Search.  Things to determine:
+   
+
+   a- Images to have the reference subtracted from.
+   b- Image to use as the 'REFERENCE' image.
+   c- Images to be used for input into the reference image
+
+Logic: Given a particular Image/CCD find all the CCDs of the same field that
+overlap that CCD but are taken more than 7 days later or earlier than
+that image.
+
+    """
+    if query is None:
+       query=( """ SELECT """
+               """ "II/246/out".raj2000 as ra, "II/246/out".dej2000 as dec, "II/246/out".jmag as jmag """
+               """ FROM "II/246/out" """
+               """ WHERE """
+               """ CONTAINS(POINT('ICRS', raj2000, dej2000), CIRCLE('ICRS', {}, {}, {})) = 1 """.format(ra,dec,rad) )
+
+    tapURL = "http://TAPVizieR.u-strasbg.fr/TAPVizieR/tap/sync"
+
+
+    ## Some default parameters for that TAP service queries.
+    tapParams={'REQUEST': 'doQuery',
+               'LANG':    'ADQL',
+               'FORMAT':  'votable',
+               'QUERY':   query}
+
+    response = requests.get(tapURL, params=tapParams)
+    data = StringIO(response.text)
+    data.seek(0)
+    data.seek(0)
+    T = votable.parse_single_table(data).to_table()
+
+    return T
+
+
+if __name__=='__main__':
+    from astropy import coordinates
+    from astropy.time.core import Time
+    import numpy as np
+    import time
+    import sys
+    stack = None
+    Ts = {}
+    T = query(ra=sys.argv[1], dec=sys.argv[2], rad=sys.argv[3])
+    T.sort('jmag')
+    f = open('usno.ccmap', 'w')
+    count = 0 
+    for row in T:
+        count += 1 
+        f.write("{} {} {} {}\n".format(row['ra'], row['dec'], row['jmag'], count))
+    f.close()
+
+
+
---
+++ src/ossos-pipeline/snr.py
@@ -0,0 +1,57 @@
+
+
+from ossos import storage
+from astropy.io import ascii
+from cStringIO import StringIO
+import math
+import sys
+
+expnum = sys.argv[1]
+ccd = sys.argv[2]
+
+fwhm = storage.get_fwhm(expnum, ccd)
+
+class Apcor(object):
+    def __init__(self, expnum, ccd):
+        self.expnum = expnum
+        self.ccd = ccd
+        self.apcor_array = storage.open_vos_or_local(self.uri).read().split()
+
+
+    @property
+    def ap_in(self):
+        return float(self.apcor_array[0])
+
+    @property
+    def ap_cor(self):
+        return float(self.apcor_array[2])
+
+    @property
+    def uri(self):
+        return storage.get_uri(self.expnum, self.ccd, ext='apcor')
+
+def get_sky(expnum, ccd):
+    uri = storage.get_uri(expnum, ccd, ext='phot')
+
+    fobj = StringIO(storage.open_vos_or_local(uri).read())
+    fobj.seek(0)
+    phot_table = ascii.read(fobj)
+    return phot_table['MSKY'].mean()
+        
+
+
+apcor = Apcor(expnum, ccd)
+fwhm = storage.get_fwhm(expnum, ccd)
+zp = storage.get_zeropoint(expnum, ccd)
+sky = get_sky(expnum, ccd)
+
+
+
+flux_24 = 10**(-((24.0 + apcor.ap_cor) - zp)/2.5)
+flux_sky = math.pi * apcor.ap_in**2 * sky
+
+snr = flux_24 / math.sqrt(flux_24 + flux_sky)
+storage.set_tag(expnum, "snr_{:02d}".format(int(ccd)), "{:5.2f}".format(snr))
+
+print expnum, ccd, sky, zp, fwhm, flux_sky, flux_24, snr
+
