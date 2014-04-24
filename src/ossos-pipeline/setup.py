de23313a451145dc776476e6dda7f3d5b90f8544
JJ Kavelaars <jjk@uvic.ca>
2014-04-24 12:44:01 -0700

    new setup.py to go with re-organized file layout

--- src/ossos-pipeline/setup.py
+++ src/ossos-pipeline/setup.py
@@ -8,14 +8,17 @@ if sys.version_info[0] > 2:
     print 'The MOP package is only compatible with Python version 2.7+'
     sys.exit(-1)
 
+## Build the list of scripts to be installed.
+script_dir = 'pipeline'
+scripts = []
+for script in os.listdir(script_dir):
+    if script[-1] in [ "~", "#"]:
+        continue
+    scripts.append(os.path.join(script_dir,script))
+scripts.append('validate.py')
+scripts.append('scripts/OSSOSplot.py')
 
-#import astropy
-#from astropy.setup_helpers import (register_commands, adjust_compiler,
-#                                   filter_packages, update_package_files,
-#                                   get_debug_option)
-#from astropy.version_helpers import get_git_devstr, generate_version_py
-#version = "0.1dev"+get_git_devstr(False)
-version = "0.2"
+version = "0.2.1"
 
 setup(name='ossos',
       version=version,
@@ -23,19 +26,6 @@ setup(name='ossos',
       description="Outer Solar System Origins Survey (OSSOS) Pipeline",
       package_data = { 'ossos' : ['gui/*.json']},
       packages=['ossos', 'ossos/gui', 'ossos/fitsviewer','ossos/downloads', 'ossos/downloads/cutouts', 'ossos/gui/models', 'ossos/gui/views' ],
-      requires=[ 'pyraf', 'astropy (<0.3)', 'vos (>1.9)', 'pyephem','requests','pyOpenSSL', 'numpy', 'wxPython', 'pyds9'],
-      scripts=['mkpsf.py',
-               'OSSOSplot.py',
-               'step1.py',
-               'step2.py',
-               'step3.py',
-               'update_header.py',
-               'scramble.py',
-               'plant.py',
-               'preproc.py',
-               'astrom_mag_check.py',
-               'validate_checker.py',
-               'validate.py',
-               'ObsStatus.py',
-               'combine.py', ],
-)
+      requires=['pyraf', 'astropy (==0.2.5)', 'vos', 'pyephem', 'requests', 'pyOpenSSL', 'numpy', 'wxPython', 'pyds9'],
+      scripts=scripts
+      )
