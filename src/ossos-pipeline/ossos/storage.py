6cb91904fefba43270adb9d1c364c1efd670e391
JJ Kavelaars <jjk@uvic.ca>
2014-08-14 18:41:58 -0700

    corrected 'Client' to use the most recent version of VOS.  the OSSOS package now requires the most resent releases of the VOS...

--- src/ossos-pipeline/ossos/storage.py
+++ src/ossos-pipeline/ossos/storage.py
@@ -36,7 +36,7 @@ TAP_WEB_SERVICE = 'http://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/tap/sync'
 OSSOS_TAG_URI_BASE = 'ivo://canfar.uvic.ca/ossos'
 OBJECT_COUNT = "object_count"
 
-vospace = vos.Client(cadc_short_cut=True, certFile=CERTFILE)
+vospace = vos.Client(cadc_short_cut=True, vospace_certfile=CERTFILE)
 vlog = logging.getLogger('vos')
 vlog.setLevel(logging.ERROR)
 sh = logging.StreamHandler(sys.stderr)
