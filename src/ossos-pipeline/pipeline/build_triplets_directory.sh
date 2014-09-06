4b7d10ec0653d93816c5f6b6bf75d5ca6b6359df
JJ Kavelaars <jjk@uvic.ca>
2014-09-05 23:08:01 -0700

    Made this script take arguments

--- src/ossos-pipeline/pipeline/build_triplets_directory.sh
+++ src/ossos-pipeline/pipeline/build_triplets_directory.sh
@@ -1,14 +1,23 @@
-vcat vos:OSSOS/triplets/E_13A_discovery_expnums.txt | while read exp1 exp2 exp3 field 
+#!/bin/bash
+if [ $# != 2 ]
+then
+  echo "Usage:  build_triplets_directory.sh semester block"
+  exit
+fi
+
+semester=$1
+block=$2
+vcat vos:OSSOS/triplets/${block}_${semester}_discovery_expnums.txt | while read exp1 exp2 exp3 field 
 do 
- vmkdir vos:OSSOS/triplets/2013A/${field} 
+ vmkdir vos:OSSOS/triplets/20${semester}/${field} 
  for ccd in $(seq -f "%02g" 0 35) 
  do 
-    vmkdir vos:OSSOS/triplets/2013A/${field}/ccd${ccd} 
+    vmkdir vos:OSSOS/triplets/20${semester}/${field}/ccd${ccd} 
     for exp in  ${exp1} ${exp2} ${exp3} 
     do   
        for file in `vls vos:OSSOS/dbimages/${exp}/ccd${ccd}` 
        do 
-          vln vos:OSSOS/dbimages/${exp}/ccd${ccd}/${file} vos:OSSOS/triplets/2013A/${field}/ccd${ccd}/${file} 
+          vln --verbose vos:OSSOS/dbimages/${exp}/ccd${ccd}/${file} vos:OSSOS/triplets/20${semester}/${field}/ccd${ccd}/${file} 
           #vln vos:OSSOS/dbimages/${exp}/ccd${ccd}/${file} vos:OSSOS/triplets/2013A/${field}/ccd${ccd}/${file} 
        done  
     done
