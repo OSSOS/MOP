913a943f9131cc76cb3c8858a755c74063a7de6c
mtbannister <michele@mso.anu.edu.au>
2014-09-29 10:28:12 -0700

    clear Calibration tags more effectively for early-version tags

--- src/ossos-pipeline/planning/plotting/brushplay.py
+++ src/ossos-pipeline/planning/plotting/brushplay.py
@@ -37,10 +37,9 @@ table, th, td
 }
 """
 
-data = parsers.ossos_discoveries()  # release_with_metadata()
-# probably not needed now this is handled by the parser
-# data = [d for d in dat if not d.name.__contains__('nt')]  # leave off the uncharacterised for now, they fill up the
-#  plot
+data = parsers.ossos_release_with_metadata()
+# Just plot the sensible ones for now
+dset = [d for d in data if ((d.a is not None) and (d.a_e / d.a < 0.05))]
 
 # # suitably bright ones
 # bright = [d for d in data if d.mag < 23.5]
@@ -52,56 +51,39 @@ data = parsers.ossos_discoveries()  # release_with_metadata()
 # data = temp
 
 fig, ax = plt.subplots(2, 1, sharex=True, figsize=(10, 10))
-# ax[0].set_title('OSSOS cold classical discoveries after 2013 observations in v2 release')
 fig.subplots_adjust(hspace=0.05)
 
-# # first all the objects. Some of these don't have all their orbital parameters yet, but dist and i are ok
-# points = ax[0].errorbar([d.dist for d in data], [d.i for d in data],
-# xerr=[d.dist_e for d in data], yerr=[d.i_e for d in data],
-#                         fmt='.', alpha=0.3, ecolor='0.1', ms=10)
-# # ax[0].set_ylim([-1, 90])
-# ax[0].grid(True, alpha=0.3)
-# ax[0].set_ylabel('inclination (degrees)')
-# ax[0].set_xlabel('heliocentric distance (AU)')
-# # ax[0].annotate('')
-
-# now the plots for objects where we have much better arcs
-
 # why is this one object not fitting at all correctly?
-dset = [d for d in data if
-        ((d.a is not None) and not d.observations[-1].provisional_name.strip(' ').__contains__('o3e17'))]
-print [obj.observations[-1].provisional_name.strip(' ') for obj in dset]
-
-for n in dset:
-    if n.observations[-1].provisional_name.strip(' ').__contains__('o3e38'):
-        print n.a, n.inc, n.e
+# and not d.observations[-1].provisional_name.strip(' ').__contains__('o3e17'))]
+# for n in dset:
+#     if n.observations[-1].provisional_name.strip(' ').__contains__('o3e38'):
+#         print n.a, n.inc, n.e
 
 # For calling with parsers.ossos_release_with_metadata()
-# points = ax[0].errorbar([d.a for d in dset], [d.i for d in dset],
-#                xerr=[d.a_e for d in dset], yerr=[d.i_e for d in dset],
-#                fmt='.', alpha=0.4, ecolor='0.1', ms=10)
+points = ax[0].errorbar([d.a for d in dset], [d.i for d in dset],
+                        xerr=[d.a_e for d in dset], yerr=[d.i_e for d in dset],
+                        fmt='.', alpha=0.4, ecolor='0.1', ms=10)
 
 # For using Orbfit objects
-points = ax[0].errorbar([d.a for d in dset], [d.inc for d in dset],
-                        xerr=[d.da for d in dset], yerr=[d.dinc for d in dset],
-                        fmt='.', alpha=0.4, ecolor='0.1', ms=15)
+# points = ax[0].errorbar([d.a for d in dset], [d.inc for d in dset],
+#                         xerr=[d.da for d in dset], yerr=[d.dinc for d in dset],
+#                         fmt='.', alpha=0.4, ecolor='0.1', ms=15)
 
-# ax[0].set_ylabel('inclination (degrees)')
+ax[0].set_ylabel('inclination (degrees)')
 ax[0].set_ylim([-1, 55])
 ax[0].grid(True, alpha=0.3)
-# ax[0].set_xlabel('semimajor axis (AU)')
 
 # For calling with parsers.ossos_release_with_metadata()
-# ax[1].errorbar([d.a for d in dset], [d.e for d in dset],
-#                xerr=[d.a_e for d in dset], yerr=[d.e_e for d in dset],
-#                fmt='.', alpha=0.4, ecolor='0.1', ms=10)
+ax[1].errorbar([d.a for d in dset], [d.e for d in dset],
+               xerr=[d.a_e for d in dset], yerr=[d.e_e for d in dset],
+               fmt='.', alpha=0.4, ecolor='0.1', ms=10)
 
 # For using Orbfit objects
-ax[1].errorbar([d.a for d in dset], [d.e for d in dset],
-               xerr=[d.da for d in dset], yerr=[d.de for d in dset],
-               fmt='.', alpha=0.4, ecolor='0.1', ms=15)
+# ax[1].errorbar([d.a for d in dset], [d.e for d in dset],
+#                xerr=[d.da for d in dset], yerr=[d.de for d in dset],
+#                fmt='.', alpha=0.4, ecolor='0.1', ms=15)
 
-# ax[1].set_ylabel('eccentricity')
+ax[1].set_ylabel('eccentricity')
 ax[1].set_ylim([-0.01, .8])
 ax[1].grid(True, alpha=0.3)
 # ax[1].set_xlabel('semimajor axis (AU)')
@@ -110,16 +92,14 @@ plt.xlim([20, 155])
 plot_fanciness.remove_border(ax[0])
 plot_fanciness.remove_border(ax[1])
 
-# plt.xlabel('semimajor axis (AU)')
+plt.xlabel('semimajor axis (AU)')
 
 # plt.legend()
 plt.draw()
-outfile = 'v3_OSSOS_discoveries_orbits'
+outfile = 'Sep24_OSSOS_discoveries_orbits'
 plt.savefig(outfile + '.pdf', transparent=True)
 
 # plugins.connect(fig, LinkedBrush(points))
-
-# To activate the interactivity
 
 labels = []
 for obj in data:
@@ -128,18 +108,70 @@ for obj in data:
         peri = obj.peri
     except:
         peri = obj.a * (1. - obj.e)
+        obj.peri = peri
     if peri is not None:
         # label = {'dist': obj.dist, 'peri':obj.peri, 'obj':obj.name}
         html = '<table><tr><th>Name</th><td>{:}</td></tr>' \
                '<tr><th>Dist</th><td>{:.2f}</td></tr>' \
-               '<tr><th>Peri</th><td>{:.2f}</td></tr></table>'.format(obj.observations[-1].provisional_name.strip(' '),
-                                                                      obj.distance, peri)
+               '<tr><th>Peri</th><td>{:.2f}</td></tr></table>'.format(obj.name, obj.dist, peri)
         labels.append(html)
+
+# To activate the interactivity
 
 tooltip = plugins.PointHTMLTooltip(points[0], labels, voffset=10, hoffset=10, css=css)
 plugins.connect(fig, tooltip)
 mpld3.show()
 
+
+# Generate a new plot of the objects designed to match the parameters in Petit et al. 2011
+# layout is to be top left, a-q, top right, i-q, lower left, a-i.
+# For calling with parsers.ossos_release_with_metadata()
+
+plt.close()
+fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+fig.subplots_adjust(hspace=0.0)  # want to work on the v-space as well
+
+points = ax[0][0].errorbar([d.a for d in dset], [d.peri for d in dset],
+                           xerr=[d.a_e for d in dset],
+                           fmt='.', alpha=0.4, ecolor='0.1', ms=10)
+ax[0][0].grid(True, alpha=0.4)
+ax[0][0].set_xlim([39., 48.])
+ax[1][0].set_xticks(range(40, 49, 2))
+ax[0][0].set_ylim([34., 48.])
+ax[0][0].set_ylabel('pericenter (AU)')
+ax[0][0].set_yticks(range(36, 50, 4))
+
+ax[0][1].errorbar([d.i for d in dset], [d.peri for d in dset],
+                  xerr=[d.i_e for d in dset],
+                  fmt='.', alpha=0.4, ecolor='0.1', ms=10)
+ax[0][1].grid(True, alpha=0.4)
+ax[0][1].set_xlim([0., 35.])
+ax[0][1].set_xlabel('inclination (deg)')
+ax[0][1].set_xticks(range(5, 40, 10))
+ax[0][1].set_ylim([34., 48.])
+ax[0][1].set_yticks(range(36, 50, 4))
+
+ax[1][0].errorbar([d.a for d in dset], [d.i for d in dset],
+                  xerr=[d.a_e for d in dset], yerr=[d.i_e for d in dset],
+                  fmt='.', alpha=0.4, ecolor='0.1', ms=10)
+ax[1][0].set_xlim([39., 48.])
+ax[1][0].set_xticks(range(40, 49, 2))
+ax[1][0].set_xlabel('semimajor axis (AU)')
+ax[1][0].set_ylim([0., 35.])
+ax[1][0].set_ylabel('inclination (deg)')
+ax[1][0].set_yticks(range(0, 40, 10))
+ax[1][0].grid(True, which='both', alpha=0.4)
+
+plot_fanciness.remove_border(ax[0][0])
+ax[0][0].tick_params(labelbottom='off')
+plot_fanciness.remove_border(ax[0][1])
+plot_fanciness.remove_border(ax[1][0])
+plot_fanciness.remove_border(ax[1][1], remove=['left', 'right', 'top', 'bottom'], keep=[])
+ax[1][1].tick_params(labelbottom='off', labelleft='off')
+
+plt.draw()
+outfile = 'Sep24_OSSOS_discoveries_Petit_style'
+plt.savefig(outfile + '.pdf', transparent=True)
 
 
 
