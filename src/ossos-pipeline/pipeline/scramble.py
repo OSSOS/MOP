478b8e4e2f6a9082a92b729f50b23cd462cfe45b
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2014-09-30 09:59:22 -0700

    PEP-8 cleanup, also created a main function to avoid shadowing.
    I'm working to get the pipeline scripts to all have a main() function.  Once this is done
    we can build scripts that call these main functions instead of calling the scripts from
    a bash shell.  This will provide better processing control.

--- src/ossos-pipeline/pipeline/scramble.py
+++ src/ossos-pipeline/pipeline/scramble.py
@@ -1,17 +1,19 @@
 #!python 
-'''plant synthetic moving objects into a set of observations.
+"""plant synthetic moving objects into a set of observations.
 
-prior to planting, the headers of the objects may be swapped around.'''
+prior to planting, the headers of the objects may be swapped around."""
 
 
 import argparse
 import os
+import sys
 from ossos import storage
 from astropy.io import fits
 import logging
 
+
 def scramble(expnums, ccd, version='p', dry_run=False):
-    '''run the plant script on this combination of exposures'''
+    """run the plant script on this combination of exposures"""
 
     mjds = []
     fobjs = []
@@ -22,16 +24,16 @@ def scramble(expnums, ccd, version='p', dry_run=False):
         # as otherwise we get pointers...
         mjds.append(fobjs[-1][0].header['MJD-OBS'])
 
-    order = [1,0,2]
+    order = [1, 0, 2]
     for idx in range(len(fobjs)):
-        logging.info("Flipping %d to %d" % ( fobjs[idx][0].header['EXPNUM'],
-                                             expnums[order[idx]]))
+        logging.info("Flipping %d to %d" % (fobjs[idx][0].header['EXPNUM'],
+                                            expnums[order[idx]]))
         fobjs[idx][0].header['EXPNUM'] = expnums[order[idx]]
         fobjs[idx][0].header['MJD-OBS'] = mjds[order[idx]]
         uri = storage.get_uri(expnums[order[idx]],
-                                ccd=ccd,
-                                version='s',
-                                ext='fits')
+                              ccd=ccd,
+                              version='s',
+                              ext='fits')
         fname = os.path.basename(uri)
         if os.access(fname, os.F_OK):
             os.unlink(fname)
@@ -42,7 +44,10 @@ def scramble(expnums, ccd, version='p', dry_run=False):
 
     return
 
-if __name__=='__main__':
+
+def main():
+    """Run the script."""
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--ccd',
                         action='store',
@@ -66,14 +71,14 @@ if __name__=='__main__':
                         default='p',
                         choices=['p', 'o'],
                         help='which type of image')
-    parser.add_argument("--verbose","-v",
+    parser.add_argument("--verbose", "-v",
                         action="store_true")
-    parser.add_argument("--debug",'-d',
+    parser.add_argument("--debug", '-d',
                         action='store_true')
     parser.add_argument("--dry_run", action="store_true", help="Do not copy back to VOSpace, implies --force")
     parser.add_argument("--force", action='store_true')
     
-    args=parser.parse_args()
+    args = parser.parse_args()
 
     if args.dry_run:
         args.force = True
@@ -95,8 +100,9 @@ if __name__=='__main__':
         args.expnums.sort()
 
     ccds = [args.ccd]
+    exit_status = 0
     if args.ccd is None:
-        ccds = range(0,36)
+        ccds = range(0, 36)
     for ccd in ccds:
         expnums = args.expnums
         if storage.get_status(expnums[0], ccd,
@@ -105,12 +111,17 @@ if __name__=='__main__':
         message = storage.SUCCESS
 
         try:
-            scramble(expnums=expnums, ccd=ccd, version='p', dry_run = args.dry_run)
+            scramble(expnums=expnums, ccd=ccd, version='p', dry_run=args.dry_run)
         except Exception as e:
             logging.error(e)
             message = str(e)
+            exit_status = message
         if not args.dry_run:
             storage.set_status(expnums[0], ccd,
                                'scramble', version='s',
                                status=message)
-            
+    return exit_status
+
+
+if __name__ == '__main__':
+    sys.exit(main())
