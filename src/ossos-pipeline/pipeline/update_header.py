0d4316c405939f3f54aaec9570dcdcb5f76cacc9
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2014-10-14 17:14:42 -0700

    switch to using the storage modules header constructor.
    The storage module has a method for building a proper FITS header from a
     .head file and that method was reproduced, in-efficiently, here.  no longer.

--- src/ossos-pipeline/pipeline/update_header.py
+++ src/ossos-pipeline/pipeline/update_header.py
@@ -6,71 +6,21 @@ import logging
 import os
 import sys
 
-from astropy.io import fits
-
 from ossos import storage
 
-hlines = None
 
+def run_update_header(image_hdulist, header_hdulist):
 
-def run_update_header(image, header_filename):
-
-    hdu_list = fits.open(image,
-                         do_not_scale_image_data=True,
-                         mode='update')
-    for hdu in hdu_list[1:]:
-        extname = hdu.header.get('EXTNAME', None)
-        logging.info("Got extno %s from header." % extname)
-        new_header = get_header_object(header_filename,
-                                       extname)
-        for key in ['BZERO', 'BSCALE', 'CADCPROC']:
-            v = hdu.header.get(key, None)
-            if v is None:
+    for hdu_idx in range(1, len(image_hdulist)):
+        new_header = header_hdulist[hdu_idx]
+        ## pull some data structure keywords out of the astrometric headers
+        for key in ['NAXIS', 'XTENSION', 'PCOUNT', 'GCOUNT',
+                    'NAXIS1', 'NAXIS2', 'BITPIX', 'BZERO', 'BSCALE', 'CADCPROC']:
+            if new_header.get(key, None) is not None:
                 del(new_header[key])
-                continue
-            new_header[key] = v
-        hdu.header = new_header
+        image_hdulist[hdu_idx].header.update(new_header)
 
-    hdu_list.flush()
-
-    return hdu_list.close()
-
-
-def get_header_object(header_filename, extname=None):
-    """Given astGwyn header file build a Header object.
-
-    astGwyn produces a single text file that contains all the
-    headers, here we look through this header for the correct
-    chunk for the image of interest.
-
-    extno: int, the name of the desired extension
-
-    """
-    global hlines
-    hlines = hlines or (
-        open(header_filename).readlines())
-
-    while len(hlines) > 0:
-        sbuff = ''
-        line = "{0:80}".format(hlines.pop(0).strip()[0:79])
-        while line[0:3] != 'END' and len(hlines) > 0:
-            sbuff += line
-            line = "{0:80}".format(hlines.pop(0).strip()[0:79])
-
-        ### we've reached a header END keyword or the end of the header
-        if line[0:3] != 'END':
-            raise ValueError('malformed header')
-
-        ### pad out the buffer to make a valid header
-        while len(sbuff) % 2880 != 0:
-            sbuff += "{0:80}".format(' ')
-
-        header = fits.Header.fromstring(sbuff)
-        
-        if extname is None or header.get('EXTNAME', None) == extname:
-            return header
-
-    raise ValueError('requested EXTNAME not found')
+    return image_hdulist
 
 
 def main():
@@ -81,17 +31,17 @@ def main():
                         help='name of extension to in header')
     parser.add_argument('expnum', type=str,
                         help='exposure to update')
-    parser.add_argument('header', type=str,
-                        nargs='?',
-                        help='filename of replacement header')
     parser.add_argument('-r', '--replace',
                         action='store_true',
                         help='store modified image back to VOSpace?')
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--force', action='store_true', help="Re-run even if previous success recorded")
+    parser.add_argument('--dbimages', help="VOSpace DATA storage area.", default="vos:OSSOS/dbimages")
 
     args = parser.parse_args()
+
+    storage.DBIMAGES = args.dbimages
 
     level = logging.CRITICAL
     message_format = "%(message)s"
@@ -104,6 +54,7 @@ def main():
 
     message = storage.SUCCESS
     expnum = args.expnum
+
     exit_status = 0
     try:
         # skip if already succeeded and not in force mode
@@ -111,33 +62,22 @@ def main():
             logging.info("Already updated, skipping")
             sys.exit(0)
     
-        header = (args.header is not None and ((os.access(args.header, os.W_OK) and args.header) or (
-            storage.get_file(args.header, ext='head')))) or (storage.get_file(args.expnum, ext='head'))
+        image_hdulist = storage.get_image(args.expnum, return_file=False)
+        ast_hdulist = storage.get_astheader(expnum, ccd=None)
 
-        image = (os.access(args.expnum, os.W_OK) and args.expnum) or (storage.get_image(args.expnum))
-
-        logging.info(
-            "Swapping header for %s for contents in %s \n" % (
-            image, header))
-
-        expnum = fits.open(image)[0].header['EXPNUM'] or args.expnum
-
-        # skip if already succeeded and not in force mode
-        if storage.get_status(expnum, 36, 'update_header') and not args.force:
-            logging.info("Skipping {} as already done.".format(expnum))
-            sys.exit(0)
-
-        run_update_header(image, header)
+        run_update_header(image_hdulist, ast_hdulist)
+        image_filename = os.path.basename(storage.get_uri(expnum))
+        image_hdulist.writeto(image_filename)
         if args.replace:
             dest = storage.dbimages_uri(expnum)
-            storage.copy(image, dest)
+            storage.copy(image_filename, dest)
             storage.set_status(expnum, 36, 'update_header', message)
     except Exception as e:
+        message = str(e)
         if args.replace:
-            message = str(e)
             storage.set_status(expnum, 36, 'update_header', message)
         exit_status = message
-        logging.error(str(e))
+        logging.error(message)
 
     return exit_status
 
