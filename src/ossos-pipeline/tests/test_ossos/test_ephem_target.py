edd32b5d1ad6713c8758fdc5a355e5664c1d519d
JJ Kavelaars <jjk@uvic.ca>
2014-10-20 23:14:53 -0700

    Cleaned up the object creation structure and created some additional unit-tests.
    The signature of this version is the same as previous, so should be forward compatible.
    Unfortunately coordinates do NOT round-trip correctly, if one puts in  00:00:00.59 for the RA
    you get back 00:00:00.58 (ie, you loose on 1/100 of second of time) similarly for DEC where
    the value loose 1/10 of second.  This error is caused by my hack that avoids having 60.00 appear
    as the value in the print.  a better approach is needed.

--- src/ossos-pipeline/tests/test_ossos/test_ephem_target.py
+++ src/ossos-pipeline/tests/test_ossos/test_ephem_target.py
@@ -1,8 +1,13 @@
 from unittest import TestCase
+from astropy import coordinates
+from astropy.time import Time
+from astropy import units
+import cStringIO
 
 __author__ = 'jjk'
 
 from ossos import ephem_target
+
 
 class TestEphemTarget(TestCase):
 
@@ -10,5 +15,35 @@ class TestEphemTarget(TestCase):
         """Make sure we can build an object with just a name."""
         target_name = "test"
         et = ephem_target.EphemTarget(target_name)
-        print et.doc.toxml()
         self.assertIn(target_name, et.name)
+
+    def test_precision_and_wrap(self):
+        """Check that the precision in the output fits within allowed space and values to round to 60s"""
+        target_name = "test"
+        coordinate = coordinates.ICRSCoordinates("11:59:59.9999999999 -10:00:59.9999999999",
+                                                 unit=(units.hour, units.degree),
+                                                 obstime=Time("2000-01-01 10:00:00", scale='utc'))
+        et = ephem_target.EphemTarget(target_name)
+        et.append(coordinate)
+        line = et.doc.getElementsByTagName("CSV")[0].firstChild.wholeText.split('\n')[-2]
+        values = line.split('|')
+        self.assertEqual(len(values), 4)
+        self.assertEqual(values[1], "11:59:59.99")
+        self.assertEqual(values[2], "-10:00:59.9")
+
+    def test_file_write(self):
+        """Check that we are writing the file."""
+
+        et = ephem_target.EphemTarget("")
+        coordinate = coordinates.ICRSCoordinates("01:27:30.260 +13:40:30.60",
+                                                 unit=(units.hour, units.degree),
+                                                 obstime=Time("2014-12-28 09:59:59", scale='utc'))
+        et.append(coordinate)
+        coordinate = coordinates.ICRSCoordinates("01:27:29.390 +13:40:20.90",
+                                                 unit=(units.hour, units.degree),
+                                                 obstime=Time("2014-12-29 09:59:59", scale='utc'))
+        et.append(coordinate)
+        f_handle = cStringIO.StringIO()
+        et.writer(f_handle)
+        f_handle.seek(0)
+        f_handle.close()
