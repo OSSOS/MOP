3bfb696703b5b37fa8f0dec5ac4dba09adca7f25
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2014-12-05 11:25:58 -0800

    Added a VOFileHandler for use with logging.  Likely this will continue to evolve.

--- src/ossos-pipeline/ossos/util.py
+++ src/ossos-pipeline/ossos/util.py
@@ -1,8 +1,12 @@
 """OSSOS helper methods"""
 
 import subprocess
-import os
 import logging
+from logging import handlers
+import os
+import vos
+
+
 
 def exec_prog(args):
     '''Run a subprocess, check for .OK and raise error if does not exist.
@@ -22,3 +26,65 @@ def exec_prog(args):
         os.unlink(program_name+".FAILED")
     return output
 
+
+class VOFileHandler(handlers.BufferingHandler):
+    """
+    A handler class that writes formatted logging records to VOSpace files.
+    """
+    def __init__(self, filename, vos_client=None):
+        self.filename = filename
+        self._client = vos_client
+        self.stream = None
+        super(VOFileHandler, self).__init__(1024*1024)
+
+    @property
+    def client(self):
+        """
+        Send back the client we were sent, or construct a default one.
+        """
+        if self._client is not None:
+            return self._client
+        self._client = vos.Client()
+        return self._client
+
+    def close(self):
+        """
+        Closes the stream.
+        """
+        self.flush()
+        try:
+            if self.stream is not None:
+                self.stream.close()
+                self.stream = None
+        except:
+            pass
+
+    def flush(self):
+        if self.stream is None:
+            self.stream = self._open()
+        for record in self.buffer:
+            self.stream.write("{}\n".format(self.format(record)))
+        self.buffer = []
+
+    def _open(self):
+        """
+        Open the current base file with the (original) mode
+        Return the resulting stream.
+        """
+        try:
+            if self.stream.closed:
+                return self.stream
+        except AttributeError:
+            pass
+
+        buff = None
+        try:
+            buff = self.client.open(self.filename, view='data').read()
+        except:
+            pass
+
+        stream = self.client.open(self.filename, os.O_WRONLY)
+        if buff is not None:
+            stream.write(buff)
+
+        return stream
