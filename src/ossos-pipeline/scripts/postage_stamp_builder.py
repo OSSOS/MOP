5760be7247fbb87a47168a19fa83df9705a89a82
mtbannister <michele@mso.anu.edu.au>
2014-12-07 11:08:11 -0800

    Postage stamps extracted based on WCS.

--- src/ossos-pipeline/scripts/postage_stamp_builder.py
+++ src/ossos-pipeline/scripts/postage_stamp_builder.py
@@ -1,5 +1,5 @@
 #!python
-"""Create links in the postage stamp directory that allow retrieval of the FITS images associated with the OSSOS detections.
+"""Retrieval of cutouts of the FITS images associated with the OSSOS detections.
 
 Takes a directory of .ast file (in dbase format) as input
 
@@ -9,45 +9,57 @@ vospace/OSSOS/dbimages/1625356/1625356p.fits&DIRECTION=pullFromVoSpace&PROTOCOL=
 ivo://ivoa.net/vospace/core%23httpget&view=cutout&cutout=CIRCLE+ICRS+242.1318+-1
 2.4747+0.05
 """
-import sys
-import re
+
 import argparse
+import logging
+
+import requests
 
 from ossos import mpc
 from ossos import storage
 
 
-def link_cutout(obj, obj_dir, args):
+BASEURL = "http://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/vospace/auth/synctrans"
+
+
+def cutout(obj, obj_dir, args):
     for obs in obj.mpc_observations:
         if obs.null_observation:
             continue
+        expnum = obs.comment.frame.split('p')[0]  # only want calibrated images
+        # Using the WCS rather than the X/Y, as the X/Y can be unreliable on a long-term basis
+        cutout = "CIRCLE ICRS {} {} {}".format(obs.coordinate.ra.degrees,
+                                               obs.coordinate.dec.degrees,
+                                               args.radius)
+        # FIXME: should be able to use line below, but bug in VOSpace requires direct-access workaround, for now.
+        # postage_stamp = storage.get_image(expnum, cutout=cutout)
+
+        target = storage.get_uri(expnum)
+        direction = "pullFromVoSpace"
+        protocol = "ivo://ivoa.net/vospace/core#httpget"
+        view = "cutout"
+        params = {"TARGET": target,
+                  "PROTOCOL": protocol,
+                  "DIRECTION": direction,
+                  "cutout": cutout,
+                  "view": view}
 
         try:
-            match = re.search('(\d+)\D(\d+)', obs.comment.frame)
-            if match is None:
-                print obs
-                continue
-            expnum = match.group(1)
-            ccd = int(match.group(2))
-            X = float(obs.comment.x)
-            Y = float(obs.comment.y)
-            x1 = int(max(1, X - args.width))
-            x2 = int(min(2112, X + args.width))
-            y1 = int(max(1, Y - args.width))
-            y2 = int(min(4644, Y + args.width))
+            r = requests.get(BASEURL, params=params, auth=(args.username, args.password))
+            r.raise_for_status()  # confirm the connection worked as hoped
+            postage_stamp_filename = "{}_{:11.5f}_{:09.5f}_{:+09.5f}.fits".format(obj.provisional_name,
+                                                                                  obs.date.mjd,
+                                                                                  obs.coordinate.ra.degrees,
+                                                                                  obs.coordinate.dec.degrees)
+            logging.info("{}".format(postage_stamp_filename))
+            with open(postage_stamp_filename, 'w') as tmp_file:
+                tmp_file.write(r.content)
+                # storage.copy(postage_stamp_filename, obj_dir)
+                # os.remove(postage_stamp_filename)   # easier not to have them hanging around
+        except requests.exceptions.HTTPError, e:
+            logging.error("{}".format(str(e)))
 
-            cutout = '[{}][{}:{},{}:{}]'.format(ccd, x1, x2, y1, y2)
-            full_image = storage.get_image(expnum, ccd=ccd, cutout=cutout)
-            postage_stamp = "{}/{}_{:11.5f}_{:09.5f}_{:+09.5f}.fits".format(
-                obj_dir,
-                obj.provisional_name,
-                obs.date.mjd,
-                obs.coordinate.ra.degrees,
-                obs.coordinate.dec.degrees)
-            print "{} -> {}".format(postage_stamp, full_image)
-            storage.vospace.link(full_image, postage_stamp)
-        except Exception as e:
-            sys.stderr.write(str(e))
+        break
 
     return
 
@@ -55,11 +67,18 @@ def link_cutout(obj, obj_dir, args):
 def main():
     parser = argparse.ArgumentParser(
         description='Parse a directory of TNO .ast files and create links in the postage stamp directory '
-                    'that allow retrieval of cutouts of the FITS images associated with the OSSOS detections.')
+                    'that allow retrieval of cutouts of the FITS images associated with the OSSOS detections. '
+                    'Cutouts are defined on the WCS RA/DEC of the object position.')
 
     parser.add_argument("version",
                         nargs='+',
                         help="The OSSOS data release version these stamps should be assigned to.")
+    parser.add_argument("username",
+                        nargs='+',
+                        help="VOSpace account username.")
+    parser.add_argument("password",
+                        nargs='+',
+                        help="VOSpace account password.")
     parser.add_argument("--ossin",
                         action="store",
                         default="vos:OSSOS/dbaseclone/ast/",
@@ -69,13 +88,22 @@ def main():
                         action="store",
                         default=["o3e", "o3o"],
                         choices=["o3e", "o3o", "O13BL", "Col3N"],
-                        help="Prefixes of object OSSOS designations to include.")
-    parser.add_argument("--width", '-w',
+                        help="Prefixes of object designations to include.")
+    parser.add_argument("--radius", '-r',
                         action='store',
-                        default=256 / 2.0,
-                        help='Dimension of cutout postage stamp.')
+                        default=0.02,
+                        help='Radius (degrees) of circle of cutout postage stamp.')
+    parser.add_argument("--debug", "-d",
+                        action="store_true")
+    parser.add_argument("--verbose", "-v",
+                        action="store_true")
 
     args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    elif args.verbose:
+        logging.basicConfig(level=logging.INFO)
 
     for fn in storage.listdir(args.ossin)[0:1]:
         obj = mpc.MPCReader(args.ossin + fn)  # let MPCReader's logic determine the provisional name
@@ -84,7 +112,7 @@ def main():
                 obj_dir = '{}/{}/{}'.format(storage.POSTAGE_STAMPS, args.version, obj.provisional_name)
                 if not storage.exists(obj_dir):
                     storage.mkdir(obj_dir)
-                link_cutout(obj, obj_dir, args)
+                cutout(obj, obj_dir, args)
 
 
 if __name__ == '__main__':
