53b9483d3fd658e974f81a8f9c804ed1af6e8875
mtbannister <michele@mso.anu.edu.au>
2015-01-11 23:29:52 -0800

    tno() now uses class methods to correctly instantiate from either a data release class file, or data release summary file.

--- src/ossos-pipeline/planning/plotting/parameters.py
+++ src/ossos-pipeline/planning/plotting/parameters.py
@@ -3,9 +3,9 @@ __author__ = 'Michele Bannister   git:@mtbannister'
 import os
 
 MPCORB_FILE = os.path.join(os.getenv('HOME', '/Users/michele/'), 'MPCORB-Distant.dat')
-L7MODEL = '/Users/michele/Dropbox/OSSOS/L7model-3.0-9.0'  # 'vos:OSSOS/CFEPS/L7SyntheticModel-v09.txt'
-REAL_KBO_AST_DIR = '/Users/michele/Dropbox/OSSOS/measure3/ossin/'
-MOST_RECENT_OSSOS_RELEASE = '/Users/michele/Dropbox/OSSOS/Release_summaries/ossos23sept2014.txt'
+L7MODEL = '/Users/michele/Dropbox/OSSOS/Release_summaries/L7model-3.0-9.0'  # 'vos:OSSOS/CFEPS/L7SyntheticModel-v09.txt'
+REAL_KBO_AST_DIR = '/Users/michele/Dropbox/OSSOS/measure3/ossin/'  # 'vos:OSSOS/dbaseclone/ast/'
+MOST_RECENT_OSSOS_RELEASE = '/Users/michele/Dropbox/OSSOS/Release_summaries/OSSOSv4/OSSOSv4summary.dat'
 PLOT_FIELD_EPOCH = 'Jun14.00'  # Jun14.00 ==> '0' days since the New Moon on Jun14
 
 PLOT_USNO_STARS = True
@@ -26,7 +26,14 @@ BLOCKS = {
     '13AO': {"RA": "15:58:01.35", "DEC": "-12:19:54.2"},  # O+0+0: image 1625346, ccd21 on May 8. O block are May 7,8.
     '13BL': {'RA': "00:54:00.00", "DEC": "+03:50:00.00"},  # 13B blocks are at their opposition locations
     '13BH': {'RA': "01:30:00.00", "DEC": "+13:00:00.00"},  # due to bad weather, discovery wasn't until Dec/Jan
-    '15AM': {'RA': "15:30:00.00", "DEC": "-12:20:00.0"}  # at its 2014 initial observation
+}
+
+FUTURE_BLOCKS = {
+    '15AM': {'RA': "15:30:00.00", "DEC": "-12:20:00.0"},  # at its 2014 initial observation
+    '15BD': {'RA': "03:15:00.00", "DEC": "+16:30:00.00"},
+    '15A?': {'RA': "14:45:00.00", "DEC": "-16:00:00.00"},
+    # '15A??': {'RA': "12:00:00.00", "DEC": "-1:00:00.00"},
+    '15B?': {'RA': "00:45:00.00", "DEC": "+00:05:00.00"},
 }
 
 DISCOVERY_NEW_MOON = 'Oct13'  # applies to the 13B blocks
@@ -54,24 +61,120 @@ NEWMOONS = {'Feb13': "2013/02/10 10:00:00",
             'Oct14': '2014/10/23 10:00:00',
             'Nov14': '2014/11/22 10:00:00',
             'Dec14': '2014/12/22 10:00:00',
+            'Jan15': '2015/01/20 10:00:00',
+            'Feb15': '2015/02/18 10:00:00',
+            'Mar15': '2015/03/19 10:00:00',
+            'Apr15': '2015/04/18 10:00:00',
+            'May15': '2015/05/17 10:00:00',
+            'Jun15': '2015/06/16 10:00:00',
+            'Jul15': '2015/07/15 10:00:00',
+            'Aug15': '2015/08/14 10:00:00',
+            'Sep15': '2015/09/12 10:00:00',
+            'Oct15': '2015/10/12 10:00:00',
+            'Nov15': '2015/11/11 10:00:00',
+            'Dec15': '2015/12/11 10:00:00',
+            'Jan16': '2016/01/09 10:00:00',
+            'Feb16': '2016/03/08 10:00:00',
+            'Mar16': '2016/03/09 10:00:00',
+            'Apr16': '2016/04/08 10:00:00',
+            'May16': '2016/05/07 10:00:00',
+            'Jun16': '2016/06/05 10:00:00',
+            'Jul16': '2016/07/05 10:00:00',
+            'Aug16': '2016/08/03 10:00:00',
+            'Sep16': '2016/09/01 10:00:00',
+            'Oct16': '2016/10/01 10:00:00',
+            'Nov16': '2016/11/01 10:00:00',
+            'Dec16': '2016/12/01 10:00:00',
+            'Jan17': '2017/01/01 10:00:00',
+            'Feb17': '2017/02/01 10:00:00',
 }
 
 
+class VersionError(Exception):
+    'An error with the TNO summary-file version'
+
+
 class tno(object):
-    def __init__(self):
-        self.classification = None
-        self.name = None
-        self.mag = None
-        self.mag_stdev = None
-        self.dist = None
-        self.dist_e = None
-        self.nobs = None
-        self.arclen = None
-        self.a = None
-        self.a_e = None
-        self.e = None
-        self.e_e = None
-        self.i = None
-        self.i_e = None
-        self.peri = None
-        self.diameter = None
+    ''' This takes two types of input lines, which populate almost the same but subtly different amounts of information.
+        Standard orbital elements are always populated.
+        Combining both class methods is the most reliable way of instantiating these objects from a data release.
+    '''
+
+    def __init__(self, name, mag, mag_stdev, dist, dist_e, nobs, arclen, av_xres, av_yres, max_x, max_y,
+                 a, a_e, e, e_e, i, i_e, node, node_e, argperi, argperi_e, time_peri, time_peri_e):
+        # self.classification = str(classification)
+        self.name = str(name)
+        self.mag = float(mag)
+        self.mag_stdev = float(mag_stdev)
+        self.dist = float(dist)
+        self.dist_e = float(dist_e)
+        self.nobs = int(nobs)
+        self.arclen = float(arclen)
+        self.av_xres = float(av_xres)
+        self.av_yres = float(av_yres)
+        self.max_x = float(max_x)
+        self.max_y = float(max_y)
+        self.a = float(a)
+        self.a_e = float(a_e)
+        self.e = float(e)
+        self.e_e = float(e_e)
+        self.i = float(i)
+        self.i_e = float(i_e)
+        self.node = float(node)
+        self.node_e = float(node_e)
+        self.argperi = float(argperi)
+        self.argperi_e = float(argperi_e)
+        self.time_peri = float(time_peri)
+        self.time_peri_e = float(time_peri_e)
+        self.peri = self.a * (1. - self.e)
+
+    @classmethod
+    def from_summary_line(cls, summaryLine, version=4):
+        ''' Summary format:
+        object  mag  stdev   dist  ..E nobs time av_xres av_yres max_x max_y
+        a ..E  e  ..E  i ..E    node     ..E    argperi     ..E           M        ..E   ra_dis  dec_dis
+        '''
+        if not summaryLine:
+            raise ValueError('No summary line given')
+        if version == 4:
+            params = summaryLine.split()
+            if len(params) != 25:
+                raise TypeError('Expected 25 columns, {0} given'.format(len(params)))
+            retval = cls(*params[0:23])
+            retval.ra_discov = params[23]
+            retval.dec_discov = params[24]
+        else:
+            raise VersionError('Unknown version "{0}"'.format(version))
+        assert retval
+        return retval
+
+    @classmethod
+    def from_class_line(cls, classLine, version=4):
+        '''
+        Class format:
+        class wrt n m security object  mag  stdev F H_sur  dist     ..E    nobs   time  av_xres av_yres max_x  max_y
+        a ..E        e  ..E      i      ..E      node     ..E   argperi      ..E   time_peri      ..E    rate
+        '''
+        if not classLine:
+            raise ValueError('No class file line given.')
+        if version == 4:
+            params = classLine.split()
+            if len(params) != 31:
+                raise TypeError('Expected 31 columns, {0} given'.format(len(params)))
+            input_params = params[5:8] + params[10:30]  # the elements that are in common
+            retval = cls(*input_params)
+            # FIXME: need to set a flag here that mag is average rather than average at discovery if instantiated
+            # this way
+            # or just have two mag settings: yes, that'd be better
+            retval.classification = params[0]
+            retval.wrt = params[1]
+            retval.n = int(params[2])
+            retval.m = int(params[3])
+            retval.security = params[4]
+            retval.filter = params[8]
+            retval.H = params[9]  # H_r magnitude, using the average m_r and the discovery geometry (same error as m_r)
+            retval.rate = params[30]
+        else:
+            raise VersionError('Unknown version "{0}"'.format(version))
+        assert retval
+        return retval
