7d272306fe24cf42f8624af80ee418d89becd9b1
JJ Kavelaars <jjk@uvic.ca>
2015-01-13 21:56:15 -0800

    Corrected arguments posted to cutout service, added getpasswd usage and adopted to astropy 0.4
     The Posted 'target' value was incorrectly using abreviated uri's those uri's only work with CADC VOSpace and are not considered
     valide 'target' argument for the cutout service run by the CADC.
    
     getpass enables non-echo request for password.  The cutout service only uses the password mechanism (not the certificate mechanism) for authentication.
    
     moved to astropy 0.4  (ie, removed use of 'degrees' instead of 'degree')

--- src/ossos-pipeline/scripts/postage_stamp_builder.py
+++ src/ossos-pipeline/scripts/postage_stamp_builder.py
@@ -12,44 +12,44 @@ ivo://ivoa.net/vospace/core%23httpget&view=cutout&cutout=CIRCLE+ICRS+242.1318+-1
 
 import argparse
 import logging
-
+import getpass
 import requests
 
 from ossos import mpc
 from ossos import storage
 
-
 BASEURL = "http://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/vospace/auth/synctrans"
 
 
-def cutout(obj, obj_dir, args):
+def cutout(obj, obj_dir, radius, username, password):
     for obs in obj.mpc_observations[10:11]:  # FIXME: TESTING ONLY
         if obs.null_observation:
             continue
         expnum = obs.comment.frame.split('p')[0]  # only want calibrated images
         # Using the WCS rather than the X/Y, as the X/Y can be unreliable on a long-term basis
-        cutout = "CIRCLE ICRS {} {} {}".format(obs.coordinate.ra.degrees,
-                                               obs.coordinate.dec.degrees,
-                                               args.radius)
+        this_cutout = "CIRCLE ICRS {} {} {}".format(obs.coordinate.ra.degree,
+                                               obs.coordinate.dec.degree,
+                                               radius)
+        print this_cutout
         # FIXME: should be able to use line below, but bug in VOSpace requires direct-access workaround, for now.
         # postage_stamp = storage.get_image(expnum, cutout=cutout)
 
-        target = storage.get_uri(expnum)
+        target = storage.vospace.fixURI(storage.get_uri(expnum))
         direction = "pullFromVoSpace"
         protocol = "ivo://ivoa.net/vospace/core#httpget"
         view = "cutout"
         params = {"TARGET": target,
                   "PROTOCOL": protocol,
                   "DIRECTION": direction,
-                  "cutout": cutout,
+                  "cutout": this_cutout,
                   "view": view}
         try:
-            r = requests.get(BASEURL, params=params, auth=(args.username, args.password))
+            r = requests.get(BASEURL, params=params, auth=(username, password))
             r.raise_for_status()  # confirm the connection worked as hoped
             postage_stamp_filename = "{}_{:11.5f}_{:09.5f}_{:+09.5f}.fits".format(obj.provisional_name,
                                                                                   obs.date.mjd,
-                                                                                  obs.coordinate.ra.degrees,
-                                                                                  obs.coordinate.dec.degrees)
+                                                                                  obs.coordinate.ra.degree,
+                                                                                  obs.coordinate.dec.degree)
             logging.info("{}".format(postage_stamp_filename))
             with open(postage_stamp_filename, 'w') as tmp_file:
                 tmp_file.write(r.content)
@@ -68,18 +68,11 @@ def main():
                     'Cutouts are defined on the WCS RA/DEC of the object position.')
 
     parser.add_argument("version",
-                        nargs='+',
                         help="The OSSOS data release version these stamps should be assigned to.")
-    parser.add_argument("username",
-                        nargs='+',
-                        help="VOSpace account username.")
-    parser.add_argument("password",
-                        nargs='+',
-                        help="VOSpace account password.")
     parser.add_argument("--ossin",
                         action="store",
                         default="vos:OSSOS/dbaseclone/ast/",
-                        help="The vospace containerNode that clones ossin's dbaseclone"
+                        help="The vospace containerNode that clones ossin dbaseclone"
                              "holding the .ast files of astrometry/photometry measurements.")
     parser.add_argument("--blocks", "-b",
                         action="store",
@@ -89,13 +82,16 @@ def main():
     parser.add_argument("--radius", '-r',
                         action='store',
                         default=0.02,
-                        help='Radius (degrees) of circle of cutout postage stamp.')
+                        help='Radius (degree) of circle of cutout postage stamp.')
     parser.add_argument("--debug", "-d",
                         action="store_true")
     parser.add_argument("--verbose", "-v",
                         action="store_true")
 
     args = parser.parse_args()
+
+    username = raw_input("CADC username: ")
+    password = getpass.getpass("CADC password: ")
 
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
@@ -109,8 +105,8 @@ def main():
                 obj_dir = '{}/{}/{}'.format(storage.POSTAGE_STAMPS, args.version[0], obj.provisional_name)
                 if not storage.exists(obj_dir, force=True):
                     storage.mkdir(obj_dir)
-                    # assert storage.exists(obj_dir, force=True)
-                cutout(obj, obj_dir, args)
+                # assert storage.exists(obj_dir, force=True)
+                cutout(obj, obj_dir, args.radius, username, password)
 
 
 if __name__ == '__main__':
