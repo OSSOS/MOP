46986f96521f5132a2b3f8710522d35f57cabda2
JJ Kavelaars <jjk@uvic.ca>
2015-01-26 08:36:33 -0800

    removed match_list from this script and moved into util for re-use. Also made the output of match files be in append mode so we can call astrom_mag_check multiple times to get .match files that are the result of mulitple runs

--- src/ossos-pipeline/pipeline/astrom_mag_check.py
+++ src/ossos-pipeline/pipeline/astrom_mag_check.py
@@ -17,6 +17,7 @@ from ossos.downloads.cutouts import ImageCutoutDownloader
 from ossos import storage
 import argparse
 import logging
+from ossos import  util
 
 from astropy.io import ascii
 
@@ -30,76 +31,12 @@ BRIGHT_LIMIT = 23.0
 OBJECT_PLANTED = "Object.planted"
 MINIMUM_BRIGHT_DETECTIONS = 5
 MINIMUM_BRIGHT_FRACTION = 0.5
-MATCH_TOLERANCE = 100.0
 
 # This image_slice_downloader handles pulling a small cutout of the image from VOSpace so we can do photometry
 # on this small chunk of the image.
 image_slice_downloader = ImageCutoutDownloader(slice_rows=60, slice_cols=60)
 
 
-def match_lists(pos1, pos2, tolerance=MATCH_TOLERANCE):
-    """
-    Given two sets of x/y positions match the lists, uniquely.
-
-    :rtype : numpy.ma, numpy.ma
-    :param pos1: list of x/y positions.
-    :param pos2: list of x/y positions.
-    :param tolerance: float distance, in pixels, to consider a match
-
-    Algorithm:
-        - Find all the members of pos2 that are within tolerance of pos1[idx1].
-                These pos2 members are match_group_1
-        - Find all the members of pos1 that are within tolerance of match_group_1[idx2].
-                These pos1 members are match_group_2
-        - If pos1[idx] is in match_group_2 then pos1[idx] is a match of object at match_group_1[idx2]
-
-    """
-
-    assert isinstance(pos1, numpy.ndarray)
-    assert isinstance(pos2, numpy.ndarray)
-
-    # build some arrays to hold the index of things that matched between lists.
-    npts1 = len(pos1[:, 0])
-    pos1_idx_array = numpy.arange(npts1, dtype=numpy.int16)
-    npts2 = len(pos2[:, 0])
-    pos2_idx_array = numpy.arange(npts2, dtype=numpy.int16)
-
-    # this is the array of final matched index, -1 indicates no match found.
-    match1 = numpy.ma.zeros(npts1, dtype=numpy.int16)
-    match1.mask = True
-
-    # this is the array of matches in pos2, -1 indicates no match found.
-    match2 = numpy.ma.zeros(npts2, dtype=numpy.int16)
-    match2.mask = True
-
-    for idx1 in range(npts1):
-
-        # compute the distance source idx1 to each member of pos2
-        sep = numpy.sqrt((pos2[:, 0] - pos1[idx1, 0]) ** 2 + (pos2[:, 1] - pos1[idx1, 1]) ** 2)
-
-        # considered a match if sep is below tolerance and is the closest match available.
-        match_condition = numpy.all((sep <= tolerance, sep == sep.min()), axis=0)
-
-        # match_group_1 is list of the indexes of pos2 entries that qualified as possible matches to pos1[idx1]
-        match_group_1 = pos2_idx_array[match_condition]
-
-        # For each of those pos2 objects that could be a match to pos1[idx] find the best match in all of pos1
-        for idx2 in match_group_1:
-            # compute the distance from this pos2 object that is a possible match to pos1[idx1] to all members of pos1
-            sep = numpy.sqrt((pos1[:, 0] - pos2[idx2, 0]) ** 2 + (pos1[:, 1] - pos2[idx2, 1]) ** 2)
-
-            # considered a match if sep is below tolerance and is the closest match available.
-            match_condition = numpy.all((sep <= tolerance, sep == sep.min()), axis=0)
-            match_group_2 = pos1_idx_array[match_condition]
-
-            # Are any of the pos1 members that were matches to the matched pos2 member the pos1[idx] entry?
-            if idx1 in match_group_2:
-                match1[idx1] = idx2
-                match2[idx2] = idx1
-                # this BREAK is in here since once we have a match we're done.
-                break
-
-    return match1, match2
 
 
 def measure_mags(measures, table_row):
@@ -177,7 +114,7 @@ def match_planted(fk_candidate_observations, match_filename, bright_limit=BRIGHT
 
     # match_idx is an order list.  The list is in the order of the first list of positions and each entry
     # is the index of the matching position from the second list.
-    (match_idx, match_fnd) = match_lists(numpy.array(planted_pos), numpy.array(found_pos))
+    (match_idx, match_fnd) = util.match_lists(numpy.array(planted_pos), numpy.array(found_pos))
     assert isinstance(match_idx, numpy.ma.MaskedArray)
     assert isinstance(match_fnd, numpy.ma.MaskedArray)
 
@@ -243,7 +180,12 @@ def match_planted(fk_candidate_observations, match_filename, bright_limit=BRIGHT
     except:
         std = "indef"
 
-    fout = storage.open_vos_or_local(match_filename, 'w')
+
+    if os.access(match_filename, os.R_OK):
+        fout = open(match_filename, 'a')
+    else:
+        fout = open(match_filename, 'w')
+
     fout.write("#K {:10s} {:10s}\n".format("EXPNUM", "FWHM"))
     for measure in detections[0].get_readings():
         fout.write('#V {:10s} {:10s}\n'.format(measure.obs.header['EXPNUM'], measure.obs.header['FWHM']))
@@ -266,7 +208,8 @@ def match_planted(fk_candidate_observations, match_filename, bright_limit=BRIGHT
                                                          n_bright_found,
                                                          offset,
                                                          std))
-    fpout = storage.open_vos_or_local(match_filename+".fp", 'w')
+
+    fpout = storage.open_vos_or_local(match_filename+".fp", 'a')
     try:
         writer = ascii.FixedWidth
         # add a hash to the start of line that will have header columns: for JMP
