5add18dbb0eb693e6344b7cb097c082ee4993835
mtbannister <michele@mso.anu.edu.au>
2015-01-29 13:38:37 -0800

    First step towards a cleanup of the overview web page code in preparation for a clean install on lark.

---
+++ src/ossos-pipeline/web/initiate/populate_dbimages.py
@@ -0,0 +1,68 @@
+# !/usr/bin/env python
+# ###############################################################################
+##                                                                            ##
+## Copyright 2013 by its authors                                              ##
+## See COPYING, AUTHORS                                                       ##
+##                                                                            ##
+## This file is part of OSSOS Moving Object Pipeline (OSSOS-MOP)              ##
+##                                                                            ##
+##    OSSOS-MOP is free software: you can redistribute it and/or modify       ##
+##    it under the terms of the GNU General Public License as published by    ##
+##    the Free Software Foundation, either version 3 of the License, or       ##
+##    (at your option) any later version.                                     ##
+##                                                                            ##
+##    OSSOS-MOP is distributed in the hope that it will be useful,            ##
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of          ##
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           ##
+##    GNU General Public License for more details.                            ##
+##                                                                            ##
+##    You should have received a copy of the GNU General Public License       ##
+##    along with OSSOS-MOP.  If not, see <http://www.gnu.org/licenses/>.      ##
+##                                                                            ##
+################################################################################
+"""Populate the dbimages directory with new images acquired by this project"""
+
+import argparse
+import urllib
+import tempfile
+
+from astropy.io.votable import parse
+
+from ossos import storage
+
+
+parser = argparse.ArgumentParser(description="Make links in the dbimages directory for a given set of observations")
+parser.add_argument('date', nargs='?', action='store', default='2013-01-01')
+parser.add_argument('--runid', nargs='*', action='store', default=list(('13AP05', '13AP06')))
+parser.add_argument('--cal', action='store', default="RAW")
+opt = parser.parse_args()
+
+runids = tuple(opt.runid)
+
+mjd_yesterday = 56401.5841892
+
+data = {
+    "QUERY": """SELECT Observation.target_name as TargetName, COORD1(CENTROID(Plane.position_bounds)) AS RA,
+COORD2(CENTROID(Plane.position_bounds)) AS DEC, Plane.time_bounds_cval1 AS StartDate, Plane.time_exposure AS
+ExposureTime, Observation.instrument_name AS Instrument, Plane.energy_bandpassName AS Filter,
+Observation.collectionID AS dataset_name, Observation.proposal_id AS ProposalID, Observation.proposal_pi AS PI FROM
+caom.Observation AS Observation JOIN caom.Plane AS Plane ON Observation.obsID = Plane.obsID WHERE  (
+Observation.collection = 'CFHT' ) AND Plane.time_bounds_cval1 > %d AND Plane.observable_ctype='%s' AND
+Observation.proposal_id IN %s """ % (
+        mjd_yesterday, opt.cal, str(tuple(opt.runid)) ),
+    "REQUEST": "doQuery",
+    "LANG": "ADQL",
+    "FORMAT": "votable"}
+
+url = "http://www.cadc.hia.nrc.gc.ca/tap/sync?" + urllib.urlencode(data)
+
+tmpFile = tempfile.NamedTemporaryFile()
+
+urllib.urlretrieve(url, tmpFile.name)
+
+vot = parse(tmpFile.name).get_first_table()
+vot.array.sort(order='StartDate')
+t = vot.array
+
+for row in t.data:
+    storage.populate(row['dataset_name'])
---
+++ src/ossos-pipeline/web/overview/__init__.py
@@ -0,0 +1,34 @@
+# package
+
+from pyramid.config import Configurator
+from pyramid.security import Allow, Authenticated
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+
+
+class Root(object):
+    __acl__ = [
+        (Allow, Authenticated, 'ossos')
+    ]
+
+    def __init__(self, request):
+        self.request = request
+
+
+def main(global_config, **settings):
+    config = Configurator(settings=settings,
+                          root_factory=Root  # then it knows what __acl__ it can have
+    )
+
+    authn_policy = AuthTktAuthenticationPolicy('ossos is wonderful', hashalg='sha512')
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(ACLAuthorizationPolicy())
+
+    # then continue as normal
+    config.add_route(name='overview', path='/')
+    config.scan(package='web')
+
+    # the first place it will go is ossos.auth.model.py's @view_config 'login' 
+    # which has a def login_view(request) which we want to set first.
+
+    return config.make_wsgi_app()
