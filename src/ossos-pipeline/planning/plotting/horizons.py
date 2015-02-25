bd1fdeb98c42242f9794a5f36906ee1979036e8b
mtbannister <michele@mso.anu.edu.au>
2015-02-25 09:32:17 -0800

    CSV parsing now handled cleanly. Output changed to pandas DataFrame.

--- src/ossos-pipeline/planning/plotting/horizons.py
+++ src/ossos-pipeline/planning/plotting/horizons.py
@@ -2,12 +2,17 @@
 # Query JPL Horizons via the web service
 # Wes Fraser, 2010
 # rewritten and documented by Michele Bannister, Dec 2010
+# rewritten again by MB, Jan 2015
 
 import urllib2 as url
 import time
-from collections import OrderedDict
+import io
+
+import pandas as pd
+
 
 '''
+Ephemeris parameters:
 1. Astrometric RA & DEC  15. Sun sub-long & sub-lat  29. Constellation ID
  *2. Apparent RA & DEC     16. Sub Sun Pos. Ang & Dis  30. Delta-T (CT - UT)
   3.   Rates; RA & DEC     17. N. Pole Pos. Ang & Dis *31. Obs eclip. lon & lat
@@ -24,32 +29,47 @@ from collections import OrderedDict
  14. Obs sub-lng & sub-lat 28. Orbit plane angle       42. Local app. hour angle
 '''
 
-PARAMS = OrderedDict([('RA-DEC', 1),  # can't actually find docs for second RA/DEC values. Think it's diff base system
-                      ('RA_rate-DEC_rate', 3),
-                      ('v_mag', 9),
-                      ('r_helio-r_helio_rate', 19),
-                      ('RA_uncert-DEC_uncert', 36),  # RA and DEc uncertainties are 3-sigma
-])
+# PARAMS = OrderedDict([('RA-DEC', 1),                    # Units: HMS (HH MM SS.ff) and DMS (DD MM SS.f)
+# ('RA_rate-DEC_rate', 3),          # d(RA)/dt is multiplied by the cosine of the declination.
+#                                                         # Units: ARCSECONDS PER HOUR
+#                       ('v_mag-surface_brightness', 9),  # Units: None and VISUAL MAGNITUDES PER SQUARE ARCSECOND
+#                       ('r_helio-r_helio_rate', 19),
+#                       ('RA_uncert-DEC_uncert', 36),     # RA and DEc uncertainties are 3-sigma. Units: ARCSECONDS
+# ])
 
-# Stores the indexed values returned by Horizons web-interface
-def parse_ephemeris(horizons_string, params):
-    retval = {}
-    keys = ['DATE']  # always there
-    for p in params:
-        keys += p.split('-')  # take care of the case where one parameter returns two values, eg. ra-dec
-    i = 0
-    for s in horizons_string.split(','):
-        if s in [" ", "\n"]:
-            continue
-        retval[keys[i]] = s
-        i += 1
-    return retval
+def parse_orbital_elements(urlData):
+    # Get required information back out of the page Horizons returns
+    # Extract the orbital elements and epoch of origin - works as long as the name parses, even if the ephemeris
+    # request fails
+    epoch_possible = False
+    for i in range(len(urlData)):
+        S = urlData[i].split()
+        if len(S) > 0:
+            if S[0] == 'EPOCH=':  # moons don't have epochs so this can't be required
+                epoch_possible = True
+                epochStr = "/".join(S[3].split('-'))
+                S = urlData[i + 1].split('=')
+                e = float(S[1].split(' QR')[0])
+                S = urlData[i + 2].split('=')
+                Omega = float(S[1].strip(' W'))
+                W = float(S[2].strip(' IN'))
+                inc = float(S[3])
+                S = urlData[i + 3].split('=')
+                a = float(S[1].strip(' MA'))
+                M = float(S[2].strip(' ADIST'))
+                break
+
+    if epoch_possible:
+        return {'a': a, 'e': e, 'i': inc, 'Omega': Omega, 'W': W, 'M': M, 'Epoch': epochStr}
+    else:
+        return None
+
 
 # Run a Horizons query
 # eg. output = batch("Haumea", "2010-12-28 10:00", "2010-12-29 10:00", 1, su='d')
 
 def batch(object, t, T, step, su='d',
-          params=['RA-DEC', 'RA_rate-DEC_rate', 'v_mag', 'r_helio-r_helio_rate', 'RA_uncert-DEC_uncert']):
+          params=[1, 3, 9, 19, 36]):
     if step == None:  # default
         step = 1
     else:
@@ -63,7 +83,7 @@ def batch(object, t, T, step, su='d',
             s += "40'"  # python leaves one off the end in range()
     else:
         for p in params:
-            s += "{},".format(PARAMS[p])
+            s += "{},".format(p)
 
     # The pieces of the url that Horizons needs for its processing instructions. Leave intact.
     urlArr = ["http://ssd.jpl.nasa.gov/horizons_batch.cgi?batch=1&COMMAND=",
@@ -112,59 +132,32 @@ def batch(object, t, T, step, su='d',
         else:
             done = 1
 
-    # print urlData  #testing
+    EPHEM_CSV_START_MARKER = '$$SOE'
+    EPHEM_CSV_END_MARKER = '$$EOE'
+    ephemCSV_start = None
+    ephemCSV_end = None
+    for i, dataLine in enumerate(urlData):
+        if dataLine.strip() == EPHEM_CSV_START_MARKER:
+            ephemCSV_start = i
+        elif dataLine.strip() == EPHEM_CSV_END_MARKER:
+            ephemCSV_end = i
+    assert ephemCSV_start is not None, 'No ephem start'
+    assert ephemCSV_end is not None, 'No ephem end'
+    assert ephemCSV_start < ephemCSV_end, 'Ephem are a bit odd'
+    # The header, the lines *after* the start marker, up to (but not including, because it is a slice) the end marker
+    csv_lines = [urlData[ephemCSV_start - 2]] + urlData[ephemCSV_start + 1: ephemCSV_end]
+    ephemCSV = ''.join(csv_lines)
+    ephemCSVfile = io.BytesIO(ephemCSV)
+    ephemerides = pd.DataFrame.from_csv(ephemCSVfile)
 
-    # Get required information back out of the page Horizons returns
-    # Extract the orbital elements and epoch of origin - works as long as the name parses, even if the ephemeris
-    # request fails
-    epoch_possible = False
-    for i in range(len(urlData)):
-        S = urlData[i].split()
-        if len(S) > 0:
-            if S[0] == 'EPOCH=':  # moons don't have epochs so this can't be required
-                epoch_possible = True
-                epochStr = "/".join(S[3].split('-'))
-                S = urlData[i + 1].split('=')
-                e = float(S[1].split(' QR')[0])
-                S = urlData[i + 2].split('=')
-                Omega = float(S[1].strip(' W'))
-                W = float(S[2].strip(' IN'))
-                inc = float(S[3])
-                S = urlData[i + 3].split('=')
-                a = float(S[1].strip(' MA'))
-                M = float(S[2].strip(' ADIST'))
-                break
-
-    if epoch_possible:
-        orbital_elements = {'a': a, 'e': e, 'i': inc, 'Omega': Omega, 'W': W, 'M': M, 'Epoch': epochStr}
-    else:
-        orbital_elements = None
-
-    # Keep the lines in urlData that have ephemeris information
-    request_worked = False
-    for i in range(len(urlData)):
-        # '$$SOE\n' and '$$EOE\n' bracket the returned ephemerides if the request worked
-        if urlData[i] == '$$SOE\n':
-            request_worked = True
-            start = i + 1
-        if request_worked and urlData[i] == '$$EOE\n':
-            end = i - 1
-    # Send back the problem if the request didn't work (catch this)
-    if request_worked is False:
-        raise EnvironmentError, 'Horizons request did not return ephemerides. Check the problem: ' + urlData[24]
-
-    # Transfer the good lines to the dictionary, which makes all the values indexed and accessible
-    ephemerides = []
-    for line in urlData[start:end + 1]:
-        ephemerides.append(parse_ephemeris(line, params))
+    orbital_elements = parse_orbital_elements(urlData)
 
     return orbital_elements, ephemerides
 
 # Run the script from the command line
 if __name__ == "__main__":
-    elems, ephems = batch("Haumea", "2010-12-28 10:00", "2010-12-30 10:00", 1, su='d')  # for example
+    elems, ephems = batch("Makemake", "2010-12-28 10:00", "2010-12-30 10:00", 1, su='d')  # for example
 
     # just a few test print statements
-    print elems  # elements
-    for ephemeris in ephems:
-        print ephemeris['DATE'], ephemeris['RA'], ephemeris['DEC'], ephemeris['RA_uncert'], ephemeris['DEC_uncert']
+    print(elems)  # elements
+    print(ephems.keys())
