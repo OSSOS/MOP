b84431095809150e1f08d0555147d59317b71290
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2015-05-14 21:29:23 -0700

    Modified the CoordinateConverter to use a symtric math opperation

--- src/ossos-pipeline/ossos/downloads/cutouts/calculator.py
+++ src/ossos-pipeline/ossos/downloads/cutouts/calculator.py
@@ -4,15 +4,34 @@ __author__ = "David Rusk <drusk@uvic.ca>"
 
 
 class CoordinateConverter(object):
-    def __init__(self, x_offset, y_offset):
+    def __init__(self, x_offset, y_offset, invert=False):
         """
 
         @type y_offset: float
         @type x_offset: float
         """
-        self.x_offset = x_offset
-        self.y_offset = y_offset
-        logger.debug("Convert initialized as dx,dy {},{}".format(x_offset, y_offset))
+        self._dx = x_offset
+        self._dy = y_offset
+        self._invert = invert and -1 or 1
+        logger.debug("Convert initialized as x_offset, y_offset {},{}".format(self.x_offset, self.y_offset))
+
+    @property
+    def x_offset(self):
+        """
+        Amount to subtract from x position to move to new reference frame.
+
+        @rtype: float
+        """
+        return self._invert * (self._dx - 1)
+
+    @property
+    def y_offset(self):
+        """
+        Amount to subtract from the y position to move to the new reference frame.
+
+        @rtype: float
+        """
+        return self._invert * (self._dy - 1)
 
     def convert(self, point):
         """
@@ -28,18 +47,20 @@ class CoordinateConverter(object):
 
         Example: convert coordinate from original image into a pixel location
           within a cutout image.
-        @type point: list(float,float)
+
+        @rtype: list(float,float)
         """
         x, y = point
-        (x1, y1) = x - (self.x_offset - 1), y - (self.y_offset - 1)
-        logger.debug("converted {} {} to {} {}".format(x, y, x1, y1))
+        (x1, y1) = x - self.x_offset, y - self.y_offset
+        logger.debug("converted {} {} ==> {} {}".format(x, y, x1, y1))
         return x1, y1
 
     def get_inverse_converter(self):
         """
         Returns a converter object for converting back from this converter's
         output coordinate system to its input coordinate system.
+
         @rtype : CoordinateConverter
         """
 
-        return CoordinateConverter(-self.x_offset, -self.y_offset)
+        return CoordinateConverter(self.x_offset, self.y_offset, invert=True)
