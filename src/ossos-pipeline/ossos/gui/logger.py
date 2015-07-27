4ec5493693d8f1ee4bafe7aaec2c43bcf3d28abe
JJ Kavelaars <jjk@uvic.ca>
2015-07-27 08:12:16 -0700

    Made logging format more useful for debuging.  Also turned on 'ERROR' debugging in VOSpace calls.

--- src/ossos-pipeline/ossos/gui/logger.py
+++ src/ossos-pipeline/ossos/gui/logger.py
@@ -10,18 +10,21 @@ import os
 import tempfile
 
 # set logging level and shunt to a file
-OSSOS_DEBUG_LEVEL = logging.CRITICAL
+logging.getLogger('vos').setLevel(logging.ERROR)
 
 logfile = os.path.join(tempfile.gettempdir(), "ossos_validation.log")
 logging.basicConfig(filename=logfile,
-                    format="%(levelname)s: %(module)s.%(funcName)s %(lineno)d: %(message)s (%(asctime)s)",
+                    format="%(levelname)s: %(thread)d %(module)s.%(funcName)s %(lineno)d: %(message)s (%(asctime)s)",
                     datefmt="%m/%d/%Y %I:%M:%S %p",
                     level=logging.ERROR)
 
 _logger = logging.getLogger(__name__)
-_logger.setLevel(OSSOS_DEBUG_LEVEL)
 debug = _logger.debug
 info = _logger.info
 warning = _logger.warning
 error = _logger.error
 critical = _logger.critical
+
+
+def set_debug():
+    logging.getLogger(__name__).setLevel(logging.DEBUG)
