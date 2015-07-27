4f956a0c654626fedbcf94314a65333b02537287
JJ Kavelaars <jjk@uvic.ca>
2015-07-27 07:56:27 -0700

    Removed some of the un-used matplotlib display functoins, centralized the connection to ds9.
    We moved to using DS9 a few versions back but had not removed the unused matplotlib calls.
    For ds9 the varous calls had been inserted into the code where ever handy, now all connections
    to ds9 are handled via a connection setup inside the 'imageview' module.
    
    Call to align images now part of the baseviewer which must be initialized with a connection
    to the ds9 display.  The baseviewer is created inside the imageview object.  The viewer object
    then passes that ds9 connection to the Displayable when an actual display occurs.
    
    Class structure is something like:  ImageView -> SingletViewer -> DisplayableImageSingle

--- src/ossos-pipeline/ossos/fitsviewer/displayable.py
+++ src/ossos-pipeline/ossos/fitsviewer/displayable.py
@@ -1,18 +1,13 @@
-from cStringIO import StringIO
 import copy
 import logging
 import tempfile
+
 from astropy import units
-from astropy.io import fits
-import pyds9 as ds9
-
-__author__ = "David Rusk <drusk@uvic.ca>"
-
-import matplotlib.pyplot as plt
+from astropy.units import Quantity
 
 from .colormap import GrayscaleColorMap
 from .exceptions import MPLViewerError
-from .interaction import InteractionContext, Signal
+from .interaction import Signal
 
 
 class Displayable(object):
@@ -23,11 +18,13 @@ class Displayable(object):
       figure: matplotlib figure the images are placed on.
     """
 
-    def __init__(self):
+    def __init__(self, display):
         self.figure = None
         # self.figure = plt.figure()  [stop using the matplotlib plt]
         self.canvas = None
+        self.display = display
         self.rendered = False
+        self.aligned = False
 
     @property
     def width(self):
@@ -37,29 +34,16 @@ class Displayable(object):
     def height(self):
         raise NotImplementedError()
 
-    def render(self, canvas=None):
+    def align(self, pos):
+        if not self.aligned:
+            self._do_align(pos)
+
+    def render(self):
         if not self.rendered:
             self._do_render()
 
-        # if canvas is None:
-        #     pass
-        #     #plt.show()
-        # else:
-        #     self.canvas = canvas
-        #     canvas.figure = self.figure
-        #
-        #     parent_size = canvas.GetClientSize()
-        #
-        #     figure_dpi = self.figure.get_dpi()
-        #     self.figure.set_size_inches(parent_size[0] / figure_dpi,
-        #                                 parent_size[1] / figure_dpi)
-        #
-        #     self._apply_event_handlers(canvas)
-
     def redraw(self):
         pass
-        # if self.canvas is not None:
-        #     self.canvas.draw()
 
     def place_error_ellipse(self, x, y, a, b, pa, color='y'):
         pass
@@ -73,6 +57,9 @@ class Displayable(object):
     def _do_render(self):
         raise NotImplementedError()
 
+    def _do_align(self, pos):
+        raise NotImplementedError()
+
     def _apply_event_handlers(self, canvas):
         pass
 
@@ -83,13 +70,11 @@ class ImageSinglet(object):
 
     """
 
-    def __init__(self, hdulist, figure, rect):
+    def __init__(self, hdulist):
         self.hdulist = hdulist
-        self.figure = figure
-        self.axes = self._create_axes(rect)
-        #self.figure.add_axes(self.axes)
 
         self.marker = None
+        self._display = None
 
         self.display_changed = Signal()
         self.xy_changed = Signal()
@@ -109,84 +94,85 @@ class ImageSinglet(object):
     def height(self):
         return _image_height(self.hdulist)
 
-    def show_image(self, colorbar=False):
-        # start xpans if needed
-        ds9.ds9_xpans()
-        # start ds9 if need, or connect to existing
-        display = None
-        cnt = 0
-        while display is None and cnt < 10:
-            cnt += 1
-            try:
-                display = ds9.ds9(target='validate')
-                cnt = 10
-            except ValueError as ve:
-                logging.error(str(ve))
-                pass
-        if self.frame_number is None:
-            # display.set('frame delete all')
-            display.set('frame new')
-            display.set('scale zscale')
-            display.set('cmap invert yes')
+    @staticmethod
+    def align(ds9, pos):
+        ra, dec = pos
+        if isinstance(ra, Quantity):
+            ra = ra.to(units.degree).value
+        if isinstance(dec, Quantity):
+            dec = dec.to(units.degree).value
+        ds9.set("pan to {} {} wcs fk5".format(ra, dec))
+        ds9.set('frame match wcs')
 
-            # create a copy of the image that does not have PV keywords, ds9 fails on those.
+    def show_image(self, ds9, colorbar=False):
+        display = ds9
+        if colorbar:
+            display.set('view colorbar yes')
+
+        if self.frame_number is None:
+            display.set('frame new')
+            display.set('scale histequ')
+            display.set('cmap cool')
+
+            # create a copy of the image that does not have Gwyn's PV keywords, ds9 fails on those.
             hdulist = copy.copy(self.hdulist)
             for hdu in hdulist:
-                del(hdu.header['PV*'])
+                del (hdu.header['PV*'])
+
+            # place in a temporary file for ds9 to use.
             f = tempfile.NamedTemporaryFile(suffix=".fits")
             hdulist.writeto(f, output_verify='ignore')
             f.flush()
             f.seek(0)
+
+            # load into display
             try:
-                display.set('file mosaicimage {}'.format(f.name))
+                display.set('mosaicimage {}'.format(f.name))
             except ValueError as ex:
                 logging.error("Failed while trying to display: {}".format(hdulist))
                 logging.error("{}".format(ex))
             f.close()
-            del(f)
-            del(hdulist)
+            del f
+            del hdulist
             self.frame_number = display.get('frame frameno')
             display.set('frame center {}'.format(self.frame_number))
             display.set('zoom to fit')
             display.set('wcs sky fk5')
             display.set('frame match wcs')
+
         display.set('frame frameno {}'.format(self.frame_number))
 
-        self._interaction_context = InteractionContext(self)
+        # self._interaction_context = InteractionContext(self)
 
         self.number_of_images_displayed += 1
 
-    def clear_markers(self):
-        display = ds9.ds9(target='valdiate')
+    @staticmethod
+    def clear_markers(ds9):
+        display = ds9
         display.set('regions delete all')
 
-    def place_marker(self, x, y, radius, colour="b"):
+    def place_marker(self, x, y, radius, colour="b", ds9=None):
         """
         Draws a marker with the specified dimensions.  Only one marker can
         be on the image at a time, so any existing marker will be replaced.
         """
-        display = ds9.ds9(target='validate')
+        display = ds9
         colour_string = {'r': 'red', 'b': 'blue'}.get(colour, 'green')
-        display.set('regions', 'wcs; circle({},{},{}) # color={}'.format(x, y, radius, colour_string))
-
-        #if self.marker is not None:
-        #    self.marker.remove_from_axes(self.axes)
-        #
-        #self.marker = Marker(x, y, radius, colour=colour)
-        #self.marker.add_to_axes(self.axes)
+        if isinstance(x, Quantity) and x.unit == units.degree:
+            display.set('regions', 'wcs; circle({},{},{}) # color={}'.format(x.value, y.value,
+                                                                             radius, colour_string))
+        else:
+            display.set('regions', 'image; circle({},{},{}) # color={}'.format(x, y, radius, colour_string))
 
         self.display_changed.fire()
 
-    def place_error_ellipse(self, x, y, a, b, pa, color='b'):
+    def place_error_ellipse(self, x, y, a, b, pa, color='b', ds9=None):
         """
         Draws an ErrorEllipse with the given dimensions.  Can not be moved later.
         """
-        display = ds9.ds9(target='validate')
-        # display.set('regions delete all')
+        display = ds9
         ell = 'ellipse({},{},{},{},{}'.format(x, y, a, b, pa.to(units.degree).value + 90)
-        display.set('regions', 'image ;{}'.format(ell))
-        #self.error_ellipse = ErrEllipse(x, y, a, b, pa, color=color)
-        #self.error_ellipse.add_to_axes(self.axes)
+        display.set('regions', 'image ;{} # color={}'.format(ell, color))
         self.display_changed.fire()
 
     def update_marker(self, x, y, radius=None):
@@ -227,35 +213,20 @@ class ImageSinglet(object):
         self.display_changed.fire()
 
     def is_event_in_axes(self, event):
-        return self.axes == event.inaxes
+        raise NotImplemented()
 
     def register_mpl_event_handler(self, eventname, handler):
         return 0
-#        handler_id = self.figure.canvas.mpl_connect(eventname, handler)
-#        self._mpl_event_handlers[handler_id] = (eventname, handler)
-#        return handler_id
 
     def deregister_mpl_event_handler(self, id_):
-        return
-#        self.figure.canvas.mpl_disconnect(id_)
-#        del self._mpl_event_handlers[id_]
+        pass
 
     def apply_event_handlers(self, canvas):
         for eventname, handler in self._mpl_event_handlers.itervalues():
             canvas.mpl_connect(eventname, handler)
 
-    def _create_axes(self, rect):
-        """
-        Args:
-          rect: [left, bottom, width, height]
-            Used to construct the matplotlib axes.
-        """
-        return None
-
     def _refresh_displayed_colormap(self):
-        self.axes_image.set_cmap(self._colormap.as_mpl_cmap())
-        self.axes_image.changed()
-        self.display_changed.fire()
+        raise NotImplemented()
 
 
 class DisplayableImageSinglet(Displayable):
@@ -268,17 +239,14 @@ class DisplayableImageSinglet(Displayable):
         See also Displayable's attributes.
     """
 
-    def __init__(self, hdulist):
+    def __init__(self, hdulist, display=None):
         """
         Args:
           hdulist: astropy.io.fits.HDUList
             The FITS image to be displayed.
         """
-        super(DisplayableImageSinglet, self).__init__()
-
-        self.hdulist = hdulist
-        self.image_singlet = ImageSinglet(self.hdulist, self.figure,
-                                          [0.025, 0.025, 0.95, 0.95])
+        super(DisplayableImageSinglet, self).__init__(display)
+        self.image_singlet = ImageSinglet(hdulist)
         self.image_singlet.display_changed.connect(self.redraw)
         self.marker_placed = False
         self.ellipse_placed = False
@@ -292,20 +260,20 @@ class DisplayableImageSinglet(Displayable):
     def focus_released(self):
         return self.image_singlet.focus_released
 
-    def place_marker(self, x, y, radius, colour="b"):
+    def place_marker(self, x, y, radius, colour="g"):
         if not self.marker_placed:
-            self.image_singlet.place_marker(x, y, radius, colour=colour)
+            self.image_singlet.place_marker(x, y, radius, colour=colour, ds9=self.display)
             self.marker_placed = True
 
-    def place_annulus(self, x, y, radii, colour='b'):
+    def place_annulus(self, x, y, radii, colour='g'):
         if not self.annulus_placed:
             for radius in radii:
-                self.image_singlet.place_marker(x, y, radius, colour=colour)
+                self.image_singlet.place_marker(x, y, radius, colour=colour, ds9=self.display)
             self.annulus_placed = True
 
-    def place_error_ellipse(self, x, y, a, b, pa, color='b'):
+    def place_error_ellipse(self, x, y, a, b, pa, color='g'):
         if not self.ellipse_placed:
-            self.image_singlet.place_error_ellipse(x, y, a, b, pa, color=color)
+            self.image_singlet.place_error_ellipse(x, y, a, b, pa, color=color, ds9=self.display)
             self.ellipse_placed = True
 
     def reset_colormap(self):
@@ -315,23 +283,33 @@ class DisplayableImageSinglet(Displayable):
         self.image_singlet.toggle_reticule()
 
     def _do_render(self):
-        self.image_singlet.show_image(colorbar=True)
+        self.image_singlet.show_image(ds9=self.display, colorbar=False)
+
+    def _do_align(self, pos):
+        self.image_singlet.align(ds9=self.display, pos=pos)
+        self.aligned = True
 
     def _apply_event_handlers(self, canvas):
         self.image_singlet.apply_event_handlers(canvas)
 
 
 class DisplayableImageTriplet(Displayable):
-    def __init__(self, cutout_grid):
-        super(DisplayableImageTriplet, self).__init__()
+
+    def width(self):
+        pass
+
+    def height(self):
+        pass
+
+    def __init__(self, cutout_grid, display):
+        super(DisplayableImageTriplet, self).__init__(display)
 
         if cutout_grid.shape != (3, 3):
             raise ValueError("Must be a 3 by 3 grid (was given %d by %d)"
                              % (cutout_grid.shape[0], cutout_grid.shape[1]))
 
         self.cutout_grid = cutout_grid
-
-        d  = ds9.ds9('validate')
+        d = self.display
         d.set('frame delete all')
         d.set('tile yes')
         d.set('tile grid layout 3 3')
@@ -340,10 +318,7 @@ class DisplayableImageTriplet(Displayable):
         for frame_index in range(num_frames):
             frame = []
             for time_index in range(num_times):
-                singlet = ImageSinglet(cutout_grid.get_hdulist(frame_index, time_index),
-                                       self.figure,
-                                       get_rect(cutout_grid.shape, frame_index, time_index,
-                                                spacing=0))
+                singlet = ImageSinglet(cutout_grid.get_hdulist(frame_index, time_index))
                 singlet.display_changed.connect(self.redraw)
                 frame.append(singlet)
 
@@ -390,6 +365,7 @@ class ErrEllipse(object):
     """
     A class for creating and drawing an ellipse in matplotlib.
     """
+
     def __init__(self, x_cen, y_cen, a, b, pa, color='b'):
         """
         :param x_cen: x coordinate at center of the ellipse
@@ -405,152 +381,20 @@ class ErrEllipse(object):
         self.pa = pa
         self.angle = self.pa - 90
 
-
     def add_to_axes(self, axes):
         return None
-
-
-class Depricated_Marker(object):
-    def __init__(self, x, y, radius, colour="b"):
-        self.circle = plt.Circle((x, y), radius, color=colour, fill=False)
-
-        self.crosshair_scaling = 2
-
-        crosshair_colour = "w"
-        linewidth = 1
-
-        self.left_hair = plt.Line2D(
-            self._get_left_x_extent(),
-            self._get_horizontal_y_extent(),
-            color=crosshair_colour,
-            linewidth=linewidth)
-
-        self.right_hair = plt.Line2D(
-            self._get_right_x_extent(),
-            self._get_horizontal_y_extent(),
-            color=crosshair_colour,
-            linewidth=linewidth)
-
-        self.top_hair = plt.Line2D(
-            self._get_vertical_x_extent(),
-            self._get_top_y_extent(),
-            color=crosshair_colour,
-            linewidth=linewidth)
-
-        self.bottom_hair = plt.Line2D(
-            self._get_vertical_x_extent(),
-            self._get_bottom_y_extent(),
-            color=crosshair_colour,
-            linewidth=linewidth)
-
-    @property
-    def x(self):
-        return self.circle.center[0]
-
-    @property
-    def y(self):
-        return self.circle.center[1]
-
-    @property
-    def center(self):
-        return self.circle.center
-
-    @center.setter
-    def center(self, new_center):
-        self.circle.center = new_center
-        self._update_cross()
-
-    @property
-    def radius(self):
-        return self.circle.radius
-
-    @radius.setter
-    def radius(self, new_radius):
-        self.circle.radius = new_radius
-        self._update_cross()
-
-    @property
-    def lines(self):
-        return [self.left_hair, self.right_hair,
-                self.top_hair, self.bottom_hair]
-
-    def add_to_axes(self, axes):
-        def transform(line):
-            line.set_transform(axes.transData)
-
-        axes.add_patch(self.circle)
-
-        for line in self.lines:
-            transform(line)
-
-        axes.lines.extend(self.lines)
-
-    def remove_from_axes(self, axes):
-        self.circle.remove()
-
-        for line in self.lines:
-            axes.lines.remove(line)
-
-    def contains(self, event):
-        return self.circle.contains(event)
-
-    def toggle_reticule(self):
-        self.circle.set_visible(not self.circle.get_visible())
-        for line in self.lines:
-            line.set_visible(not line.get_visible())
-
-    def _get_vertical_x_extent(self):
-        return self.x, self.x
-
-    def _get_bottom_y_extent(self):
-        bottom = self.y - self.radius
-        top = bottom + self.radius / self.crosshair_scaling
-        return bottom, top
-
-    def _get_top_y_extent(self):
-        top = self.y + self.radius
-        bottom = top - self.radius / self.crosshair_scaling
-        return bottom, top
-
-    def _get_horizontal_y_extent(self):
-        return self.y, self.y
-
-    def _get_left_x_extent(self):
-        left = self.x - self.radius
-        right = left + self.radius / self.crosshair_scaling
-        return left, right
-
-    def _get_right_x_extent(self):
-        right = self.x + self.radius
-        left = right - self.radius / self.crosshair_scaling
-        return left, right
-
-    def _update_cross(self):
-        self.left_hair.set_data(
-            self._get_left_x_extent(),
-            self._get_horizontal_y_extent())
-
-        self.right_hair.set_data(
-            self._get_right_x_extent(),
-            self._get_horizontal_y_extent())
-
-        self.top_hair.set_data(
-            self._get_vertical_x_extent(),
-            self._get_top_y_extent())
-
-        self.bottom_hair.set_data(
-            self._get_vertical_x_extent(),
-            self._get_bottom_y_extent())
-
 
 def _image_width(hdulist):
     return _image_shape(hdulist)[1]
 
+
 def _image_height(hdulist):
     return _image_shape(hdulist)[0]
 
+
 def _image_shape(hdulist):
     return _image_data(hdulist).shape
+
 
 def _image_data(hdulist):
     return hdulist[0].data
--- src/ossos-pipeline/ossos/gui/views/imageview.py
+++ src/ossos-pipeline/ossos/gui/views/imageview.py
@@ -1,9 +1,8 @@
 __author__ = "David Rusk <drusk@uvic.ca>"
 
-import wx
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_wxagg import \
-    FigureCanvasWxAgg as FigureCanvas
+import logging
+
+import ds9
 
 from ossos.fitsviewer.singletviewer import SingletViewer
 from ossos.fitsviewer.tripletviewer import TripletViewer
@@ -14,13 +13,37 @@ class ImageViewManager(object):
         # Note: the figure we pass in is just a temporary placeholder.
         # 'Displayable Items' provide their own figure which the canvas can
         # be made to use, but it also requires one on its creation.
-        #self.canvas = FigureCanvas(mainframe.main_panel, wx.ID_ANY, plt.figure())
-        #mainframe.add_to_main_sizer(self.canvas)
 
-        self._singlet_viewer = SingletViewer(mainframe.main_panel)
-        self._triplet_viewer = TripletViewer(mainframe.main_panel)
+        self._ds9 = None
+        self._singlet_viewer = SingletViewer(mainframe.main_panel, display=self.ds9)
+        self._triplet_viewer = TripletViewer(mainframe.main_panel, display=self.ds9)
 
         self._image_viewer = self._singlet_viewer
+
+    @property
+    def ds9(self):
+        if not self._ds9:
+            # start xpans if needed
+            ds9.ds9_xpans()
+
+            # start ds9 if need, or connect to existing
+            cnt = 0
+            while cnt < 10:
+                cnt += 1
+                try:
+                    self._ds9 = ds9.ds9(target='validate')
+                    self._ds9.set('width 1024')
+                    self._ds9.set('height 1024')
+                    break
+                except ValueError as ve:
+                    logging.warning('Error on attempt {0} to connect to DS9 {1}'.format(cnt, ve))
+
+        if self._ds9 is None:
+            raise IOError("Failed to connect to DS9.")
+
+        self._turn_off_view_panels()
+
+        return self._ds9
 
     @property
     def image_viewer(self):
@@ -29,6 +52,10 @@ class ImageViewManager(object):
         @rtype: SingletViewer
         """
         return self._image_viewer
+
+    def _turn_off_view_panels(self):
+        for panel in ['info', 'panner', 'magnifier', 'buttons']:
+            self._ds9.set('view {} no'.format(panel))
 
     @image_viewer.setter
     def image_viewer(self, viewer):
