9179a1cde4b3148d779f4e968bcce09eb1e3780e
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2015-09-14 16:59:18 -0700

    reworked some of the internals of ephem_target so that saving CFHT_ET style files actually works.  Also adjusted the 'test' to check the content of the saved files, not just their existance.

--- src/ossos-pipeline/ossos/ephem_target.py
+++ src/ossos-pipeline/ossos/ephem_target.py
@@ -29,7 +29,6 @@ class EphemTarget(object):
               "DEC_J2000": {"attr": {"datatype": "A", "width": "11", "format": "DEd:DEm:DEs", "unit": "deg"},
                             "DESCRIPTION": "Declination of target"}}
 
-
     def __init__(self, name, column_separator=COLUMN_SEPARATOR, format='CFHT ET'):
         """
         create an ephmeris target, either with a 'orbfit' object or some mean rate of motion.
@@ -40,50 +39,8 @@ class EphemTarget(object):
         self.name = str(name)
         self.format = format
         self.doc = create_astrores_document()
-        table = self.doc.createElement("TABLE")
-        table.setAttribute("ID", "Table")
-        self.doc.lastChild.appendChild(table)
-
-        self.nodes = EphemTarget.nodes
-        self.fields = EphemTarget.fields
         self.column_separator = column_separator
-        nodes = self.nodes
-        fields = self.fields
-        self.field_names = ["DATE_UTC", "RA_J2000", "DEC_J2000"]
-
-        for (key, value) in nodes.iteritems():
-            element = self.doc.createElement(key)
-            element.appendChild(self.doc.createTextNode(value))
-            table.appendChild(element)
-
-        table.getElementsByTagName("TITLE")[0].lastChild.appendData(" target {}".format(self.name))
-
-        table.appendChild(self.doc.createComment("Definition of each field"))
-
-        for fieldName in self.field_names:
-            field = self.doc.createElement("FIELD")
-            field.setAttribute("name", fieldName)
-            for (key, value) in fields[fieldName]['attr'].iteritems():
-                field.setAttribute(key, value)
-            description = self.doc.createElement("DESCRIPTION")
-            description.appendChild(self.doc.createTextNode(fields[fieldName]['DESCRIPTION']))
-            field.appendChild(description)
-            table.appendChild(field)
-
-        table.appendChild(self.doc.createComment("Data table"))
-
-        data = self.doc.createElement("DATA")
-        table.appendChild(data)
-
-        header_lines = self._cdata_header(colsep=self.column_separator)
-
-        csv = self.doc.createElement("CSV")
-        csv.setAttribute("headlines", str(len(header_lines)))
-        csv.setAttribute("colsep", self.column_separator)
-        data.appendChild(csv)
         self.coordinates = []
-        self.cdata = self.doc.createCDATASection("\n"+"\n".join(header_lines)+"\n")
-        csv.appendChild(self.cdata)
 
     def _init_cfht_et_file(self):
         self.doc = create_astrores_document()
@@ -93,7 +50,6 @@ class EphemTarget(object):
 
         self.nodes = EphemTarget.nodes
         self.fields = EphemTarget.fields
-        self.column_separator = column_separator
         nodes = self.nodes
         fields = self.fields
         self.field_names = ["DATE_UTC", "RA_J2000", "DEC_J2000"]
@@ -128,7 +84,6 @@ class EphemTarget(object):
         csv.setAttribute("headlines", str(len(header_lines)))
         csv.setAttribute("colsep", self.column_separator)
         data.appendChild(csv)
-        self.coordinates = []
         self.cdata = self.doc.createCDATASection("\n"+"\n".join(header_lines)+"\n")
         csv.appendChild(self.cdata)
 
@@ -182,11 +137,11 @@ class EphemTarget(object):
         Append an target location to the ephemeris listing.
         """
         fields = self.fields
-        sra = coordinate.ra.format(units.hour, sep=':', precision=2, pad=True)
-        sdec = coordinate.dec.format(units.degree, sep=':', precision=1, alwayssign=True)
+        sra = coordinate.ra.to_string(units.hour, sep=':', precision=2, pad=True)
+        sdec = coordinate.dec.to_string(units.degree, sep=':', precision=1, alwayssign=True)
         coord = SkyCoord(sra+" "+sdec, unit=(units.hour, units.degree))
-        sra = coord.ra.format(units.hour, sep=":", precision=2, pad=True)
-        sdec = coord.dec.format(units.degree, sep=":", precision=1, pad=True)
+        sra = coord.ra.to_string(units.hour, sep=":", precision=2, pad=True)
+        sdec = coord.dec.to_string(units.degree, sep=":", precision=1, pad=True, alwayssign=True)
         sdate = str(coordinate.obstime.replicate(format('iso')))
         self.cdata.appendData(self._entry(sdate, fields["DATE_UTC"]['attr']['width'], colsep=self.column_separator))
         self.cdata.appendData(self._entry(sra, fields["RA_J2000"]['attr']['width'], colsep=self.column_separator))
@@ -219,6 +174,7 @@ class EphemTarget(object):
 
     def cfht_writer(self, f_handle):
         self._init_cfht_et_file()
+        print len(self.coordinates)
         for coordinate in self.coordinates:
             self._append_cdata(coordinate)
         self.doc.writexml(f_handle, indent="  ", addindent="  ", newl='\n')
