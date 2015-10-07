cd7e93a6901996200e0c0980cedbe0f965cf1600
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2015-10-06 18:54:29 -0700

    Working setup.py for push to PyPI

--- src/ossos/core/setup.py
+++ src/ossos/core/setup.py
@@ -1,5 +1,5 @@
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
 import sys
 
 if sys.version_info[0] > 2:
@@ -7,27 +7,22 @@ if sys.version_info[0] > 2:
     sys.exit(-1)
 
 # # Build the list of scripts to be installed.
-script_dir = 'pipeline'
+script_dir = 'tools'
 scripts = []
 for script in os.listdir(script_dir):
     if script[-1] in ["~", "#"]:
         continue
     scripts.append(os.path.join(script_dir, script))
-scripts.append('validate.py')
 
 setup(name='ossos',
-      version="0.3.3",
+      version="0.4.1",
       url='http://github.com/OSSOS/MOP',
       author='''JJ Kavelaars (jjk@uvic.ca),
-              Brett Gladman (gladman@astro.ubc.ca),
-              Jean-Marc Petit (jmpetit@obs-besancon.fr),
-              Matt Holman (holman@cfa.harvard.edu),
-              Hans Scholl,
               Michele Bannister (micheleb@uvic.ca),
               David Rusk''',
       maintainer='M Bannister and JJ Kavelaars',
       maintainer_email='jjk@uvic.ca',
-      description="Outer Solar System Origins Survey (OSSOS) Pipeline",
+      description="Outer Solar System Origins Survey (OSSOS)",
       long_description='See http://www.ossos-survey.org/ for science details.',
       classifiers=['Intended Audience :: Science/Research',
                    'Topic :: Scientific/Engineering :: Astronomy',
@@ -36,13 +31,13 @@ setup(name='ossos',
                    'Operating System :: MacOS :: MacOS X',
                    'Environment :: X11 Applications',
                    'License :: OSI Approved :: GNU General Public License (GPL)',
-      ],
+                   ],
       package_data={'ossos': ['gui/*.json']},
       install_requires=['pyraf >= 2.1.1',
                         'astropy >= 0.2.5',
-                        'vos >= 1.11.3',
+                        'vos >= 2.0',
                         'pyephem',
-                        'requests',
+                        'requests >= 2.7',
                         'pyOpenSSL',
                         'numpy >= 1.6.1',
                         'wxPython >= 2.8.12.1',
@@ -50,12 +45,7 @@ setup(name='ossos',
                         'matplotlib',
                         'Polygon2',
                         'd2to1 >= 0.2.10', 'scipy', 'uncertainties',
-      ],
+                        ],
       scripts=scripts,
-      packages=['ossos',
-                'ossos/fitsviewer',
-                'ossos/downloads', 'ossos/downloads/cutouts',
-                'ossos/gui', 'ossos/gui/models', 'ossos/gui/views',
-                'planning', 'planning/plotting',
-      ],
-)
+      packages=find_packages(exclude="tests*")
+      )
