8ba48b02adb8968d3c0ddf7b59cab0af336c56fc
mtbannister <micheleb@uvic.ca>
2015-10-11 02:30:18 -1000

    Add ability to install with a bypass on the required packages. This should allow compatibility with pip's ability to identify libraries in a location of the user's choice.

--- src/ossos/core/setup.py
+++ src/ossos/core/setup.py
@@ -1,6 +1,35 @@
 import os
 from setuptools import setup, find_packages
 import sys
+from optparse import OptionParser
+
+parser=OptionParser()
+parser.add_option('--skip_dependencies',
+                  action='store_true', dest='we_trust_you',
+                  default=False,
+                  help='Install assuming you have already installed the necessary dependencies, '
+                       'and they are all available in your paths. '
+                       'DEFAULT=%default.')
+# e.g. pip install ossos -t /Users/username/mybin/ --global-option="--skip_dependencies"
+(opt,args) = parser.parse_args()
+
+if not opt.skip_dependencies:
+    dependencies = ['pyraf >= 2.1.1',
+                    'astropy >= 0.2.5',
+                    'vos >= 2.0',
+                    'pyephem',
+                    'requests >= 2.7',
+                    'pyOpenSSL',
+                    'numpy >= 1.6.1',
+                    'wxPython >= 2.8.12.1',
+                    'pyds9',
+                    'matplotlib',
+                    'Polygon2',
+                    'd2to1 >= 0.2.10', 'scipy', 'uncertainties',
+                        ]
+else:
+    dependencies = []
+
 
 if sys.version_info[0] > 2:
     print 'The MOP package is only compatible with Python version 2.7+, not yet with 3.x'
@@ -33,19 +62,7 @@ setup(name='ossos',
                    'License :: OSI Approved :: GNU General Public License (GPL)',
                    ],
       package_data={'ossos': ['gui/*.json']},
-      install_requires=['pyraf >= 2.1.1',
-                        'astropy >= 0.2.5',
-                        'vos >= 2.0',
-                        'pyephem',
-                        'requests >= 2.7',
-                        'pyOpenSSL',
-                        'numpy >= 1.6.1',
-                        'wxPython >= 2.8.12.1',
-                        'pyds9',
-                        'matplotlib',
-                        'Polygon2',
-                        'd2to1 >= 0.2.10', 'scipy', 'uncertainties',
-                        ],
+      install_requires=dependencies,
       scripts=scripts,
       packages=find_packages(exclude="tests*")
       )
