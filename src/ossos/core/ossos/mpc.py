c37eb139694e7e62f7ff508481c416b453c1e5ec
kavelaar <jjk@uvic.ca>
2016-09-27 12:26:19 -0700

    Put the values for MEAUSRE/OBSERVER/TELECOPE/ASTROMETRIC_NETWORK into the config.json file instead of as values in the mpc module.
    This is a continuing effort to get parameters out of the code and into a config area.

--- src/ossos/core/ossos/mpc.py
+++ src/ossos/core/ossos/mpc.py
@@ -6,17 +6,3 @@ from mp_ephem.ephem import *
 MPCReader = EphemerisReader
 Observation = mp_ephem.Observation
 
-
-mp_ephem.DEFAULT_OBSERVERS = ['M. T. Bannister',
-                     'J. J. Kavelaars',
-                     'B. J. Gladman',
-                     'J.-M. Petit',
-                     'T. Burdullis'
-                     ]
-mp_ephem.DEFAULT_MEASURERS = ['S. D. J. Gwyn',
-                     'Y.-T. Chen.'
-                     ]
-mp_ephem.DEFAULT_TELESCOPE = "CFHT 3.6m + CCD"
-mp_ephem.DEFAULT_ASTROMETRIC_NETWORK = "UCAC4"
-
-
