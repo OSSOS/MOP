44a7382bf2ffb1afd13eb26f160abd032dba165f
jjk <jjk@uvic.ca>
2017-01-09 08:59:46 -0800

    Added a camera test

--- src/ossos/core/tests/test_ossos/test_camera.py
+++ src/ossos/core/tests/test_ossos/test_camera.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
-
+from astropy.coordinates import SkyCoord
+from ...ossos import cameras
 
 class TestCamera(TestCase):
-  def test_coordinate(self):
-    self.fail()
+
+    def setUp(self):
+        self.camera = cameras.Camera()
+
+    def test_coordinate(self):
+        self.camera.coordinate = 0.0, 0.0
+        assert isinstance(self.camera.coordinate, SkyCoord)
+
