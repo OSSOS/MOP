1488696fc315db627a0c2fac1cf54677f662f252
JJ Kavelaars <jjk@uvic.ca>
2017-03-13 08:04:56 -0700

    Starting to create some geometry tests

--- src/ossos/core/tests/test_ossos/test_camera.py
+++ src/ossos/core/tests/test_ossos/test_camera.py
@@ -1,13 +1,46 @@
 from unittest import TestCase
-from astropy.coordinates import SkyCoord
-from ...ossos import cameras
+from astropy import units
+
+from ossos import cameras
+
 
 class TestCamera(TestCase):
 
     def setUp(self):
-        self.camera = cameras.Camera()
+        self.camera = cameras.Camera(0*units.degree, 0*units.degree)
+
+    def test_offset(self):
+        self.camera.offset(index=22)
+        self.assertEquals(self.camera.ra, 0*units.degree)
+        self.assertEquals(self.camera.dec, 0*units.degree)
+        self.fail()
 
     def test_coordinate(self):
-        self.camera.coordinate = 0.0, 0.0
-        assert isinstance(self.camera.coordinate, SkyCoord)
+        self.fail()
 
+    def test_coord(self):
+        self.fail()
+
+    def test_ra(self):
+        self.fail()
+
+    def test_dec(self):
+        self.fail()
+
+    def test_origin(self):
+        self.fail()
+
+    def test_origin(self):
+        self.fail()
+
+    def test_set_origin(self):
+        self.fail()
+
+    def test_polygon(self):
+        self.fail()
+
+    def test_geometry(self):
+        self.fail()
+
+    def test_separation(self):
+        self.fail()
