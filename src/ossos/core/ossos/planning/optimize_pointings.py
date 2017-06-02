2031bf75bd66f2babfa961c9a3ce9b19743b2cff
JJ Kavelaars <jjk@uvic.ca>
2017-06-02 08:35:58 -0700

    Many changes to get optmize working correctly.
    The optmizier has been modified after interactions with CFHT on how they want the ephem target coordinates added.
    
    1 - no positions provided when target is below horizon.
    
    2 - runid must be part of the target field name.
    
    3 - provide a postion ever 1.5 hours instead of 3.0 hours.  OK as we now only provide positions when target is up.

--- src/ossos/core/ossos/planning/optimize_pointings.py
+++ src/ossos/core/ossos/planning/optimize_pointings.py
@@ -1,21 +1,60 @@
 #!python
-
 from astropy import units
 from astropy.coordinates import SkyCoord
-from astropy.time import TimeDelta
+from astropy.time import TimeDelta, Time
 import numpy as np
 from ossos.cameras import Camera
 from ossos import mpc
 import mp_ephem
 from ossos.ephem_target import EphemTarget
-import sys, os
+import sys
+import os
 import argparse
 import logging
 import math
 from copy import deepcopy
+import ephem
+
+cfht = ephem.Observer()
+cfht.lat = 0.344
+cfht.lon = -2.707
+cfht.elevation = 4100
+cfht.date = '2018/03/28 20:00:00'
+sun = ephem.Sun()
+fb = ephem.FixedBody()
 
 
-def create_ephemeris_file(name, camera, kbos, orbits, pointing_date, qrunid):
+def is_up(coordinate, current_time):
+    """
+    Given the position and time determin if the given target is up.
+
+    @param coordinate: the J2000 location of the source
+    @param current_time: The time of the observations
+    @return: True/False
+    """
+    cfht.date = current_time.iso.replace('-', '/')
+    cfht.horizon = math.radians(-7)
+    sun.compute(cfht)
+    sun_rise = Time(str(sun.rise_time).replace('/', '-'))
+    sun_set = Time(str(sun.set_time).replace('/', '-'))
+
+    if current_time < sun_set or current_time > sun_rise:
+        return False
+
+    fb._ra = coordinate.ra.radian
+    fb._dec = coordinate.dec.radian
+    cfht.horizon = math.radians(40)
+    fb.compute(cfht)
+    fb_rise_time = Time(str(fb.rise_time).replace('/', '-'))
+    fb_set_time = Time(str(fb.set_time).replace('/', '-'))
+
+    if (current_time > fb_set_time > fb_set_time or
+       fb_rise_time > current_time > fb_set_time):
+        return False
+    return True
+
+
+def create_ephemeris_file(name, camera, kbos, orbits, pointing_date, runid):
     """
 
     @param name: name of the pointing
@@ -23,10 +62,11 @@ def create_ephemeris_file(name, camera, kbos, orbits, pointing_date, qrunid):
     @param kbos: list of kbos covered by this pointing
     @param orbits: orbits of all known kbos
     @param pointing_date: data of pointing associated with camera object
+    @param runid:  The CFHT RunID that will be used to observer the target.
     @return: None
     """
 
-    et = EphemTarget(name, format="CFHT API", qrunid=qrunid)
+    et = EphemTarget(name, format="CFHT API", runid=runid)
     # determine the mean motion of target KBOs in this field.
     field_kbos = []
     center_ra = 0
@@ -35,7 +75,7 @@ def create_ephemeris_file(name, camera, kbos, orbits, pointing_date, qrunid):
     pointing_date = mpc.Time(pointing_date)
     start_date = pointing_date - TimeDelta(8.1 * units.day)
     end_date = start_date + TimeDelta(17 * units.day)
-    time_step = TimeDelta(3.0 * units.hour)
+    time_step = TimeDelta(1.5 * units.hour)
 
     # Compute the mean position of KBOs in the field on current date.
     for kbo_name in kbos:
@@ -68,6 +108,8 @@ def create_ephemeris_file(name, camera, kbos, orbits, pointing_date, qrunid):
                       dec=dec,
                       unit=(units.radian, units.radian),
                       obstime=today)
+        if not is_up(cc, today):
+            continue
         dt = pointing_date - today
         cc.dra = (mean_motion[0] * units.radian / dt.to(units.hour)).to(units.arcsec / units.hour).value * math.cos(dec)
         cc.ddec = (mean_motion[1] * units.radian / dt.to(units.hour)).to(units.arcsec / units.hour).value
@@ -83,6 +125,8 @@ def optimize(orbits, required, locations, tokens):
 
     @param orbits: a dictionary of orbits to optimize the point of.
     @param required: list of objects that MUST be observed.
+    @param locations: locations of the objects at the start of observing period.
+    @param tokens: List of tokens (object names) that we will try and cover.
     @return:
     """
 
@@ -90,9 +134,8 @@ def optimize(orbits, required, locations, tokens):
     token_order = np.random.permutation(required)
     optimal_pointings = {}
     covered = []  # the objects that have already been covered by a planned pointing.
-    search_order = [22+4]
+    search_order = [22 + 4]
     search_order.extend(range(len(Camera.names)))
-    print search_order
 
     # For each required target find the pointing that will include the largest number of other required targets
     # and then tweak that specific pointing to include the maximum number of secondary targets.
@@ -175,8 +218,7 @@ def optimize(orbits, required, locations, tokens):
                 optimal_coverage = this_coverage
                 optimal_pointing = idx
 
-        #  remove all sources covered by optimal_pointing from further consideration.
-        optimal_unique_count = 0
+        # remove all sources covered by optimal_pointing from further consideration.
         sys.stdout.write("{} pointing covers: ".format(token))
         unique_coverage_list = []
         for this_token in optimal_coverage:
@@ -212,13 +254,11 @@ def main():
     parser.add_argument('--nattempts', type=int,
                         help="Number of random variations of pointings to try",
                         default=2)
-    parser.add_argument('--qrunid', 
-                        help="CFHT ID for this QUEUE Run',
+    parser.add_argument('--runid',
+                        help="CFHT RUN ID",
                         default='16BQ17')
 
     args = parser.parse_args()
-
-    reader = mp_ephem.EphemerisReader()
 
     logging.basicConfig(level=logging.INFO)
 
@@ -232,10 +272,10 @@ def main():
     required_filename = args.required_objects
     for required_object in open(required_filename):
         if '#' in required_object:
-           continue
+            continue
         required_objects.append(required_object.strip())
 
-    pointings_filename = os.path.splitext(required_filename)[0]+"_pointings.txt"
+    pointings_filename = os.path.splitext(required_filename)[0] + "_pointings.txt"
 
     filenames = open(args.pointing_objects).readlines()
 
@@ -246,15 +286,15 @@ def main():
     for filename in filenames:
         filename = filename.strip()
         token = os.path.splitext(os.path.basename(filename))[0]
-        abg_filename = os.path.splitext(filename)[0]+".abg"
+        abg_filename = os.path.splitext(filename)[0] + ".abg"
         orbits[token] = mp_ephem.BKOrbit(None, ast_filename=filename, abg_file=abg_filename)
         tokens.append(token)
-        print token, orbits[token].r_mag
 
     # Turn the object locations at the time of interest into a SkyCoord numpy array.
     tokens = np.array(tokens)
     minimum_number_of_pointings = len(required_objects)
 
+    best_pointing_list = []
     for attempt in range(args.nattempts):
         locations = []
         for token in tokens:
@@ -271,17 +311,15 @@ def main():
         pobj.write("index {}\n".format(pointing_date))
         pointing_number = 0
         for token in best_pointing_list:
-            pobj.write("{} {} {} {} # {}\n".format(pointing_number+1,
-                                              "{}".format(token),
-                                              best_pointing_list[token][0].coord.to_string("hmsdms", sep=" "),
-                                              2000,
-                                              len(best_pointing_list[token][1])))
+            pobj.write("{} {} {} {} # {}\n".format(pointing_number + 1,
+                                                   "{}".format(token),
+                                                   best_pointing_list[token][0].coord.to_string("hmsdms", sep=" "),
+                                                   2000,
+                                                   len(best_pointing_list[token][1])))
             pointing_number += 1
-            create_ephemeris_file(token, best_pointing_list[token][0], best_pointing_list[token][1], orbits, pointing_date, args.qrunid)
+            create_ephemeris_file(token, best_pointing_list[token][0], best_pointing_list[token][1], orbits,
+                                  pointing_date, args.runid)
 
 
 if __name__ == '__main__':
     sys.exit(main())
-
-
-
