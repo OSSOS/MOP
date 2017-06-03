74c396bda57523ad318ddd7d7f2c7b013f287653
JJ Kavelaars <jjk@uvic.ca>
2017-06-03 15:14:26 +0000

    Committed these files on the slow branch but they ppear to alraedy be in Master

---
+++ src/ossos/core/ossos/pipeline/mkpsf.py
@@ -0,0 +1,173 @@
+#!python 
+################################################################################
+##                                                                            ##
+## Copyright 2013 by its authors                                              ##
+## See COPYING, AUTHORS                                                       ##
+##                                                                            ##
+## This file is part of OSSOS Moving Object Pipeline (OSSOS-MOP)              ##
+##                                                                            ##
+##    OSSOS-MOP is free software: you can redistribute it and/or modify       ##
+##    it under the terms of the GNU General Public License as published by    ##
+##    the Free Software Foundation, either version 3 of the License, or       ##
+##    (at your option) any later version.                                     ##
+##                                                                            ##
+##    OSSOS-MOP is distributed in the hope that it will be useful,            ##
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of          ##
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           ##
+##    GNU General Public License for more details.                            ##
+##                                                                            ##
+##    You should have received a copy of the GNU General Public License       ##
+##    along with OSSOS-MOP.  If not, see <http://www.gnu.org/licenses/>.      ##
+##                                                                            ##
+################################################################################
+"""Run the OSSOS makepsf proceedure"""
+
+import argparse
+import os
+from subprocess import CalledProcessError
+import sys
+import logging
+from ossos import storage
+from ossos import util
+
+task  = 'mkpsf'
+dependency = 'mk_mopheader'
+
+
+def run(expnum, ccd, version, dry_run=False, prefix="", force=False):
+    """Run the OSSOS jmpmakepsf script.
+
+    """
+
+    message = storage.SUCCESS
+    if storage.get_status(task, prefix, expnum, version=version, ccd=ccd) and not force:
+        logging.info("{} completed successfully for {} {} {} {}".format(task, prefix, expnum, version, ccd))
+        return
+
+    with storage.LoggingManager(task, prefix, expnum, ccd, version, dry_run):
+        try:
+            if not storage.get_status(dependency, prefix, expnum, "p", ccd=ccd):
+                raise IOError("{} not yet run for {}".format(dependency, expnum))
+
+            # confirm destination directory exists.
+            destdir = os.path.dirname(
+                storage.dbimages_uri(expnum, ccd, prefix=prefix, version=version, ext='fits'))
+            if not dry_run:
+                storage.mkdir(destdir)
+
+            # get image from the vospace storage area
+            logging.info("Getting fits image from VOSpace")
+            filename = storage.get_image(expnum, ccd, version=version, prefix=prefix)
+
+            # get mopheader from the vospace storage area
+            logging.info("Getting mopheader from VOSpace")
+            mopheader_filename = storage.get_file(expnum, ccd, version=version, prefix=prefix, ext='mopheader')
+
+
+            # run mkpsf process
+            logging.info("Running mkpsf on %s %d" % (expnum, ccd))
+            logging.info(util.exec_prog(['jmpmakepsf.csh',
+                                         './',
+                                         filename,
+                                         'yes', 'yes']))
+            
+            if dry_run:
+                return
+
+            # place the results into VOSpace
+            basename = os.path.splitext(filename)[0]
+
+            for ext in ('mopheader', 'psf.fits',
+                        'zeropoint.used', 'apcor', 'fwhm', 'phot'):
+                dest = storage.dbimages_uri(expnum, ccd, prefix=prefix, version=version, ext=ext)
+                source = basename + "." + str(ext)
+                count = 0
+                with open(source, 'r'):
+                  while True:
+                    count += 1
+                    try:
+                        logging.info("Attempt {} to copy {} -> {}".format(count, source, dest))
+                        storage.copy(source, dest)
+                        break
+                    except Exception as ex:
+                        if count > 10:
+                            raise ex
+
+            # set some data parameters associated with the image, determined in this step.
+            storage.set_status('fwhm', prefix, expnum, version=version, ccd=ccd, status=str(storage.get_fwhm(
+                expnum, ccd=ccd, prefix=prefix, version=version)))
+            storage.set_status('zeropoint', prefix, expnum, version=version, ccd=ccd,
+                               status=str(storage.get_zeropoint(
+                                   expnum, ccd=ccd, prefix=prefix, version=version)))
+            logging.info(message)
+        except Exception as e:
+            message = str(e)
+            logging.error(message)
+            
+        storage.set_status(task, prefix, expnum, version, ccd=ccd, status=message)
+
+    return
+
+
+def main():
+
+
+    parser = argparse.ArgumentParser(
+        description='Run makepsf chunk of the OSSOS pipeline')
+
+    parser.add_argument('--ccd', '-c',
+                        action='store',
+                        type=int,
+                        dest='ccd',
+                        default=None,
+                        help='which ccd to process, default is all')
+    parser.add_argument("--dbimages",
+                        action="store",
+                        default="vos:OSSOS/dbimages",
+                        help='vospace dbimages containerNode')
+    parser.add_argument("expnum",
+                        type=int,
+                        nargs='+',
+                        help="expnum(s) to process")
+    parser.add_argument("--dry-run",
+                        action="store_true",
+                        help="DRY RUN, don't copy results to VOSpace, implies --force")
+
+    parser.add_argument("--fk", action="store_true", help="Run fk images")
+
+    parser.add_argument("--type", "-t", choices=['o', 'p', 's'],
+                        help="which type of image: o-RAW, p-ELIXIR, s-SCRAMBLE", default='p')
+    parser.add_argument("--verbose", "-v",
+                        action="store_true")
+    parser.add_argument("--force", default=False,
+                        action="store_true")
+    parser.add_argument("--debug", "-d",
+                        action="store_true")
+
+    cmd_line = " ".join(sys.argv)
+    args = parser.parse_args()
+
+    util.set_logger(args)
+    logging.info("Started {}".format(cmd_line))
+
+    prefix = (args.fk and 'fk') or ''
+
+    storage.DBIMAGES = args.dbimages
+
+    exit_code = 0
+    for expnum in args.expnum:
+        if args.ccd is None:
+           if int(expnum) < 1785619:
+               # Last exposures with 36 CCD Megaprime
+               ccdlist = range(0,36)
+           else:
+               # First exposrues with 40 CCD Megaprime
+               ccdlist = range(0, 40)
+        else:
+           ccdlist = [args.ccd]
+        for ccd in ccdlist:
+            run(expnum, ccd, args.type, args.dry_run, prefix, args.force)
+    return exit_code
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ src/ossos/core/ossos/pipeline/slow.py
@@ -0,0 +1,154 @@
+#!python
+from multiprocessing import Pool
+from ossos import mop_file, storage
+import os
+from astropy.io import fits
+import numpy
+from vos import Client
+import sys
+from ossos import junk_keywords
+from ossos import util
+import argparse
+import logging
+
+client = Client()
+
+RA = 'RA_J2000'
+DEC = 'DE_J2000'
+del_keyword_list = junk_keywords.bad_keywords_list
+task = 'slow'
+dependency = 'step1'
+
+def run(expnum, ccd, version='p', prefix='', dry_run=False, force=False):
+
+    message = 'success'
+
+    if storage.get_status(task, prefix, expnum, version=version, ccd=ccd) and not force:
+        logging.info("{} completed successfully for {} {} {} {}".format(task, prefix, expnum, version, ccd))
+        return
+
+    with storage.LoggingManager(task=task, prefix=prefix, expnum=expnum, ccd=ccd, version=version, dry_run=dry_run):
+        try:
+            if not storage.get_status(dependency, prefix, expnum, "p", ccd=ccd):
+                raise IOError("{} not yet run for {}".format(dependency, expnum))
+
+
+            header = storage.get_astheader(expnum, ccd)
+            datasec = storage.datasec_to_list(header.get('DATASEC', '[80:2080,30,4160]'))
+            try:
+                fwhm = "{:5.2f}".format(storage.get_fwhm(expnum, ccd))
+            except:
+                fwhm = 'unknown'
+            for keyword in del_keyword_list:
+                try:
+                    del(header[keyword])
+                except:
+                    pass
+            header['FWHM'] = (fwhm, 'FWHM in pixels')
+            header['EXTNAME'] = 'header'
+            primary_hdu = fits.PrimaryHDU(header=header)
+            hdu_list = fits.HDUList([primary_hdu, ])
+            for ext in ['jmp', 'matt']:
+                extension = 'obj.'+ext
+                name = "{}p{:02d}.{}".format(expnum, ccd, extension)
+                try:
+                    os.unlink(name)
+                    os.unlink(name+".fits")
+                except:
+                    pass
+                logging.info("Retrieving {}".format(name))
+                obj_file = mop_file.Parser(expnum, ccd, extension)
+                obj_file.parse()
+        
+                t = numpy.all([datasec[0] < obj_file.data['X'], obj_file.data['X'] < datasec[1],
+                               datasec[2] < obj_file.data['Y'], obj_file.data['Y'] < datasec[3]], axis=0)
+                logging.info("Source remaining after datasec cut: {} of {}".format(len(obj_file.data[t]['X']), len(t)))
+                table_hdu = fits.table_to_hdu(obj_file.data[t])
+                table_hdu.header['CATALOG'] = name
+                table_hdu.header['EXTNAME'] = ext
+                hdu_list.append(table_hdu)
+                del table_hdu
+                del obj_file
+                os.unlink(name)
+
+            name = "{}p{:02d}.{}".format(expnum, ccd, 'obj.fits')
+            if os.access(name, os.F_OK):
+                os.unlink(name)
+            hdu_list.writeto(name)
+            uri = storage.dbimages_uri(expnum, ccd, 'p', ext=".obj.fits")
+            logging.info(name+" -> "+uri)
+            count = 0
+            with open(name):
+              while True:
+                count += 1
+                logging.info("Copy attempt {}".format(count))
+                try:
+                    storage.copy(name, uri)
+                    os.unlink(name)
+                    break
+                except Exception as ex:
+                    if count > 10:
+                        raise ex
+
+            logging.info(message)
+        except Exception as e:
+            message = str(e)
+            logging.error(message)
+        
+        if not dry_run:
+            storage.set_status(task, prefix, expnum, version=version, ccd=ccd, status=message)
+
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser(
+        description='Run FITS catalog builder chunk of the OSSOS pipeline')
+
+    parser.add_argument('--ccd', '-c',
+                        action='store',
+                        type=int,
+                        dest='ccd',
+                        default=None,
+                        help='which ccd to process, default is all')
+    parser.add_argument("--dbimages",
+                        action="store",
+                        default="vos:OSSOS/dbimages",
+                        help='vospace dbimages containerNode')
+    parser.add_argument("expnum",
+                        type=int,
+                        nargs='+',
+                        help="expnum(s) to process")
+    parser.add_argument("--dry-run",
+                        action="store_true",
+                        help="DRY RUN, don't copy results to VOSpace, implies --force")
+
+    parser.add_argument("--fk", action="store_true", help="Run fk images")
+
+    parser.add_argument("--type", "-t", choices=['o', 'p', 's'],
+                        help="which type of image: o-RAW, p-ELIXIR, s-SCRAMBLE", default='p')
+    parser.add_argument("--verbose", "-v",
+                        action="store_true")
+    parser.add_argument("--force", default=False,
+                        action="store_true")
+    parser.add_argument("--debug", "-d",
+                        action="store_true")
+
+    cmd_line = " ".join(sys.argv)
+    args = parser.parse_args()
+
+    util.set_logger(args)
+    logging.info("Started {}".format(cmd_line))
+
+    prefix = (args.fk and 'fk') or ''
+
+    storage.DBIMAGES = args.dbimages
+
+    for expnum in args.expnum:
+        if args.ccd is None:
+            ccdlist = storage.get_ccdlist(expnum)
+        else:
+           ccdlist = [args.ccd]
+
+        for ccd in ccdlist:
+            run(expnum, ccd)
