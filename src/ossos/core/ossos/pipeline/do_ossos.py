7b8fc35181513197d8d0dfe6dd46dd0fa8ca9f13
JJ Kavelaars <jjkavelaars@gmail.com>
2018-04-10 16:56:46 -0700

    Make this more appropriate for current release of CAOM2

--- src/ossos/core/ossos/pipeline/do_ossos.py
+++ src/ossos/core/ossos/pipeline/do_ossos.py
@@ -3,7 +3,7 @@ params = {"LANG": "ADQL",
           "REQUEST": "doQuery",
           "FORMAT": "csv" }
 
-QUERY="""SELECT Observation.sequenceNumber AS "Sequence Number" FROM caom2.Plane AS Plane JOIN caom2.Observation AS Observation ON Plane.obsID = Observation.obsID WHERE  ( Observation.instrument_name = 'MegaPrime' AND Observation.collection = 'CFHT' AND INTERSECTS( INTERVAL( 57754.0, 1.7976931348623157E308 ), Plane.time_bounds ) = 1 AND lower(Observation.proposal_id) IN ('17ad96', '17bf14', '17bc09' ) AND  Plane.calibrationLevel=1 AND ( Plane.quality_flag IS NULL OR Plane.quality_flag != 'junk') ) ORDER BY Observation.sequenceNumber """
+QUERY="""SELECT Observation.sequenceNumber AS expnum FROM caom2.Plane AS Plane JOIN caom2.Observation AS Observation ON Plane.obsID = Observation.obsID WHERE  ( Observation.instrument_name = 'MegaPrime' AND Observation.collection = 'CFHT' AND INTERSECTS( INTERVAL( 57754.0, 1.7976931348623157E308 ), Plane.time_bounds ) = 1 AND lower(Observation.proposal_id) IN ('17bt01', '17bs02', '17ad96', '17bf14', '17bc09' ) AND  Plane.calibrationLevel=1 AND ( Plane.quality_flag IS NULL OR Plane.quality_flag != 'junk') ) ORDER BY Observation.sequenceNumber """
 
 params["QUERY"] = QUERY
 
@@ -24,10 +24,11 @@ args = parser.parse_args()
 
 current_list = vos.Client().listdir('vos:OSSOS/dbimages')
 recon_exposures = Table.read(StringIO(requests.get(url, params=params).content), format='ascii.csv')
+print recon_exposures
 
 if args.output:
     with open('list.txt', 'w') as fout:
-        cond = [ str(g) not in current_list for g in recon_exposures['Sequence Number']]
+        cond = [ str(g) not in current_list for g in recon_exposures['expnum']]
         ascii.write(recon_exposures[cond], fout, format='no_header')
 
 if not args.output:
@@ -37,7 +38,7 @@ if not args.output:
 
     with tempfile.NamedTemporaryFile() as fout:
         fout.write("Hi Stephen, \n Here is correct list of OSSOS exposures that need processing (PITCAIRN+ASTGWYN).\n\nThanks\nJJ\n")
-        cond = [ str(g) not in current_list for g in recon_exposures['Sequence Number']]
+        cond = [ str(g) not in current_list for g in recon_exposures['expnum']]
         ascii.write(recon_exposures[cond], fout, format='no_header', overwrite=True)
         fout.seek(0)
         msg = MIMEText(fout.read())
