6650597bbe65de7d58281c3f96af0d51969e93d1
JJ Kavelaars <jjk@uvic.ca>
2019-06-05 09:19:54 -0700

    PEP8 clean up of comment lines

--- src/ossos/core/ossos/pipeline/step1.py
+++ src/ossos/core/ossos/pipeline/step1.py
@@ -1,25 +1,25 @@
 #!python
-################################################################################
-##                                                                            ##
-## Copyright 2013 by its authors                                              ##
-## See COPYING, AUTHORS                                                       ##
-##                                                                            ##
-## This file is part of OSSOS Moving Object Pipeline (OSSOS-MOP)              ##
-##                                                                            ##
-##    OSSOS-MOP is free software: you can redistribute it and/or modify       ##
-##    it under the terms of the GNU General Public License as published by    ##
-##    the Free Software Foundation, either version 3 of the License, or       ##
-##    (at your option) any later version.                                     ##
-##                                                                            ##
-##    OSSOS-MOP is distributed in the hope that it will be useful,            ##
-##    but WITHOUT ANY WARRANTY; without even the implied warranty of          ##
-##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           ##
-##    GNU General Public License for more details.                            ##
-##                                                                            ##
-##    You should have received a copy of the GNU General Public License       ##
-##    along with OSSOS-MOP.  If not, see <http://www.gnu.org/licenses/>.      ##
-##                                                                            ##
-################################################################################
+###############################################################################
+#                                                                            ##
+# Copyright 2013 by its authors                                              ##
+# See COPYING, AUTHORS                                                       ##
+#                                                                            ##
+# This file is part of OSSOS Moving Object Pipeline (OSSOS-MOP)              ##
+#                                                                            ##
+#    OSSOS-MOP is free software: you can redistribute it and/or modify       ##
+#    it under the terms of the GNU General Public License as published by    ##
+#    the Free Software Foundation, either version 3 of the License, or       ##
+#    (at your option) any later version.                                     ##
+#                                                                            ##
+#    OSSOS-MOP is distributed in the hope that it will be useful,            ##
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          ##
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           ##
+#    GNU General Public License for more details.                            ##
+#                                                                            ##
+#    You should have received a copy of the GNU General Public License       ##
+#    along with OSSOS-MOP.  If not, see <http://www.gnu.org/licenses/>.      ##
+#                                                                            ##
+###############################################################################
 """step1 is to run the two source finding algorithms in the image.
 
 step1jmp is a stand-alone fortran code from Jean-Marc Petit et al.
@@ -28,7 +28,6 @@ step1matt is a script from M. Holman that runs E. Bertain's sExtractor.
 """
 
 import argparse
-from subprocess import CalledProcessError
 from astropy.io import fits
 import logging
 import os
@@ -44,8 +43,8 @@ _MAX_COUNT = 30000
 dependency = "mkpsf"
 task = "step1"
 
-def _get_weight_map(filename, ccd):
 
+def _get_weight_map(filename, ccd):
     # for step1matt we need the weight image
     hdulist = fits.open(filename)
     flat_name = hdulist[0].header.get('FLAT', 'weight.fits')
@@ -58,9 +57,9 @@ def _get_weight_map(filename, ccd):
     logging.info("Getting the Flat Field image.")
     try:
         flat_filename = storage.get_image(flat_name, ccd, version='', ext='fits', subdir='calibrators')
-    except:
+    except Exception:
         flat_filename = storage.get_image(flat_name, ccd, version='', ext='fits', subdir='old_calibrators')
-        
+
     if os.access('weight.fits', os.R_OK):
         os.unlink('weight.fits')
 
@@ -77,7 +76,7 @@ def run(expnum,
         sex_thresh=_SEX_THRESHOLD,
         wave_thresh=_WAVE_THRESHOLD,
         maxcount=_MAX_COUNT,
-        dry_run=False, 
+        dry_run=False,
         force=True):
     """run the actual step1jmp/matt codes.
 
@@ -94,20 +93,20 @@ def run(expnum,
     if storage.get_status(task, prefix, expnum, version, ccd) and not force:
         logging.info("{} completed successfully for {} {} {} {}".format(task, prefix, expnum, version, ccd))
         return
-        
+
     with storage.LoggingManager(task, prefix, expnum, ccd, version, dry_run):
-        try:        
+        try:
             if not storage.get_status(dependency, prefix, expnum, version, ccd):
                 raise IOError(35, "Cannot start {} as {} not yet completed for {}{}{}{:02d}".format(
                     task, dependency, prefix, expnum, version, ccd))
-            logging.info("Retrieving imaging and input parameters from VOSpace")    
+            logging.info("Retrieving imaging and input parameters from VOSpace")
             storage.get_file(expnum, ccd, prefix=prefix, version=version, ext='mopheader')
             filename = storage.get_image(expnum, ccd, version=version, prefix=prefix)
             fwhm = storage.get_fwhm(expnum, ccd, prefix=prefix, version=version)
             basename = os.path.splitext(filename)[0]
 
             _get_weight_map(filename, ccd)
-            
+
             logging.info("Launching step1jmp")
             logging.info(util.exec_prog(['step1jmp',
                                          '-f', basename,
@@ -116,14 +115,14 @@ def run(expnum,
                                          '-m', str(maxcount)]))
 
             logging.info(util.exec_prog(['step1matt',
-                                 '-f', basename,
-                                 '-t', str(sex_thresh),
-                                 '-w', str(fwhm),
-                                 '-m', str(maxcount)]))
+                                         '-f', basename,
+                                         '-t', str(sex_thresh),
+                                         '-w', str(fwhm),
+                                         '-m', str(maxcount)]))
 
             if os.access('weight.fits', os.R_OK):
                 os.unlink('weight.fits')
-            
+
             if not dry_run:
                 for ext in ['obj.jmp', 'obj.matt']:
                     obj_uri = storage.get_uri(expnum, ccd, version=version, ext=ext,
@@ -131,15 +130,15 @@ def run(expnum,
                     obj_filename = basename + "." + ext
                     count = 0
                     with open(obj_filename, 'r'):
-                      while True:
-                        try:
-                            count += 1
-                            logging.info("Attempt {} to copy {} -> {}".format(count, obj_filename, obj_uri))
-                            storage.copy(obj_filename, obj_uri)
-                            break
-                        except Exception as ex:
-                            if count > 10:
-                                raise ex
+                        while True:
+                            try:
+                                count += 1
+                                logging.info("Attempt {} to copy {} -> {}".format(count, obj_filename, obj_uri))
+                                storage.copy(obj_filename, obj_uri)
+                                break
+                            except Exception as ex:
+                                if count > 10:
+                                    raise ex
             logging.info(message)
         except Exception as ex:
             message = str(ex)
@@ -150,7 +149,7 @@ def run(expnum,
 
 
 def main():
-    ### Must be running as a script
+    # Must be running as a script
 
     parser = argparse.ArgumentParser(
         description='Run step1jmp and step1matt on a given exposure.')
@@ -196,7 +195,6 @@ def main():
     parser.add_argument("--force", action="store_true")
     parser.add_argument("--dry-run", action="store_true", help="Do a dry run, not changes to vospce, implies --force")
 
-
     cmd_line = " ".join(sys.argv)
     args = parser.parse_args()
 
@@ -208,15 +206,14 @@ def main():
     prefix = (args.fk and 'fk') or ''
     version = args.type
 
-    exit_code = 0
     for expnum in args.expnum:
         if args.ccd is None:
-           if int(expnum) < 1785619:
-              # Last exposures with 36 CCD Megaprime
-              ccdlist = list(range(0,36))
-           else:
-              # First exposrues with 40 CCD Megaprime
-              ccdlist = list(range(0, 40))
+            if int(expnum) < 1785619:
+                # Last exposures with 36 CCD Megaprime
+                ccdlist = list(range(0, 36))
+            else:
+                # First exposrues with 40 CCD Megaprime
+                ccdlist = list(range(0, 40))
         else:
             ccdlist = [args.ccd]
         for ccd in ccdlist:
@@ -226,8 +223,9 @@ def main():
                 version=version,
                 sex_thresh=args.sex_thresh,
                 wave_thresh=args.wavelet_thresh,
-                dry_run=args.dry_run, 
+                dry_run=args.dry_run,
                 force=args.force)
+
 
 if __name__ == '__main__':
     sys.exit(main())
