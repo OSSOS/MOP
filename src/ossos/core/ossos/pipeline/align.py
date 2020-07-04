e0dbb56072e2d21a88db224b1de653fa7900429f
JJ Kavelaars <jjk@uvic.ca>
2020-07-03 19:02:16 -0700

    Remove unwanted import

--- src/ossos/core/ossos/pipeline/align.py
+++ src/ossos/core/ossos/pipeline/align.py
@@ -18,7 +18,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with OSSOS-MOP.  If not, see <http://www.gnu.org/licenses/>.
 #
-import traceback
 import argparse
 import json
 import logging
