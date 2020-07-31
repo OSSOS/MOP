2b133940d6bd4de969ac89618a7de056403a4e75
JJ Kavelaars <jjk@uvic.ca>
2020-07-31 10:50:00 -0700

    PEP-8 reformatting

--- src/ossos/core/ossos/pipeline/astrom_mag_check.py
+++ src/ossos/core/ossos/pipeline/astrom_mag_check.py
@@ -1,24 +1,23 @@
-#!python
 ################################################################################
-##                                                                            ##
-## Copyright 2013 by its authors                                              ##
-## See COPYING, AUTHORS                                                       ##
-##                                                                            ##
-## This file is part of OSSOS Moving Object Pipeline (OSSOS-MOP)              ##
-##                                                                            ##
-##    OSSOS-MOP is free software: you can redistribute it and/or modify       ##
-##    it under the terms of the GNU General Public License as published by    ##
-##    the Free Software Foundation, either version 3 of the License, or       ##
-##    (at your option) any later version.                                     ##
-##                                                                            ##
-##    OSSOS-MOP is distributed in the hope that it will be useful,            ##
-##    but WITHOUT ANY WARRANTY; without even the implied warranty of          ##
-##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           ##
-##    GNU General Public License for more details.                            ##
-##                                                                            ##
-##    You should have received a copy of the GNU General Public License       ##
-##    along with OSSOS-MOP.  If not, see <http://www.gnu.org/licenses/>.      ##
-##                                                                            ##
+#                                                                            ##
+# Copyright 2013 by its authors                                              ##
+# See COPYING, AUTHORS                                                       ##
+#                                                                            ##
+# This file is part of OSSOS Moving Object Pipeline (OSSOS-MOP)              ##
+#                                                                            ##
+#    OSSOS-MOP is free software: you can redistribute it and/or modify       ##
+#    it under the terms of the GNU General Public License as published by    ##
+#    the Free Software Foundation, either version 3 of the License, or       ##
+#    (at your option) any later version.                                     ##
+#                                                                            ##
+#    OSSOS-MOP is distributed in the hope that it will be useful,            ##
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          ##
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           ##
+#    GNU General Public License for more details.                            ##
+#                                                                            ##
+#    You should have received a copy of the GNU General Public License       ##
+#    along with OSSOS-MOP.  If not, see <http://www.gnu.org/licenses/>.      ##
+#                                                                            ##
 ################################################################################
 """
 Compare the measured fluxes of planted sources against those returned for by digiphot.
