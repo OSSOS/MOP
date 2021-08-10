9dfbd277f3077279f256c4d19b71faea000dbb52
JJ Kavelaars <jjkavelaars@gmail.com>
2021-08-10 15:56:24 -0400

    Make py3 compatible.

--- src/ossos/core/ossos/ssos.py
+++ src/ossos/core/ossos/ssos.py
@@ -361,7 +361,7 @@ class SSOSParser(object):
             orbit.predict(obs_date)
             logger.info("Done calling predict")
             if orbit.dra > 15 * units.arcminute or orbit.ddec > 15.0 * units.arcminute:
-                print "Skipping entry as orbit uncertainty at date {} is large.".format(obs_date)
+                print("Skipping entry as orbit uncertainty at date {} is large.".format(obs_date))
                 continue
             if expnum in expnums_examined:
                 logger.debug("Already checked this exposure.")
